92f080279d1f00d401cba712961a72b03776d7f7
Scott Hostovich <hostovic@imsweb.com>
2002-07-19 20:51:44 +0000

    replacing units PTdata.cpp,.h

---
+++ calculation/satscan_data/PurelyTemporalData.cpp
@@ -0,0 +1,138 @@
+#pragma hdrstop
+#include "PurelyTemporalData.h"
+
+CPurelyTemporalData::CPurelyTemporalData(CParameters* pParameters, BasePrint *pPrintDirection)
+                    :CSaTScanData(pParameters, pPrintDirection)
+{
+   m_pPTCases    = 0;
+   m_pPTSimCases = 0;
+   m_pPTMeasure  = 0;
+}
+
+CPurelyTemporalData::~CPurelyTemporalData()
+{
+   if (m_pPTSimCases)
+      DeAllocSimCases();
+}
+
+void CPurelyTemporalData::AllocSimCases()
+{
+   try
+      {
+      CSaTScanData::AllocSimCases();  // Use until MakePurelyTemporalData implemented
+      m_pPTSimCases = (count_t*)Smalloc(m_nTimeIntervals * sizeof(count_t), gpPrintDirection);
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("AllocSimCases()", "CPurelyTemporalData");
+      throw;
+      }
+}
+
+void CPurelyTemporalData::DeAllocSimCases()
+{
+   try
+      {
+      CSaTScanData::DeAllocSimCases();  // Use until MakePurelyTemporalData implemented
+      free(m_pPTSimCases);
+      m_pPTSimCases = 0;
+      }
+   catch (...)
+      {
+      }
+}
+
+void CPurelyTemporalData::ReadDataFromFiles()
+{
+   try
+      {
+      CSaTScanData::ReadDataFromFiles();
+      SetPurelyTemporalCases();
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("ReadDataFromFiles()", "CPurelyTemporalData");
+      throw;
+      }
+}
+
+bool CPurelyTemporalData::CalculateMeasure()
+{
+   bool bResult;
+
+   try
+      {
+      bResult = CSaTScanData::CalculateMeasure();
+      SetPurelyTemporalMeasures();
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("CalculateMeasure()", "CPurelyTemporalData");
+      throw;
+      }
+  return bResult;
+}
+
+void CPurelyTemporalData::DisplayCases(FILE* pFile)
+{
+   try
+      {
+      fprintf(pFile, "PT Case counts (m_pPTCases)   m_nTimeIntervals=%i\n\n", m_nTimeIntervals);
+      for (int i = 0; i < m_nTimeIntervals; i++)
+         fprintf(pFile, "PTCases [%i] = %i\n", i,m_pPTCases[i]);
+      fprintf(pFile, "\n\n");
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("DisplayCases()", "CPurelyTemporalData");
+      throw;
+      }
+}
+
+void CPurelyTemporalData::DisplaySimCases(FILE* pFile)
+{
+   try
+      {
+      fprintf(pFile, "PT Simulated Case counts (m_pPTSimCases)\n\n");
+      for (int i = 0; i < m_nTimeIntervals; i++)
+         fprintf(pFile, "PTSimCases [%i] = %i\n", i,m_pPTSimCases[i]);
+      fprintf(pFile, "\n");
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("DisplaySimCases()", "CPurelyTemporalData");
+      throw;
+      }
+}
+
+void CPurelyTemporalData::DisplayMeasure(FILE* pFile)
+{
+   try
+      {
+      fprintf(pFile, "PT Measures (m_pPTMeasure)   m_nTimeIntervals=%i\n\n", m_nTimeIntervals);
+      for (int i = 0; i < m_nTimeIntervals; i++)
+         fprintf(pFile, "PTMeasure [%i] = %f\n", i,m_pPTMeasure[i]);
+      fprintf(pFile, "\n\n");
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("DisplayMeasure()", "CPurelyTemporalData");
+      throw;
+      }
+}
+
+void CPurelyTemporalData::MakeData()
+{
+   try
+      {
+      CSaTScanData::MakeData();
+      SetPurelyTemporalSimCases();
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("MakeData()", "CPurelyTemporalData");
+      throw;
+      }
+}
+
+
---
+++ calculation/satscan_data/PurelyTemporalData.h
@@ -0,0 +1,27 @@
+//*********************************************************************************
+#ifndef __PURELYTEMPORALDATA_H
+#define __PURELYTEMPORALDATA_H
+//*********************************************************************************
+#include "SaTScanData.h"
+
+class CPurelyTemporalData : public CSaTScanData
+{
+  public:
+    CPurelyTemporalData(CParameters* pParameters, BasePrint *pPrintDirection);
+    virtual ~CPurelyTemporalData();
+
+    virtual void DisplayCases(FILE* pFile);
+    virtual void DisplaySimCases(FILE* pFile);
+    virtual void DisplayMeasure(FILE* pFile);
+
+//  protected:
+    virtual void ReadDataFromFiles();
+    virtual bool CalculateMeasure();
+    virtual void AllocSimCases();
+    virtual void DeAllocSimCases();
+    virtual void MakeData();
+
+};
+
+//*********************************************************************************
+#endif
