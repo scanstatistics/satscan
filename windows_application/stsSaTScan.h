7459771910e1d520b563fb34a3040be1370b30ed
Scott Hostovich <hostovic@imsweb.com>
2002-07-19 20:20:13 +0000

    replacing file SatScan.h

---
+++ windows_application/stsSaTScan.h
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------------
+#ifndef __SatScan_H
+#define __SatScan_H
+//---------------------------------------------------------------------------
+#include <vcl.h>
+#include "SaTScan.h"
+#include "stsFrmAbout.h"
+#include "SSException.h"
+#include "stsFrmAnalysisRun.h"
+#include "stsPrintWindow.h"
+#include "stsDlgException.h"
+#include "stsFrmOutPutViewer.h"
+#include "stsUtilityFunctions.h"
+#include "stsFrmAnalysisParameters.h"
+#include "stsFrmAnalysisRun.h"
+#include "stsCalculationThread.h"
+#include "StsMain.h"
+
+#define TECH_EMAIL "satscan@nih.gov"
+#define VERSION_NUMBER "v 3.0.0"
+#define VERSION_DATE "July 1, 2002"
+
+#endif
