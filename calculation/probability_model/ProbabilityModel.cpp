7b8e0b8f91ac878b474256e345d5da75352a5935
Scott Hostovich <hostovic@imsweb.com>
2002-07-19 20:54:17 +0000

    replacing units model.cpp,.h

---
+++ calculation/probability_model/ProbabilityModel.cpp
@@ -0,0 +1,36 @@
+#pragma hdrstop
+#include "ProbabilityModel.h"
+
+CModel::CModel(CParameters* pParameters, CSaTScanData* pData, BasePrint *pPrintDirection)
+{
+   try
+      {
+      m_pParameters = pParameters;
+      m_pData       = pData;
+      gpPrintDirection = pPrintDirection;
+
+#ifdef DEBUGMODEL
+      if ((m_pDebugModelFile = fopen("DebugModel.TXT", "w")) == NULL)
+         {
+         fprintf(stderr, "  Error: Unable to create makedata debug file.\n");
+         //FatalError(0, gpPrintDirection);
+         SSGenerateException("  Error: Unable to create makedata debug file.\n","CModel constructor");
+         }
+#endif
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("CModel", "CModel");
+      throw;
+      }
+}
+
+CModel::~CModel()
+{
+#ifdef DEBUGMODEL
+  fclose(m_pDebugModelFile);
+#endif
+}
+
+
+
---
+++ calculation/probability_model/ProbabilityModel.h
@@ -0,0 +1,36 @@
+//*****************************************************************************
+#ifndef __PROBABILITYMODEL_H
+#define __PROBABILITYMODEL_H
+//*****************************************************************************
+#include "SaTScan.h"
+#include "Parameters.h"
+
+//#define DEBUGMODEL 0
+
+class CPSMonotoneCluster;
+class CSaTScanData;
+
+class CModel
+{
+  public:
+    CModel(CParameters* pParameters, CSaTScanData* pData, BasePrint *pPrintDirection);
+    virtual ~CModel();
+
+    CParameters*  m_pParameters;
+    CSaTScanData* m_pData;
+    BasePrint *gpPrintDirection;
+    FILE*         m_pDebugModelFile;
+
+    virtual bool   ReadData() = 0;
+    virtual bool   CalculateMeasure() = 0;
+    virtual double GetLogLikelihoodForTotal() const = 0;
+    virtual double CalcLogLikelihood(count_t n, measure_t u) = 0;
+    virtual double CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster) = 0;
+    virtual void   MakeData() = 0;
+    virtual double GetPopulation(int m_iEllipseOffset, tract_t nCenter, tract_t nTracts,
+                                 int nStartInterval, int nStopInterval) = 0;
+};
+
+//*****************************************************************************
+#endif
+
