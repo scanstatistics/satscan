79da8ca8771cf901ee46327184afc9d6c0caaeb9
Scott Hostovich <hostovic@imsweb.com>
2002-07-19 20:47:14 +0000

    replacing units GInfo.cpp,.h

---
+++ calculation/satscan_data/GridTractCoordinates.cpp
@@ -0,0 +1,369 @@
+#pragma hdrstop
+#include "GridTractCoordinates.h"
+
+/**********************************************************************
+ file: GridTractCoordinates.cpp
+ This file abstracts data on a special grid as it is read from a file.
+
+ DATA REPRESENTATION (internal):
+ Each special grid geographic tract is represented by a "struct gnode" in an
+ array, sorted by increasing grid tract-id.
+ The gnode records the grid tract-id and coordinates.
+ **********************************************************************/
+
+/* ==========================Grid  Tract Routines =========================== */
+GInfo::GInfo(BasePrint *pPrintDirection)
+{
+   Init();
+   gpPrintDirection = pPrintDirection;
+}
+
+GInfo::~GInfo()
+{
+   Free();
+}
+
+void GInfo::Init()
+{
+ GridTractInfo = 0;
+ gi_length     = 0;
+ NumGridTracts = 0;
+ nDimensions   = 0;
+}
+void GInfo::Free()
+{
+   giCleanup();
+}
+/**********************************************************************
+ Insert a grid tract into the vector.
+ Parameters:
+   gid   - grid tract ID
+   x, y  - grid tract coordinates
+ Return value:
+   0 = duplicate tract ID
+   1 = success
+ **********************************************************************/
+int GInfo::giInsertGnode(char *gid, double* pCoords)
+{
+   tract_t t = NumGridTracts - 1;
+   int i;
+
+   try
+      {
+      /* Grow GridTractInfo, if needed */
+      if (NumGridTracts >= gi_length)
+        {
+        gi_length += TI_GROW;
+        GridTractInfo = (gnode*)Srealloc(GridTractInfo, gi_length * sizeof(struct gnode), gpPrintDirection);
+        }
+
+      /* Find insertion point */
+      while (t >= 0 && strcmp(gid, GridTractInfo[t].gid) < 0)
+      {
+        GridTractInfo[t + 1] = GridTractInfo[t];
+        --t;
+      }
+
+      /* if duplicate */
+      if (t >= 0 && !strcmp(gid, GridTractInfo[t].gid))
+        return 0;
+
+      t++;
+
+      GridTractInfo[t].gid = (char*) malloc(strlen(gid)+1);
+      strcpy(GridTractInfo[t].gid, gid);
+      GridTractInfo[t].pCoords = (double*)Smalloc(nDimensions * sizeof(double), gpPrintDirection);
+      for (i=0; i<nDimensions; i++)
+      {
+         GridTractInfo[t].pCoords[i] = pCoords[i];
+      }
+      //  GridTractInfo[t].y    = (float)y;
+      //  GridTractInfo[t].z    = (float)z;
+
+      NumGridTracts++;
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("giInsertGnode()", "GInfo");
+      throw;
+      }
+  return(1);
+} /* giInsertGnode() */
+
+/**********************************************************************
+ Returns the number of tracts observed
+ **********************************************************************/
+int GInfo::giGetNumDimensions()
+{
+   return nDimensions;
+}
+/**********************************************************************
+ Returns the number of tracts observed
+ **********************************************************************/
+tract_t GInfo::giGetNumTracts(void)
+{
+   return(NumGridTracts);
+} /* giGetNumTracts() */
+
+/**********************************************************************
+ Searches GridTractInfo for tract-id "gid".  Returns the index,
+ or -1 if not found.
+ Uses binary search.
+ **********************************************************************/
+tract_t GInfo::giGetTractNum(char *gid)
+{
+   tract_t a = 0;                               /* start of array to search */
+   tract_t b = NumGridTracts - 1;                 /* end of array to search */
+   tract_t m;                                                   /* midpoint */
+
+   try
+      {
+      while (b - a > 1)
+         {
+         m = (a + b) / 2;
+         if (strcmp(gid, GridTractInfo[m].gid) < 0)
+            b = m - 1;
+         else
+            a = m;
+         }
+
+      if (!strcmp(gid, GridTractInfo[a].gid))
+         return(a);
+      if (!strcmp(gid, GridTractInfo[b].gid))
+         return(b);
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("giGetTractNum()", "GInfo");
+      throw;
+      }
+  return(-1);
+} /* giGetTractNum() */
+
+
+/**********************************************************************
+ Returns the tract name (gid) for the given tract_t index.
+ **********************************************************************/
+char* GInfo::giGetGid(tract_t t)
+{
+  if (0 <= t && t < NumGridTracts)
+     return(GridTractInfo[t].gid);
+  else
+    return(0);
+} /* giGetGid() */
+
+/**********************************************************************
+ Sets the number of dimensions in Ginfo.
+ **********************************************************************/
+void GInfo::giSetDimensions(int nDim)
+{
+   nDimensions = nDim;
+}
+
+
+/**********************************************************************
+ Returns the tract coords for the given tract_t index.  The allocation
+ of pCoords MUST be deallocated in the calling function.
+ **********************************************************************/
+void GInfo::giGetCoords(tract_t t, double** pCoord) const
+{
+   int i;
+
+   try
+      {
+      //pCoords = (float**)Smalloc(nDimensions * sizeof(float*));
+      if (0 <= t && t < NumGridTracts)
+         {
+         //*x = GridTractInfo[t].x;
+         //*y = GridTractInfo[t].y;
+         //*z = GridTractInfo[t].z;
+         *pCoord = (double*)Smalloc(nDimensions * sizeof(double), gpPrintDirection);
+         for (i=0; i<nDimensions; i++)
+            {
+    	    (*pCoord)[i] = GridTractInfo[t].pCoords[i];
+            }
+         }
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("giGetCoords()", "GInfo");
+      throw;
+      }
+} /* giGetCoords() */
+
+/**********************************************************************
+ Returns the tract coords for the given tract_t index.  The allocation
+ of pCoords is made in the calling function.
+ **********************************************************************/
+void GInfo::giGetCoords2(tract_t t, double* pCoord) const
+{
+   int i;
+
+   try
+      {
+      if (0 <= t && t < NumGridTracts)
+         {
+         for (i=0; i<nDimensions; i++)
+    	    pCoord[i] = GridTractInfo[t].pCoords[i];
+         }
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("giGetCoords2()", "GInfo");
+      throw;
+      }
+} /* giGetCoords2() */
+
+/* =========================== Display Routines ============================ */
+
+/**********************************************************************
+ Display tract info - x and y coordinates.
+ **********************************************************************/
+void GInfo::giDisplayGridTractInfo()
+{
+   int i;
+
+   try
+      {
+      gpPrintDirection->SatScanPrintf("GID        x           y\n");
+      for (i=0; i<NumGridTracts; i++)
+         {
+        gpPrintDirection->SatScanPrintf("%s   %f   %f\n", GridTractInfo[i].gid, GridTractInfo[i].pCoords[0], GridTractInfo[i].pCoords[1]);
+        }
+      gpPrintDirection->SatScanPrintf("\n");
+      }
+   catch (SSException & x)
+      {
+      x.AddCallpath("giDisplayGridTractInfo()", "GInfo");
+      throw;
+      }
+} /* giDisplayTractInfo */
+
+
+/**********************************************************************
+ Cleanup all tract storage.
+ **********************************************************************/
+void GInfo::giCleanup()
+{
+  int i;
+  //int j;
+
+  if (GridTractInfo)
+     {
+     for (i=0; i<NumGridTracts; i++)
+        {
+        free(GridTractInfo[i].gid);
+        free(GridTractInfo[i].pCoords);
+        }
+     free(GridTractInfo);
+     }
+} /*giCleanup */
+
+
+/**********************************************************************
+Look for grid tract with identical coordinates.  Copied from tinfo
+version, 7/6/98, G. Gherman
+ **********************************************************************/
+bool GInfo::giFindDuplicateCoords(FILE* pDisplay)
+{
+   //bool bStop = false;
+   bool bFirstTime = true;
+   bool bFoundDuplicates = false;
+   double* pCoords = 0;
+   double* pCoords2 = 0;
+   tract_t i=0;
+   tract_t j;
+   int nDims;							// Counter for dimensions
+
+   try
+      {
+ /*     while (!bStop && i<NumGridTracts)
+      {
+        giGetCoords(i, &pCoords);
+        j = i+1;
+
+        while (!bStop && j<NumGridTracts)
+        {
+          giGetCoords(j, &pCoords2);
+          nDims = 0;
+          while (nDims < nDimensions && (pCoords[nDims] == pCoords2[nDims]))
+          {
+          	nDims++;
+          }
+          if (nDims == nDimensions)
+          	bStop=true;
+          else
+          	j++;
+
+          free(pCoords2);
+        }
+
+        if (!bStop)
+        {
+        	free(pCoords);
+          i++;
+        }
+      }
+
+      if (bStop)
+      {
+        fprintf(pDisplay, "  Error: Duplicate coordinates found for grid tracts %s and %s Coords=(",
+            GridTractInfo[i].gid, GridTractInfo[j].gid);
+        gpPrintDirection->SatScanPrintWarning("  Error: Duplicate coordinates found for grid tracts %s and %s Coords=(",
+            GridTractInfo[i].gid, GridTractInfo[j].gid);
+        for (nDims=0; nDims<(nDimensions-1); nDims++)
+        {
+        	fprintf(pDisplay, "%.0f, ", pCoords[nDims]);
+        }
+        fprintf(pDisplay, "%.0f).\n\n", pCoords[nDimensions-1]);
+        free(pCoords);
+      } */
+     while (i<NumGridTracts)
+        {
+        giGetCoords(i, &pCoords);
+        j = i+1;
+
+        while ((j<NumGridTracts) && !bFoundDuplicates)
+          {
+          giGetCoords(j, &pCoords2);
+          nDims = 0;
+          while (nDims < nDimensions && (pCoords[nDims] == pCoords2[nDims]))
+             {
+             nDims++;
+             }
+          if (nDims == nDimensions)
+             bFoundDuplicates=true;
+          j++;
+
+          free(pCoords2);
+          }
+
+        if (bFoundDuplicates && bFirstTime)
+           {
+           bFirstTime = false;
+           //fprintf(pDisplay, "  Error: Duplicate coordinates found for grid tracts %s and %s Coords=(",
+           //  GridTractInfo[i].gid, GridTractInfo[j].gid);
+           //gpPrintDirection->SatScanPrintWarning("  Error: Duplicate coordinates found for grid tracts %s and %s Coords=(",
+           //    GridTractInfo[i].gid, GridTractInfo[j].gid);
+           //for (nDims=0; nDims<(nDimensions-1); nDims++)
+           //{
+           //   fprintf(pDisplay, "%.0f, ", pCoords[nDims]);
+           // }
+           //fprintf(pDisplay, "%.0f).\n\n", pCoords[nDimensions-1]);
+           gpPrintDirection->SatScanPrintWarning("Note: The grid file has multiple identical coordinates.\nThis does not effect the results, but the program will run faster if duplicates are removed.");
+           }
+        free(pCoords);
+        i++;
+        }
+
+      }
+   catch (SSException & x)
+      {
+      if (pCoords)  free(pCoords);
+      if (pCoords2)  free(pCoords2);
+      x.AddCallpath("giFindDuplicateCoords(File *)", "GInfo");
+      throw;
+      }
+  //return (bStop);
+  return false;
+}
---
+++ calculation/satscan_data/GridTractCoordinates.h
@@ -0,0 +1,58 @@
+//*****************************************************************************
+#ifndef __GRIDTRACTCOORDINATES_H
+#define __GRIDTRACTCOORDINATES_H
+//*****************************************************************************
+#include "SaTScan.h"
+#include "JulianDates.h"
+
+/**********************************************************************
+ file: GridTractCoordinates.h
+ Header file for GridTractCoordinates.cpp, which abstracts grid tract coordinates
+ **********************************************************************/
+
+struct gnode {              /* grid tract record    */
+   char*         gid;       /* grid tract id string */
+   double*        pCoords;   /* coordinates          */
+};
+
+class GInfo
+{
+private:
+ BasePrint *gpPrintDirection;
+
+ struct gnode* GridTractInfo ;  /* grid tract info vector           */
+ tract_t       gi_length     ;  /* allocated len of GridTractInfo   */
+ tract_t       NumGridTracts ;  /* number of grid tracts filled     */
+ int 	        nDimensions  ;
+
+ void Free();
+ void Init();
+public:
+   GInfo(BasePrint *pPrintDirection);
+   ~GInfo();
+
+/* Tract Routines */
+int     giInsertGnode(char *gid, double* pCoords);
+tract_t giGetNumTracts(void);
+tract_t giGetTractNum(char *gid);
+char*   giGetGid(tract_t t);
+void    giGetCoords(tract_t t, double** pCoords) const ;
+void    giGetCoords2(tract_t t, double* pCoord) const ;
+int     giGetNumDimensions();
+
+/* Display Routines */
+void giDisplayGridTractInfo();
+
+/* Misc. Routines */
+void giCleanup();
+void giSetDimensions(int nDim);
+bool giFindDuplicateCoords(FILE* pDisplay);
+};
+//*****************************************************************************
+#endif
+
+
+
+
+
+
