1945d28f895c980761717a85dcd52dd1979f0690
Scott Hostovich <hostovic@imsweb.com>
2002-07-19 20:21:44 +0000

    modifications regarding file removals and additions

--- windows_application/StsWinPrj.cpp
+++ windows_application/StsWinPrj.cpp
@@ -1,63 +1,61 @@
 //---------------------------------------------------------------------------
-
-#include <vcl.h>
+#include "stsSaTScan.h"
 #pragma hdrstop
+//---------------------------------------------------------------------------
+
 USERES("StsWinPrj.res");
-USEFORM("Stswin.cpp", frmMainForm);
-USEFORM("TfrmAnalysis.cpp", frmAnalysis);
-USEFORM("TfrmAbout.cpp", frmAbout);
-USEUNIT("Utils.cpp");
-USEUNIT("CalcThread.cpp");
-USEUNIT("Calc\tinfo.cpp");
-USEUNIT("Calc\BernoulliModel.cpp");
-USEUNIT("Calc\calcmsr.cpp");
-USEUNIT("Calc\cats.cpp");
-USEUNIT("Calc\Cluster.cpp");
-USEUNIT("Calc\Data.cpp");
-USEUNIT("Calc\data_display.cpp");
-USEUNIT("Calc\data_read.cpp");
-USEUNIT("Calc\Analysis.cpp");
-USEUNIT("Calc\RNG.cpp");
-USEUNIT("Calc\Salloc.cpp");
-USEUNIT("Calc\SigRatios05.cpp");
-USEUNIT("Calc\Sort.cpp");
-USEUNIT("Calc\ST+PS+PTAnalysis.cpp");
-USEUNIT("Calc\ST+PSAnalysis.cpp");
-USEUNIT("Calc\ST+PTAnalysis.cpp");
-USEUNIT("Calc\STanalysis.cpp");
-USEUNIT("Calc\STCluster.cpp");
-USEUNIT("Calc\STdata.cpp");
-USEUNIT("Calc\TIAlive.cpp");
-USEUNIT("Calc\TIAll.cpp");
-USEUNIT("Calc\TimeEstimate.cpp");
-USEUNIT("Calc\rate.cpp");
-USEUNIT("Calc\model.cpp");
-USEUNIT("Calc\Ginfo.cpp");
-USEUNIT("Calc\LatLong.cpp");
-USEUNIT("Calc\Loglikelihood.cpp");
-USEUNIT("Calc\MakeNeighbors.cpp");
-USEUNIT("Calc\MeasureList.cpp");
-USEUNIT("Calc\GetWord.cpp");
-USEUNIT("Calc\PSCluster.cpp");
-USEUNIT("Calc\PoissonModel.cpp");
-USEUNIT("Calc\PSanalysis.cpp");
-USEUNIT("Calc\PSdata.cpp");
-USEUNIT("Calc\PSManalysis.cpp");
-USEUNIT("Calc\PSMCluster.cpp");
-USEUNIT("Calc\PTanalysis.cpp");
-USEUNIT("Calc\PTCluster.cpp");
-USEUNIT("Calc\PTdata.cpp");
-USEUNIT("Calc\Randdist.cpp");
-USEFORM("xmfrmAnalysisRun.cpp", frmAnalysisRun);
-USEUNIT("Calc\date.cpp");
-USEUNIT("Calc\Param.cpp");
-USEUNIT("Calc\display.cpp");
-USEUNIT("Calc\BasePrint.cpp");
-USEUNIT("PrintWindow.cpp");
-USEUNIT("Calc\PrintScreen.cpp");
-USEUNIT("Calc\SSException.cpp");
-USEFORM("xbDlgException.cpp", BdlgException);
-USEFORM("xmfrmFileViewer.cpp", frmOutputViewer);
+USEFORM("stsMain.cpp", frmMainForm);
+USEFORM("stsFrmAnalysisParameters.cpp", frmAnalysis);
+USEFORM("stsFrmAbout.cpp", frmAbout);
+USEFORM("stsFrmAnalysisRun.cpp", frmAnalysisRun);
+USEUNIT("stsUtilityFunctions.cpp");
+USEUNIT("stsCalculationThread.cpp");
+USEUNIT("stsPrintWindow.cpp");
+USEFORM("stsDlgException.cpp", BdlgException);
+USEFORM("stsFrmOutPutViewer.cpp", frmOutputViewer);
+USEUNIT("..\calculation\utility\UtilityFunctions.cpp");
+USEUNIT("..\calculation\utility\JulianDates.cpp");
+USEUNIT("..\calculation\utility\RandonNumberGenerator.cpp");
+USEUNIT("..\calculation\utility\Salloc.cpp");
+USEUNIT("..\calculation\utility\RandomDistribution.cpp");
+USEUNIT("..\calculation\utility\Sort.cpp");
+USEUNIT("..\calculation\utility\SSException.cpp");
+USEUNIT("..\calculation\print\BasePrint.cpp");
+USEUNIT("..\calculation\print\PrintScreen.cpp");
+USEUNIT("..\calculation\probability_model\BernoulliModel.cpp");
+USEUNIT("..\calculation\probability_model\CalculateMeasure.cpp");
+USEUNIT("..\calculation\probability_model\ProbabilityModel.cpp");
+USEUNIT("..\calculation\probability_model\PoissonModel.cpp");
+USEUNIT("..\calculation\satscan_data\Tracts.cpp");
+USEUNIT("..\calculation\satscan_data\PopulationCategories.cpp");
+USEUNIT("..\calculation\satscan_data\SaTScanData.cpp");
+USEUNIT("..\calculation\satscan_data\SaTScanDataDisplay.cpp");
+USEUNIT("..\calculation\satscan_data\GridTractCoordinates.cpp");
+USEUNIT("..\calculation\satscan_data\MakeNeighbors.cpp");
+USEUNIT("..\calculation\satscan_data\PurelySpatialData.cpp");
+USEUNIT("..\calculation\satscan_data\PurelyTemporalData.cpp");
+USEUNIT("..\calculation\satscan_data\SaTScanDataRead.cpp");
+USEUNIT("..\calculation\satscan_data\SpaceTimeData.cpp");
+USEUNIT("..\calculation\cluster\Cluster.cpp");
+USEUNIT("..\calculation\cluster\SpaceTimeCluster.cpp");
+USEUNIT("..\calculation\cluster\TimeIntervalAlive.cpp");
+USEUNIT("..\calculation\cluster\TimeIntervalAll.cpp");
+USEUNIT("..\calculation\cluster\TimeEstimate.cpp");
+USEUNIT("..\calculation\cluster\IncidentRate.cpp");
+USEUNIT("..\calculation\cluster\PurelySpatialCluster.cpp");
+USEUNIT("..\calculation\cluster\PurelyTemporalCluster.cpp");
+USEUNIT("..\calculation\cluster\PurelySpatialMonotoneCluster.cpp");
+USEUNIT("..\calculation\analysis\MeasureList.cpp");
+USEUNIT("..\calculation\analysis\Analysis.cpp");
+USEUNIT("..\calculation\analysis\SignificantRatios05.cpp");
+USEUNIT("..\calculation\analysis\SpaceTimeIncludePureAnalysis.cpp");
+USEUNIT("..\calculation\analysis\SpaceTimeIncludePurelySpatialAnalysis.cpp");
+USEUNIT("..\calculation\analysis\SpaceTimeIncludePurelyTemporalAnalysis.cpp");
+USEUNIT("..\calculation\analysis\SpaceTimeAnalysis.cpp");
+USEUNIT("..\calculation\analysis\PurelySpatialAnalysis.cpp");
+USEUNIT("..\calculation\analysis\PurelySpatialMonotoneAnalysis.cpp");
+USEUNIT("..\calculation\analysis\PurelyTemporalAnalysis.cpp");
+USEUNIT("..\calculation\Parameters.cpp");
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
