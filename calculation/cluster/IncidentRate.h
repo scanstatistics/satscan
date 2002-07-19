cac1028790657f46ebde1e86bf6210d326f36267
Scott Hostovich <hostovic@imsweb.com>
2002-07-19 21:00:30 +0000

    replacing files rate.cpp,.h

---
+++ calculation/cluster/IncidentRate.h
@@ -0,0 +1,15 @@
+//*****************************************************************************
+#ifndef __INCIDENTRATE_H
+#define __INCIDENTRATE_H
+//*****************************************************************************
+#include "SaTScan.h"
+#include "UtilityFunctions.h"
+
+typedef bool(*RATE_FUNCPTRTYPE)(count_t, measure_t, count_t, measure_t);
+
+bool LowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
+bool HighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
+bool HighOrLowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
+
+//*****************************************************************************
+#endif
