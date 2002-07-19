cc5fcef02f528f2dc3696a70dbc7dc19aa84f845
Scott Hostovich <hostovic@imsweb.com>
2002-07-19 21:10:38 +0000

    replacing files SigRatios05.cpp,.h

---
+++ calculation/analysis/SignificantRatios05.h
@@ -0,0 +1,26 @@
+//**********************************************************************************
+#ifndef __SIGNIFICANTRATIOS05_H
+#define __SIGNIFICANTRATIOS05_H
+//**********************************************************************************
+#include "SatScan.h"
+
+class CSignificantRatios05
+{
+  public:
+    CSignificantRatios05(int nReplications, BasePrint *pPrintDirection);
+    ~CSignificantRatios05();
+
+    void   Initialize();
+    bool   AddRatio(double r);
+    void   CalcAlphas();
+    double GetAlpha01();
+    double GetAlpha05();
+
+  private:
+    int     m_nTotalReplications;
+    int     m_nRatios;
+    double* m_pRatiosList;
+
+};
+//**********************************************************************************
+#endif
