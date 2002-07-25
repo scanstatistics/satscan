3f58e5fa4f3658d5f42c0d1dff2da7e3b630b652
Scott Hostovich <hostovic@imsweb.com>
2002-07-25 14:09:09 +0000

    batch mode project files

---
+++ windows_application/SaTScanBatchMain.cpp
@@ -0,0 +1,132 @@
+// main.cpp
+//---------------------------------------------------------------------------
+
+#include <time.h>
+#include <process.h>
+#include "UtilityFunctions.h"
+#include "Parameters.h"
+#include "PurelySpatialAnalysis.h"
+#include "PurelySpatialMonotoneAnalysis.h"
+#include "PurelyTemporalAnalysis.h"
+#include "SpaceTimeAnalysis.h"
+#include "SpaceTimeIncludePurelySpatialAnalysis.h"
+#include "SpaceTimeIncludePurelyTemporalAnalysis.h"
+#include "SpaceTimeIncludePureAnalysis.h"
+#include "PurelySpatialData.h"
+#include "PurelyTemporalData.h"
+#include "SpaceTimeData.h"
+#include "PrintScreen.h"
+
+//#define PROFILE_RUN 0
+
+int main(int argc, char *argv[])
+{
+   char szOutputFilename[400];
+   time_t RunTime;
+   char c;
+   CSaTScanData* pData        = 0;
+   CAnalysis*    pAnalysis    = 0;
+   CParameters*  pParams      = 0;
+   PrintScreen  *pPrintScreen = 0;
+
+   try
+      {
+      time(&RunTime); // Pass to analysis to include in report
+
+      DisplayVersion(stdout, 0);
+      fprintf(stdout, "\n");
+
+      pParams = new CParameters(true);
+      pPrintScreen = new PrintScreen();
+      pParams->SetPrintDirection(pPrintScreen);
+      
+      if (argc != 2)
+         SSGenerateException("  Error: Invalid Number of Arguments on Command Line.\n","main.cpp");
+
+      //pParameters = new CParameters(argv[1]);
+
+      //the SetParameters() function calls ValidateParameters !!
+      if (!(pParams->SetParameters(argv[1])))
+         SSGenerateException("Invalid parameter file.","main.cpp");
+
+      switch (pParams->m_nAnalysisType)
+         {
+         case PURELYSPATIAL  : pData = new CPurelySpatialData(pParams, pPrintScreen);  break;
+         case PURELYTEMPORAL : pData = new CPurelyTemporalData(pParams, pPrintScreen); break;
+         case SPACETIME      : pData = new CSpaceTimeData(pParams, pPrintScreen);      break;
+         case PROSPECTIVESPACETIME : pData = new CSpaceTimeData(pParams, pPrintScreen);break;
+         default:
+             SSGenerateException("Invalid Analysis Type Encountered.", "main.cpp");
+         };
+
+      pData->ReadDataFromFiles();
+
+      switch (pParams->m_nAnalysisType)
+         {
+         case PURELYSPATIAL  : if (pParams->m_nRiskFunctionType == STANDARDRISK)
+                                  pAnalysis = new CPurelySpatialAnalysis(pParams, pData, pPrintScreen);
+                               else if (pParams->m_nRiskFunctionType == MONOTONERISK)
+                                  pAnalysis = new CPSMonotoneAnalysis(pParams, pData, pPrintScreen);
+                               break;
+         case PURELYTEMPORAL : pAnalysis = new CPurelyTemporalAnalysis(pParams, pData, pPrintScreen);
+                               break;
+         case SPACETIME      : if (pParams->m_bIncludePurelySpatial &&
+                                   pParams->m_bIncludePurelyTemporal)
+                                  pAnalysis = new C_ST_PS_PT_Analysis(pParams, pData, pPrintScreen);
+                               else if (pParams->m_bIncludePurelySpatial)
+                                  pAnalysis = new C_ST_PS_Analysis(pParams, pData, pPrintScreen);
+                               else if (pParams->m_bIncludePurelyTemporal)
+                                  pAnalysis = new C_ST_PT_Analysis(pParams, pData, pPrintScreen);
+                               else
+                                  pAnalysis = new CSpaceTimeAnalysis(pParams, pData, pPrintScreen);
+                                break;
+         case PROSPECTIVESPACETIME      : if (pParams->m_bIncludePurelySpatial &&
+                                   pParams->m_bIncludePurelyTemporal)
+                                  pAnalysis = new C_ST_PS_PT_Analysis(pParams, pData, pPrintScreen);
+                               else if (pParams->m_bIncludePurelySpatial)
+                                  pAnalysis = new C_ST_PS_Analysis(pParams, pData, pPrintScreen);
+                               else if (pParams->m_bIncludePurelyTemporal)
+                                  pAnalysis = new C_ST_PT_Analysis(pParams, pData, pPrintScreen);
+                               else
+                                  pAnalysis = new CSpaceTimeAnalysis(pParams, pData, pPrintScreen);
+                                break;
+         }
+
+      if (!pAnalysis->Execute(RunTime))
+         SSGenerateException("An Error has occured within the Calculation Module.", "main.cpp");
+      else
+         {
+         pPrintScreen->SatScanPrintf("\nSaTScan completed successfully.\n");
+         pPrintScreen->SatScanPrintf("The results have been written to: \n");
+         pPrintScreen->SatScanPrintf("  %s\n\n",pParams->m_szOutputFilename);
+         pPrintScreen->SatScanPrintf("\nPress <Enter> to exit the SaTScan program.");
+         getc(stdin);
+//  #if !PROFILE_RUN
+//  HoldForEnter();
+//  #endif
+
+         delete pAnalysis;    pAnalysis = 0;
+         delete pData;        pData = 0;
+         delete pParams;      pParams=0;
+         delete pPrintScreen; pPrintScreen=0;
+         }
+      }
+   catch (SSException & x)
+      {
+      delete pAnalysis;
+      delete pData;
+      delete pParams;
+      pPrintScreen->SatScanPrintf("\nAn error has occured!!\n");
+      pPrintScreen->SatScanPrintf(x.GetCallpath());
+      pPrintScreen->SatScanPrintf(" \n ");
+      pPrintScreen->SatScanPrintf(x.GetErrorMessage());
+      pPrintScreen->SatScanPrintf("\nProgram terminated.");
+      //then print this....
+      pPrintScreen->SatScanPrintf("\nPress <Enter> to exit the SaTScan program.");
+      getc(stdin);
+      delete pPrintScreen;
+      exit(1);
+      }
+  return 1;
+} /* main() */
+
