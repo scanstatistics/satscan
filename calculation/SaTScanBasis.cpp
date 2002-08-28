376fd000cbe4c36d2b5ec0afb2881afa6add7409
Scott Hostovich <hostovic@imsweb.com>
2002-08-28 15:01:37 +0000

    basis inclusion file

---
+++ calculation/SaTScanBasis.cpp
@@ -0,0 +1,11 @@
+#include "SaTScan.h"
+#pragma hdrstop
+#include "SaTScanBasis.h"
+
+#ifndef __BATCH_COMPILE
+ #include<Basis540.h>
+ #include<Basis540.cpp>
+#else
+ #include<Basis540Unix.h>
+ #include<Basis540Unix.cpp>
+#endif
---
+++ calculation/SaTScanBasis.h
@@ -0,0 +1,14 @@
+//---------------------------------------------------------------------------
+#ifndef __SaTScanBasis_H
+#define __SaTScanBasis_H
+//---------------------------------------------------------------------------
+
+#ifndef __BATCH_COMPILE
+// defines go here for windows interface -- vcl dependent stuff
+#endif
+
+// defines for non-vcl dependent stuff here
+
+//---------------------------------------------------------------------------
+#endif
+ 
