7e52bcd1855cd15b4bb7bb25eb171799b768248e
Adam Vaughn <vaughna@imsweb.com>
2002-08-29 18:19:10 +0000

    added defines to support inclusion of a few Basis units

--- calculation/SaTScanBasis.h
+++ calculation/SaTScanBasis.h
@@ -5,12 +5,12 @@
 
 #ifndef __BATCH_COMPILE
 // defines go here for windows interface -- vcl dependent stuff
-   #define __USE_xbDlgBaseImporter   
-   #define __USE_xbCSVFile
+  #define __USE_xbDlgBaseImporter
+  #define __USE_xbCSVFile
+  #define __USE_xbDlgException
 #endif
-   
-// defines for non-vcl dependent stuff here
 
+// defines for non-vcl dependent stuff here
 //---------------------------------------------------------------------------
 #endif
  
