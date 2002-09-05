432b89433328a323e77a0071eab5b68e4da050a1
Adam Vaughn <vaughna@imsweb.com>
2002-09-05 17:57:22 +0000

    small changes needed to make members accessible to the descendants

--- calculation/Output/stsOutputDBF.h
+++ calculation/Output/stsOutputDBF.h
@@ -3,15 +3,15 @@
 
 class DBaseOutput {
    private:
-      ZdString	                gsFileName;
-      ZdVector<ZdField*>        gvFields;
+      
 
       void	Init();
       void	Setup(const ZdString& sFileName);
    protected:
-      void 	CreateDBFFile();
-//      void 	FillDBFFile();
+      ZdString	                gsFileName;
+      ZdVector<ZdField*>        gvFields;
 
+      void 	        CreateDBFFile();
       void              GetFields(ZdVector<ZdField*>& vFields);
       virtual void 	SetupFields(ZdVector<ZdString>& vFieldNames, ZdVector<char>& vFieldTypes, ZdVector<short>& vFieldLengths) = 0;
    public:
