96129f201b614d16d6670284389cef03a8e3ec72
Scott Hostovich <hostovic@imsweb.com>
2002-09-06 16:31:37 +0000

    * removed include directive for stsDlgException.h - replaced by basis unit * moved include directive for Basis540.h up, so that basis classes defined   before units that attempt to referance them.

--- windows_application/stsSaTScan.h
+++ windows_application/stsSaTScan.h
@@ -4,18 +4,18 @@
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #include "SaTScan.h"
+#include <Basis540.h>
+#include "DBFFile.h"
 #include "stsFrmAbout.h"
 #include "SSException.h"
 #include "stsFrmAnalysisRun.h"
 #include "stsPrintWindow.h"
-#include "stsDlgException.h"
 #include "stsFrmOutPutViewer.h"
 #include "stsUtilityFunctions.h"
 #include "stsFrmAnalysisParameters.h"
 #include "stsFrmAnalysisRun.h"
 #include "stsCalculationThread.h"
 #include "stsMain.h"
-#include <Basis540.h>
 
 #define TECH_EMAIL "satscan@nih.gov"
 #define VERSION_NUMBER "v 3.0.0"
