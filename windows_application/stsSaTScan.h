a62181c81103bf7bee95bb054321ce38a252acdc
Scott Hostovich <hostovic@imsweb.com>
2002-09-16 18:22:09 +0000

    * set version to 3.0.0 beta

--- windows_application/stsSaTScan.h
+++ windows_application/stsSaTScan.h
@@ -18,7 +18,7 @@
 #include "stsMain.h"
 
 #define TECH_EMAIL "satscan@nih.gov"
-#define VERSION_NUMBER "v 3.0.0"
+#define VERSION_NUMBER "v 3.0.0 beta"
 #define VERSION_DATE "July 1, 2002"
 
 #endif
