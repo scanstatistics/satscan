f6f8c96f27919fb1cbcf4abee27d3631a56bdaa6
Scott Hostovich <hostovic@imsweb.com>
2002-10-02 15:07:28 +0000

    * Alpha ordered function prototypes. * Pure virtual functions defined to provide means if determining case count   and measure for an area/location as defined by interval:    virtual count _t      GetCaseCountForTract(tract_t tTract, count_t** pCases) const = 0;       - returns number of cases for tract as defined by interval    virtual measure_t   GetMeasureForTract(tract_t tTract,  measure_t** pMeasure)       - returns measure for tract as defined by interval

--- calculation/cluster/TimeIntervals.h
+++ calculation/cluster/TimeIntervals.h
@@ -10,18 +10,16 @@ class CTimeIntervals
     CTimeIntervals(int nTotal, int nCut) {m_nTotal=nTotal; m_nCut=nCut;};
     virtual ~CTimeIntervals() {};
 
-    virtual void Initialize() {};
-    virtual void InitializeRange(int nLow, int nHigh) { };
-    virtual bool GetNextTimeInterval(const count_t*& pCases,
-                                     const measure_t*& pMeasure,
-                                     count_t&   nCases,
-                                     measure_t& nMeasure,
-                                     int&       nStart,
-                                     int&       nStop) {return false;};
-    virtual bool GetNextTimeIntervalProsp(const count_t*& pCases,
-                                     const measure_t*& pMeasure,
-                                     count_t& nCases,
-                                     measure_t& nMeasure) {return false;};
+    virtual void        Initialize() {};
+    virtual void        InitializeRange(int nLow, int nHigh) { };
+    virtual count_t     GetCaseCountForTract(tract_t tTract, count_t** pCases) const = 0;
+    virtual measure_t   GetMeasureForTract(tract_t tTract,  measure_t** pMeasure) const = 0;
+    virtual bool        GetNextTimeInterval(const count_t*& pCases, const measure_t*& pMeasure,
+                                            count_t& nCases, measure_t& nMeasure,
+                                            int& nStart, int& nStop)
+                                            {return false;}
+    virtual bool GetNextTimeIntervalProsp(const count_t*& pCases, const measure_t*& pMeasure,
+                                          count_t& nCases, measure_t& nMeasure) {return false;};
     /*virtual bool GetNextTimeIntervalProsp(const count_t*& pCases,
                                      const measure_t*& pMeasure,
                                      count_t& nCases,
@@ -34,5 +32,4 @@ class CTimeIntervals
     int m_nTotal;
     int m_nCut;
 };
-
 #endif
