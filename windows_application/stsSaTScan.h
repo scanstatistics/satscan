f52b130b0f6cf8ef4aca496a7c4bd170748466ef
Adam Vaughn <vaughna@imsweb.com>
2002-11-20 14:28:30 +0000

    added define for license agreement website

--- windows_application/stsSaTScan.h
+++ windows_application/stsSaTScan.h
@@ -22,4 +22,5 @@
 #define VERSION_NUMBER "v 3.0"
 #define VERSION_DATE "October 17, 2002"
 #define SATSCAN_WEBSITE "http:/\/srab.cancer.gov/satscan/"
+#define SATSCAN_LICENSE "http:/\/srab.cancer.gov/satscan/license.html"
 #endif
