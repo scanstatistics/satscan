4d8bec360fe9a8d68bd7bb7b0ebab57d95c01671
Adam Vaughn <vaughna@imsweb.com>
2002-11-25 19:40:41 +0000

    new function SatScanPrintInputFileWarning to be used to track and limit input file warnings which are printed out to the user (Squish #13285)

--- calculation/print/BasePrint.cpp
+++ calculation/print/BasePrint.cpp
@@ -2,6 +2,8 @@
 #pragma hdrstop
 #include "BasePrint.h"
 
+const int MAX_INPUT_FILE_WARNING_LIMIT = 75;
+
 //---------------------------------------------------------------------------
 BasePrint::BasePrint()
 {
@@ -14,6 +16,26 @@ BasePrint::~BasePrint() {
     delete [] gsMessage;
   }
   catch (...){}
+}
+
+// returns the string representation of what the enumeration actually stands for
+// pre : none
+// post : returns a string by reference of the file type name if eType is valid
+void BasePrint::GetInputFileType(eInputFileType eType, std::string& sName) {
+   switch (eType) {
+      case CASEFILE :
+         sName = "Cases"; break;
+      case CONTROLFILE :
+         sName = "Controls"; break;
+      case POPFILE :
+         sName = "Population"; break;
+      case COORDFILE :
+         sName = "Coordinates"; break;
+      case GRIDFILE :
+         sName = "Special Grid"; break;
+      default :
+         ZdException::GenerateNotification("Invalid input file type warning message!", "ERROR!");
+   }
 }
 
 void BasePrint::SatScanPrintf(const char * sMessage, ... )
@@ -35,6 +57,48 @@ void BasePrint::SatScanPrintWarning(const char * sMessage, ... )
    PrintWarningLine(gsMessage);
    va_end ( varArgs );
 }
+
+// function for printing out input file warning messages, this function will print out MAX_INPUT_FILE_WARNING_LIMIT
+// number of input file messages from each input file type, then will print a warning telling the user to check the
+// input file format
+// pre : none
+// post : increments the counter in the global map for the message type (or starts a new counter if not found) and
+//       if the number of messages for that file type is less than the maximum then it just prints as normal
+void BasePrint::SatScanPrintInputFileWarning(eInputFileType eType, const char* sMessage, ...)
+{
+   bool bPrintAsNormal(true);
+   std::map<eInputFileType, int>::iterator iter = gInputFileWarningsMap.find(eType);
+
+   if (iter == gInputFileWarningsMap.end()) {
+      gInputFileWarningsMap.insert(std::make_pair(eType, 1));
+   }
+   else {
+      iter->second++;
+
+      // print the excessive warning message on the MAX_INPUT_FILE_WARNING_LIMIT time - else print nothing past -- AJV
+      if (iter->second == MAX_INPUT_FILE_WARNING_LIMIT) {
+         bPrintAsNormal = false;
+         std::string sInputFile, message;
+         GetInputFileType(eType, sInputFile);
+         message = "ERROR: Excessive number of warnings in the ";
+         message += sInputFile;
+         message += " input file.\n       Please check the file structure.\n";
+         PrintWarningLine(const_cast<char*>(message.c_str()));
+      }
+      else if(iter->second > MAX_INPUT_FILE_WARNING_LIMIT)
+         bPrintAsNormal = false;
+   }
+
+   if(bPrintAsNormal) {  // just some code stolen from the SatScanPrintWarning function
+      va_list   varArgs;
+
+      va_start ( varArgs, sMessage );
+      PrintMessage ( varArgs, sMessage );
+      PrintWarningLine(gsMessage);
+      va_end ( varArgs );
+   }
+}
+
 //---------------------------------------------------------------------------
 //*** There are two different versions of this function because vsnprintf behaves
 //*** in completely different ways on the two platforms. Under BCB 5, vsnprintf()
--- calculation/print/BasePrint.h
+++ calculation/print/BasePrint.h
@@ -5,22 +5,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <map>
 
 class BasePrint
 {
+   public:
+     enum eInputFileType { CASEFILE, CONTROLFILE, POPFILE, COORDFILE, GRIDFILE };
    private:
      char *gsMessage;
+     std::map<eInputFileType, int>   gInputFileWarningsMap;
+     std::map<eInputFileType, bool>  gInputFileWarningsExceeded;
 
+     void GetInputFileType(eInputFileType eType, std::string& sName);
      void PrintMessage( va_list varArgs, const char *sMessage );
+   
    public:
-      BasePrint();
+     BasePrint();
      ~BasePrint();
 
-   virtual bool GetIsCanceled() = 0;
-   void SatScanPrintf(const char * sMessage, ... );
-   void SatScanPrintWarning(const char * sMessage, ... );
-   virtual void PrintLine(char *) = 0;
-   virtual void PrintWarningLine(char *) = 0;
+     virtual bool GetIsCanceled() = 0;
+     void SatScanPrintf(const char * sMessage, ... );
+     void SatScanPrintWarning(const char * sMessage, ... );
+     void SatScanPrintInputFileWarning(eInputFileType eType, const char* sMessage, ...);
+     virtual void PrintLine(char *) = 0;
+     virtual void PrintWarningLine(char *) = 0;
 };
 
 //*****************************************************************************
