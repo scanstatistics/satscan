b55ec88fe8bb21d58d354f81c4ada3dfedb28e9c
Scott Hostovich <hostovic@imsweb.com>
2002-11-27 22:01:35 +0000

    * bug fix for ver 3.0.1

--- windows_application/stsFrmAnalysisParameters.cpp
+++ windows_application/stsFrmAnalysisParameters.cpp
@@ -319,12 +319,11 @@ void __fastcall TfrmAnalysis::btnPopBrowseClick(TObject *Sender) {
           //GetLongPathName(sBuffer, sBuffer, sizeof(sBuffer));
           sFileName.SetLocation(sBuffer);
           sFileName.SetExtension(TXD_EXT);
-          ImportDescriptor.SetDestinationFile(sFileName.GetFullPath());
           // Prefix filename so that we know this sessions created imported files are unique.
           sFileNamePrefix << sFileName.GetFileName();
           sFileName.SetFileName(sFileNamePrefix);
-          SetupPopFileFieldDescriptors(vFieldDescriptors);
-          CreateTXDFile(sFileName, vFieldDescriptors);
+          ImportDescriptor.SetDestinationFile(sFileName.GetFullPath());
+          CreateTXDFile(sFileName, gvPopFileFieldDescriptors);
           auto_ptr<TBdlgImporter> pImporter(new TBdlgImporter(0, 0, &ImportDescriptor));
           pImporter->ShowOptionalPanels(false, false, false);
           if (pImporter->ShowModal() == mrOk) {
