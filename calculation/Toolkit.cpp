17ffe85342a4c5f9a3e032d71a5a28642bc5b96c
Scott Hostovich <hostovic@imsweb.com>
2002-12-13 13:38:08 +0000

    * Toolkit class derived from basis Toolkit.    - Provides controlled/scoped global access to program constants.    - Insures existance of system ini file and defined constants.    - Permits re-defining constants w/o re-compiling program.

---
+++ calculation/Toolkit.cpp
@@ -0,0 +1,217 @@
+//---------------------------------------------------------------------------
+#include "SaTScan.h"
+#pragma hdrstop
+//---------------------------------------------------------------------------
+
+/** system filename */
+const char * SaTScanToolkit::gsSystemIniFileName = "system.ini";
+/** ini section property name for run history filename */
+const char * SaTScanToolkit::gsHistoryFileNameProperty = "[RunHistory].FileName";
+/** ini section property name for logging run history */
+const char * SaTScanToolkit::gsLoggingProperty = "[RunHistory].LogHistory";
+/** ini section property name for website */
+const char * SaTScanToolkit::gsSaTScanWebSiteProperty = "[Internet].WebsiteURL";
+/** ini section property name for license website */
+const char * SaTScanToolkit::gsSaTScanLicenceWebSiteProperty = "[Internet].LicenceURL";
+/** ini section property name for substantive support email */
+const char * SaTScanToolkit::gsSubstantiveSupportEmailProperty = "[Email].SubstantiveSupportEmail";
+/** ini section property name for technical support email */
+const char * SaTScanToolkit::gsTechnicalSupportEmailProperty = "[Email].TechnicalSupportEmail";
+/** analysis history filename */
+const char * SaTScanToolkit::gsDefaultRunHistoryFileName = "AnalysisHistory";
+/** Default website. */
+const char * SaTScanToolkit::gsDefaultSaTScanWebSite = "http:/\/srab.cancer.gov/satscan/";
+/** Default website of licence. */
+const char * SaTScanToolkit::gsDefaultSaTScanLicenceWebSite = "http:/\/srab.cancer.gov/satscan/license.html";
+/** Default Substantive Support Email. */
+const char * SaTScanToolkit::gsDefaultSubstantiveSupportEmail = "satscan@nih.gov";
+/** Default Technical Support Email. */
+const char * SaTScanToolkit::gsDefaultTechnicalSupportEmail = "satscan@imsweb.com";
+
+/** constructor */
+SaTScanToolkit::SaTScanToolkit():BToolkit() {
+  try {
+    Setup();
+  }
+  catch (ZdException& x) {
+    x.AddCallpath("constructor()", "SaTScanToolkit");
+    throw;
+  }
+}
+
+/** destructor */
+SaTScanToolkit::~SaTScanToolkit() {}
+
+/** Returns license URL. */
+const char * SaTScanToolkit::GetLicenceWebSite() /*const*/ {
+  return GetSession().GetProperty(gsSaTScanLicenceWebSiteProperty)->GetValue();
+}
+
+/** Returns indicator of run history logging. */
+bool SaTScanToolkit::GetLogRunHistory() /*const*/ {
+  return GetSession().GetProperty(gsLoggingProperty)->GetBool(true);
+}
+
+/** Returns run history filename. */
+const char * SaTScanToolkit::GetRunHistoryFileName() /*const*/ {
+  return GetSession().GetProperty(gsHistoryFileNameProperty)->GetValue();
+}
+
+/** Returns substantive support email address. */
+const char * SaTScanToolkit::GetSubstantiveSupportEmail() /*const*/ {
+  return GetSession().GetProperty(gsSubstantiveSupportEmailProperty)->GetValue();
+}
+/** Returns substantive support email address. */
+const char * SaTScanToolkit::GetTechnicalSupportEmail() /*const*/ {
+  return GetSession().GetProperty(gsTechnicalSupportEmailProperty)->GetValue();
+}
+
+/** Returns website URL. */
+const char * SaTScanToolkit::GetWebSite() /*const*/ {
+  return GetSession().GetProperty(gsSaTScanWebSiteProperty)->GetValue();
+}
+
+/** Insures run history filename section in ZdIniSession. */
+bool SaTScanToolkit::InsureRunHistoryFileName() {
+  ZdFileName            FileName;
+  ZdString              sDefaultHistoryFileName;
+  long                  lPosition;
+  BSessionProperty    * pProperty;
+  bool                  bUpdatedSection=false;
+
+  try {
+    sDefaultHistoryFileName << ZdFileName(_argv[0]).GetLocation() << gsDefaultRunHistoryFileName;
+    sDefaultHistoryFileName << DBFFileType::GetDefaultInstance().GetFileTypeExtension();
+
+    //run history filename property
+    lPosition = GetSession().FindProperty(gsHistoryFileNameProperty);
+    if (lPosition == -1) {
+      GetSession().AddProperty(gsHistoryFileNameProperty, sDefaultHistoryFileName.GetCString());
+      bUpdatedSection = true;
+    }
+    else {
+      //property exists but does it have a value?
+      pProperty = GetSession().GetProperty(lPosition);
+      if (!pProperty->GetValue() || !strlen(pProperty->GetValue())) {
+        GetSession().AddProperty(gsHistoryFileNameProperty, sDefaultHistoryFileName.GetCString());
+        bUpdatedSection = true;
+      }
+      else {
+        //Check that file extension and filename exists,these will be the only attributes
+        //of filename that we will correct for them. Invalid file path and permissions
+        //will be encountered and handled during logging.
+        FileName.SetFullPath(pProperty->GetValue());
+        if (strcmp(FileName.GetExtension(), DBFFileType::GetDefaultInstance().GetFileTypeExtension())) {
+          FileName.SetExtension(DBFFileType::GetDefaultInstance().GetFileTypeExtension());
+          pProperty->SetValue(FileName.GetFullPath());
+          bUpdatedSection = true;
+        }
+        if (!strlen(FileName.GetFileName())) {
+          FileName.SetFileName(gsDefaultRunHistoryFileName);
+          pProperty->SetValue(FileName.GetFullPath());
+          bUpdatedSection = true;
+        }
+      }
+    }
+  }
+  catch (ZdException& x) {
+    x.AddCallpath("InsureRunHistoryFileName()", "SaTScanToolkit");
+    throw;
+  }
+  return bUpdatedSection;
+}
+
+/** */
+bool SaTScanToolkit::InsureSessionProperty(const char * sSessionProperty, const char * sDefaultValue) {
+  long                  lPosition;
+  BSessionProperty    * pProperty;
+  bool                  bUpdatedSection=false;
+
+  try {
+    //log history property
+    lPosition = GetSession().FindProperty(sSessionProperty);
+    if (lPosition == -1) {
+      GetSession().AddProperty(sSessionProperty, sDefaultValue);
+      bUpdatedSection = true;
+    }
+    else {
+      //property exists but does it have a value?
+      pProperty = GetSession().GetProperty(lPosition);
+      if (!pProperty->GetValue() || !strlen(pProperty->GetValue())) {
+        GetSession().AddProperty(sSessionProperty, sDefaultValue);
+        bUpdatedSection = true;
+      }
+    }
+  }
+  catch (ZdException& x) {
+    x.AddCallpath("InsureSessionProperty()", "SaTScanToolkit");
+    throw;
+  }
+  return bUpdatedSection;
+}
+
+/** Insures that all section keys are present and writes to disk if
+    necessary and permissions permit. */
+void SaTScanToolkit::InsureSessionStructure() {
+  bool                  bNeedsWrite=false;
+
+  try {
+    if (InsureRunHistoryFileName())
+      bNeedsWrite = true;
+    if (InsureSessionProperty(gsLoggingProperty, "true"))
+      bNeedsWrite = true;
+    if (InsureSessionProperty(gsSaTScanWebSiteProperty, gsDefaultSaTScanWebSite))
+      bNeedsWrite = true;
+    if (InsureSessionProperty(gsSaTScanLicenceWebSiteProperty, gsDefaultSaTScanLicenceWebSite))
+      bNeedsWrite = true;
+    if (InsureSessionProperty(gsSubstantiveSupportEmailProperty, gsDefaultSubstantiveSupportEmail))
+      bNeedsWrite = true;
+    if (InsureSessionProperty(gsTechnicalSupportEmailProperty, gsDefaultTechnicalSupportEmail))
+      bNeedsWrite = true;
+
+    //Write to same directory as executable, when needed.
+    if (bNeedsWrite) {
+      try {
+        GetSession().Write(gsSystemFileName.GetCString());
+      }
+      catch (ZdException & x){
+         /* File not currently writable.
+            NOTE: We really want to catch ZdFileOpenFailedException exceptions here
+            but ZdIniSession is causing initial thrown exception type to be lost. */
+      }
+    }
+  }
+  catch (ZdException& x) {
+    x.AddCallpath("InsureSessionStructure()", "SaTScanToolkit");
+    throw;
+  }
+}
+
+/** internal setup */
+void SaTScanToolkit::Setup() {
+  try {
+    //Set system ini located at same directory as executable.
+    gsSystemFileName << ZdFileName(_argv[0]).GetLocation() << gsSystemIniFileName;
+    
+    try {
+      //Open or create system ini file.
+      GetSession().Read(gsSystemFileName.GetCString(), ZDIO_OPEN_READ);
+    }
+    catch (ZdException & x){
+      /* can't read ini file: missing/corrupt/permissions? */
+    }
+
+    InsureSessionStructure();
+    SetErrorReportDestination(GetTechnicalSupportEmail());
+  }
+  catch (ZdException& x) {
+    x.AddCallpath("Setup()", "SaTScanToolkit");
+    throw;
+  }
+}
+
+/** Returns referance to toolkit. */
+SaTScanToolkit & GetToolkit() {
+  return dynamic_cast<SaTScanToolkit&>(BasisGetToolkit());
+}
+
---
+++ calculation/Toolkit.h
@@ -0,0 +1,48 @@
+//*****************************************************************************
+#ifndef __SaTScanToolkit_H
+#define __SaTScanToolkit_H
+//*****************************************************************************
+#include "DBFFile.h"
+
+class SaTScanToolkit : public BToolkit {
+  private:
+    // system file
+    static const char         * gsSystemIniFileName;
+    static const char         * gsHistoryFileNameProperty;
+    static const char         * gsLoggingProperty;
+    static const char         * gsSaTScanWebSiteProperty;
+    static const char         * gsSaTScanLicenceWebSiteProperty;
+    static const char         * gsSubstantiveSupportEmailProperty;
+    static const char         * gsTechnicalSupportEmailProperty;
+
+    // default defines
+    static const char         * gsDefaultRunHistoryFileName;
+    static const char         * gsDefaultSaTScanWebSite;
+    static const char         * gsDefaultSaTScanLicenceWebSite;
+    static const char         * gsDefaultSubstantiveSupportEmail;
+    static const char         * gsDefaultTechnicalSupportEmail;
+
+    ZdString                    gsSystemFileName;
+
+    bool                        InsureLogRunHistory();
+    bool                        InsureRunHistoryFileName();
+    bool                        InsureSessionProperty(const char * sSessionProperty, const char * sDefaultValue);
+    void                        InsureSessionStructure();
+    void                        Setup();
+
+  public:
+    SaTScanToolkit();
+    virtual ~SaTScanToolkit();
+
+   const char                 * GetLicenceWebSite() /*const*/;
+   bool                         GetLogRunHistory() /*const*/;
+   const char                 * GetRunHistoryFileName() /*const*/;
+   const char                 * GetSubstantiveSupportEmail() /*const*/;
+   const char                 * GetTechnicalSupportEmail() /*const*/;
+   const char                 * GetWebSite() /*const*/;
+};
+
+SaTScanToolkit & GetToolkit();
+
+//*****************************************************************************
+#endif
