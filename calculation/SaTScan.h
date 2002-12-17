0103cf1bd524dec13677a32f50e682d104cceaaf
Scott Hostovich <hostovic@imsweb.com>
2002-12-17 21:50:27 +0000

    * defined constants for average number of days in years and months * removed obsolete defines and declarations * documented defines

--- calculation/SaTScan.h
+++ calculation/SaTScan.h
@@ -22,71 +22,37 @@
  **********************************************************************/
 
 /** Current version information. */ 
-#define VERSION_NUMBER "3.0"
+#define VERSION_NUMBER "3.1 beta"
 #define VERSION_DATE "October 17, 2002"
-
 /** New defines for ellipsoid calculations. */
 #define MAXELLIPSESHAPES 10
+/** PI declaration - this is now defined in math.h but changing may cause
+    precision to differ and the programs results. */
 #define PI 3.1415926535897932384626433832795028841972
-
-/** Maximum number of time intervals allowed */
-/** Moved from montecarlo.c and cluster.c KR-6/20/97 */
-#define MAXTIMEINTERVALS 400 /*KR-6/20/97 120  */
-
-/** Starting size for timesorted[] (in % of MaxCircleSize) */
-#define TS_START  200
-/** Growth increment for timesorted[] (in % of MaxCircleSize) */
-#define TS_GROW   20
-
-/** Max. length of a line in an input file */
+/** Average number of days in year and month. */
+#define AVERAGE_DAYS_IN_YEAR 364.2425
+#define AVERAGE_DAYS_IN_MONTH (364.2425/12)
+/** Maximum length of a line in input file (scanf). */
 #define MAX_LINESIZE 4096
+/** Maximum string length - used in static allocating of character arrays. */
+#define MAX_STR_LEN 150
+/** Maximum string length - used in static allocating of character arrays
+    for reading file infomation in scanf format. */
 #define MAX_LINEITEMSIZE 512
-
-/** Growth increment for TractInfo in tinfo.c */
+/** Growth increment for GInfo in GridTractCoordinates.cpp */
 #define TI_GROW 100
-
 /** Number of clusters to rank */
 #define NUM_RANKED 500  //KR-980615 100
-
-/*#if (sizeof(Julian) == sizeof(long))              /* "Infinite" time value */
-/*#define MAX_TIME  LONG_MAX
-#elif (sizeof(Julian) == sizeof(int))
-#define MAX_TIME  INT_MAX
-#elif (sizeof(Julian) == sizeof(short))
-#define MAX_TIME  SHRT_MAX
-#endif
-*/
-#define MAX_TIME LONG_MAX
-
-typedef long            count_t;                   /* used for case counts */
-typedef double          measure_t;                    /* used for measures */
-typedef /*short*/int    tract_t;     /* used for tract indeces (not names) */
-typedef unsigned short ushort_tract_t; /* used to create and reference sorted array */
-
-//typedef struct {                                           /* cluster data */
-//   double    cd_ratio;        /* likelihood ratio              */
-//   count_t   cd_ncases;       /* number of cases               */
-//   tract_t   cd_center;       /* center of cluster             */
-//   tract_t   cd_ntracts;      /* number of tracts              */
-//   Julian    cd_clusterstart; /* start time of cluster         */
-//   Julian    cd_clusterend;   /* end time of cluster           */
-//   measure_t cd_measure;      /* measure of cluster            */
-//   BOOL      cd_bClusterInit; /* Has cluster been initialized? */
-//   BOOL      cd_bClusterSet;  /* Has cluster been set?         */
-//   int       cd_nClusterType; /* Type of cluster               */
-//} cluster_t;
-
-/*KR tract_t GetNeighbor(tract_t t, unsigned nearness); (1/14/97) */
-/*KR void    FatalError(char *s);                       (1/14/97) */
-
-void ShowStackAvail(); /*KR-7/23/97*/
-
-#define MAX_STR_LEN 150//150
-/*#define PARAMFILENAME "STSDOS.TXT"*/
-#define PARAMFILENAME "STSPARAM.PRM"
-enum { enSTRING, enINTEGER, enENUM, enFILENAME, enDATE, ebREPLICA };
-enum { NONE, YEAR=1, MONTH, DAY };
-typedef unsigned int UInt;
+/** case count type */
+typedef long            count_t;
+/** measure type */
+typedef double          measure_t;
+/** used for tract indeces */
+typedef int             tract_t;
+/** unsigned int type declaration */
+typedef unsigned int    UInt;
+/** date precision units */
+enum DatePrecisionType { NONE, YEAR=1, MONTH, DAY };
 //*****************************************************************************
 #endif
 
