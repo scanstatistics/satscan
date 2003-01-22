5b943986f530b99b87383b95424f215bb6886cf8
Scott Hostovich <hostovic@imsweb.com>
2003-01-22 20:41:06 +0000

    * Updated basis include declarations.

--- calculation/SaTScanBasis.h
+++ calculation/SaTScanBasis.h
@@ -5,8 +5,10 @@
 
 #ifndef __BATCH_COMPILE
 // defines go here for windows interface -- vcl dependent stuff
-  #define __USE_xbDlgImporter
+  #define __USE_xbZdFileImporter
   #define __USE_xbCSVFile
+  #define __USE_xbZdFileViewController
+  #define __USE_xbGridZdSingleFileModel
   #define __USE_xbDlgException
 #endif
 
