fd424a4e79a7431cbbb5f16549775a8e1d26579f
Scott Hostovich <hostovic@imsweb.com>
2003-02-26 22:26:39 +0000

    * direct references to parameter class variables replaced with getter functions * parameter functions Read and ValidateParameters now take   print direction as parameter

--- windows_application/SaTScanBatchMain.cpp
+++ windows_application/SaTScanBatchMain.cpp
@@ -32,23 +32,27 @@ int main(int argc, char *argv[]) {
     BasisSetToolkit(new SaTScanToolkit()); //Set toolkit
     ZdGetFileTypeArray()->AddElement(&(DBFFileType::GetDefaultInstance()));
     ConsolePrint.SatScanPrintf(GetToolkit().GetAcknowledgment(sMessage));
-    Parameters.SetPrintDirection(&ConsolePrint);
     if (argc < 2)
-      ZdGenerateException("No parameter file specified.\nusage: %s parameter file\n", "main(int,char*)", argv[0]);
+      ZdGenerateException("No parameter file specified.\nusage: %s \"parameter file name\"\n", "main(int,char*)", argv[0]);
     time(&RunTime); //get start time
-    Parameters.Read(argv[1]);
+    Parameters.Read(argv[1], ConsolePrint);
+    if (Parameters.GetErrorOnRead()) {
+      sMessage << ZdString::reset << "\nThe parameter file contains incorrect settings that prevent SaTScan from continuing.\n";
+      sMessage << "Please review above message(s) and modify parameter settings accordingly.";
+      ZdGenerateException(sMessage.GetCString(),"main(int,char*)");
+    }
     //Set run history attributes here
     Parameters.SetRunHistoryFilename(GetToolkit().GetRunHistoryFileName());
     Parameters.SetIsLoggingHistory(GetToolkit().GetLogRunHistory());
     //Convert maximum temporal cluster size to percentage of population.
     Parameters.ConvertMaxTemporalClusterSizeToType(PERCENTAGETYPE);
-    if (! Parameters.ValidateParameters()) {
-      sMessage << ZdString::reset << "The parameter file contains incorrect settings that prevent SaTScan from continuing.\n";
+    if (! Parameters.ValidateParameters(ConsolePrint)) {
+      sMessage << ZdString::reset << "\nThe parameter file contains incorrect settings that prevent SaTScan from continuing.\n";
       sMessage << "Please review above message(s) and modify parameter settings accordingly.";
       ZdGenerateException(sMessage.GetCString(),"main(int,char*)");
     }
 
-    switch (Parameters.m_nAnalysisType) {
+    switch (Parameters.GetAnalysisType()) {
       case PURELYSPATIAL        : pData = new CPurelySpatialData(&Parameters, &ConsolePrint); break;
       case PURELYTEMPORAL       : pData = new CPurelyTemporalData(&Parameters, &ConsolePrint); break;
       case SPACETIME            : pData = new CSpaceTimeData(&Parameters, &ConsolePrint); break;
@@ -56,42 +60,39 @@ int main(int argc, char *argv[]) {
       default                   : ZdGenerateException("Invalid Analysis Type Encountered.", "main(int,char*)");
     };
     pData->ReadDataFromFiles();
-    switch (Parameters.m_nAnalysisType) {
-      case PURELYSPATIAL        : if (Parameters.m_nRiskFunctionType == STANDARDRISK)
+    switch (Parameters.GetAnalysisType()) {
+      case PURELYSPATIAL        : if (Parameters.GetRiskType() == STANDARDRISK)
                                     pAnalysis = new CPurelySpatialAnalysis(&Parameters, pData, &ConsolePrint);
-                                  else if (Parameters.m_nRiskFunctionType == MONOTONERISK)
+                                  else if (Parameters.GetRiskType() == MONOTONERISK)
                                     pAnalysis = new CPSMonotoneAnalysis(&Parameters, pData, &ConsolePrint);
                                   break;
       case PURELYTEMPORAL       : pAnalysis = new CPurelyTemporalAnalysis(&Parameters, pData, &ConsolePrint);
                                   break;
-      case SPACETIME            : if (Parameters.m_bIncludePurelySpatial && Parameters.m_bIncludePurelyTemporal)
+      case SPACETIME            : if (Parameters.GetIncludePurelySpatialClusters() && Parameters.GetIncludePurelyTemporalClusters())
                                     pAnalysis = new C_ST_PS_PT_Analysis(&Parameters, pData, &ConsolePrint);
-                                  else if (Parameters.m_bIncludePurelySpatial)
+                                  else if (Parameters.GetIncludePurelySpatialClusters())
                                     pAnalysis = new C_ST_PS_Analysis(&Parameters, pData, &ConsolePrint);
-                                  else if (Parameters.m_bIncludePurelyTemporal)
+                                  else if (Parameters.GetIncludePurelyTemporalClusters())
                                     pAnalysis = new C_ST_PT_Analysis(&Parameters, pData, &ConsolePrint);
                                   else
                                     pAnalysis = new CSpaceTimeAnalysis(&Parameters, pData, &ConsolePrint);
                                   break;
-      case PROSPECTIVESPACETIME : if (Parameters.m_bIncludePurelySpatial && Parameters.m_bIncludePurelyTemporal)
+      case PROSPECTIVESPACETIME : if (Parameters.GetIncludePurelySpatialClusters() && Parameters.GetIncludePurelyTemporalClusters())
                                     pAnalysis = new C_ST_PS_PT_Analysis(&Parameters, pData, &ConsolePrint);
-                                  else if (Parameters.m_bIncludePurelySpatial)
+                                  else if (Parameters.GetIncludePurelySpatialClusters())
                                     pAnalysis = new C_ST_PS_Analysis(&Parameters, pData, &ConsolePrint);
-                                  else if (Parameters.m_bIncludePurelyTemporal)
+                                  else if (Parameters.GetIncludePurelyTemporalClusters())
                                     pAnalysis = new C_ST_PT_Analysis(&Parameters, pData, &ConsolePrint);
                                   else
                                     pAnalysis = new CSpaceTimeAnalysis(&Parameters, pData, &ConsolePrint);
                                   break;
        default                  : ZdGenerateException("Invalid Analysis Type Encountered.", "main(int,char*)");
     }
-    if (! pAnalysis->Execute(RunTime)) {
-      sMessage << "\nProblem(s) occurred that caused the analysis to stop.\n";
-      ZdGenerateException(sMessage.GetCString(), "main(int,char*)");
-    }
-    else {
-      sMessage << "\nSaTScan completed successfully.\nThe results have been written to: \n  %s\n\n";
-      ConsolePrint.SatScanPrintf(sMessage.GetCString(), Parameters.GetOutputFileName().c_str());
-    }
+    if (! pAnalysis->Execute(RunTime))
+      ZdGenerateException("\nProblem(s) occurred that caused the analysis to stop.\n", "main(int,char*)");
+    else
+      ConsolePrint.SatScanPrintf("\nSaTScan completed successfully.\nThe results have been written to: \n  %s\n\n",
+                                 Parameters.GetOutputFileName().c_str());
 
     delete pAnalysis; pAnalysis=0;
     delete pData; pData=0;
