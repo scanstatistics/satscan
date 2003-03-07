cc671e2ba1575e4e2721c07beaaaf412b8db5832
Scott Hostovich <hostovic@imsweb.com>
2003-03-07 21:32:48 +0000

    * GetWord() function converted to class. A static variable was being used   to store word and could be problematic in mutti-threaded application.

--- calculation/utility/UtilityFunctions.cpp
+++ calculation/utility/UtilityFunctions.cpp
@@ -37,24 +37,12 @@ double CalcLogLikelihood(count_t n, measure_t u,
 //
 //		Latitude and Longitude values should be float, ie, real numbers
 //		with ranges [-90,90] and [-180,180] (in degrees) respectively.
-void ConvertFromLatLong(float Latitude, float Longitude, double* pCoords)
-{
-   float RADIUS = 6367; // Constant; radius of earth in km)
+void ConvertFromLatLong(float Latitude, float Longitude, std::vector<double>& vCoordinates) {
+  float RADIUS = 6367; // Constant; radius of earth in km)
 
-   try
-      {
-      // Need to allocate pCoords
-      //*pCoords = (double*)Smalloc(3 * sizeof(double), gpPrintDirection);
-
-      pCoords[0] = (double)(RADIUS * cos(Longitude*PI/180.0) * cos(Latitude*PI/180.0)); // x coordinate
-      pCoords[1] = (double)(RADIUS * sin(Longitude*PI/180.0) * cos(Latitude*PI/180.0)); // y coordinate
-      pCoords[2] = (double)(RADIUS * sin(Latitude*PI/180.0));														// z coordinate
-      }
-   catch (SSException & x)
-      {
-      x.AddCallpath("ConvertFromLatLong(float, float, double *)", "UtilityFunctions.cpp");
-      throw;
-      }
+  vCoordinates[0] = (double)(RADIUS * cos(Longitude*PI/180.0) * cos(Latitude*PI/180.0)); // x coordinate
+  vCoordinates[1] = (double)(RADIUS * sin(Longitude*PI/180.0) * cos(Latitude*PI/180.0)); // y coordinate
+  vCoordinates[2] = (double)(RADIUS * sin(Latitude*PI/180.0));														// z coordinate
 }
 
 void ConvertToLatLong(float* Latitude, float* Longitude, double* pCoords)
@@ -105,6 +93,95 @@ double GetDuczmalCorrection(double dEllipseShape) {
   return ( 4*dEllipseShape/(pow(dEllipseShape + 1, 2)) );
 }
 
+
+/** constructor */
+StringParser::StringParser(const ZdString & sSource) : gsSource(sSource) {
+  gwCurrentWordIndex = -1;
+  giSizeOfWordBuffer = MAX_LINEITEMSIZE;
+  gpWord = new char[MAX_LINEITEMSIZE];
+  gpWord[0] = 0;
+}
+
+/** destructor */
+StringParser::~StringParser(){
+  try {
+    delete[] gpWord;
+  }
+  catch (...){}  
+}
+
+/** Returns whether string has words. */
+bool StringParser::HasWords() {
+  return GetWord(0) != 0;
+}
+
+/** Returns number of words in string -- this could be better  */
+int StringParser::GetNumberWords() {
+  int   iWords=0;
+
+  while (GetWord(iWords) != 0)
+       ++iWords;
+  return iWords;
+}
+
+/** Returns wWordIndex + 1 'th word in associated string.
+    If wWordIndex is greater than number of words, NULL pointer returned. */
+const char * StringParser::GetWord(short wWordIndex) {
+  int           inwd, wdlen;
+  short         w = wWordIndex;
+  const char  * cp = gsSource.GetCString();
+  const char  * cp2;
+
+  //short cut if this word has already been read
+  if (wWordIndex == gwCurrentWordIndex)
+    return gpWord;
+
+  /* ignore spaces at start of line */
+  while(isspace(*cp)) cp++;
+
+  /* find start of word */
+  inwd = !isspace(*cp);
+  while (*cp != '\0' && (w > 0 || !inwd)) {
+       if (inwd == !!(isspace(*cp))) { /* entered or exited a word */
+         inwd = !inwd;
+         if (inwd) /* if entered a word, count it */
+           if (--w == 0)
+             break;
+       }
+       cp++; /* next character */
+  }
+
+  /* handle underflow */
+  if (*cp == '\0')
+     return 0;
+
+  /* find end of word */
+  cp2 = cp + 1;
+  while (!isspace(*cp2)) cp2++;
+  wdlen = cp2 - cp;
+  if (wdlen > giSizeOfWordBuffer) {
+    delete[] gpWord; gpWord=0;
+    gpWord =  new char[wdlen];
+    gpWord[0] = 0;
+  }
+  memcpy(gpWord, cp, wdlen);
+  gpWord[wdlen] = '\0';
+  gwCurrentWordIndex = wWordIndex; 
+  return gpWord;
+}
+
+/** Resets current string index and appends new line character.
+    This function should be called whenever the string is modified
+    especially after ZdIO::ReadLine(). The GetWord() function uses
+    isspace() to parse string and could scan into invalid memory
+    without presence of newline. The data read routines used to use
+    fgets(), which retained newline characters, but ZdIO::ReadLine()
+    does not have this behavior.                                    */
+void StringParser::StringReloaded() {
+  ClearWordIndex();
+  const_cast<ZdString&>(gsSource).Append('\n');
+}
+
 /**********************************************************************
  Returns a pointer to the "num"-th whitespace-delimited word in a string
  (starting at word number 0).
@@ -112,11 +189,10 @@ double GetDuczmalCorrection(double dEllipseShape) {
  If there are not "num" words, NULL is returned.
  **********************************************************************/
 /*KR-6/20/97staticKR-6/20/97*/
-char* GetWord(char *s, int num, BasePrint *pPrintDirection)
-{
+const char* GetWord(const char *s, int num, BasePrint *pPrintDirection) {
    int inwd;
-   char *cp = s;
-   char *cp2;
+   const char *cp = s;
+   const char *cp2;
    int wdlen;
   // static int   buflen = 0;
    static char buf[MAX_LINESIZE]/*KR-6/22/97 = 0*/;
--- calculation/utility/UtilityFunctions.h
+++ calculation/utility/UtilityFunctions.h
@@ -7,11 +7,41 @@
 #define DEBUGLATLONG 0
 
 double 			CalcLogLikelihood(count_t n, measure_t u, count_t N, measure_t U);
-void 			ConvertFromLatLong(float Latitude, float Longitude, double* pCoords);
+void 			ConvertFromLatLong(float Latitude, float Longitude, std::vector<double>& vCoordinates);
 void 			ConvertToLatLong(float* Latitude, float* Longitude, double* pCoords);
 void 			DisplayVersion(FILE* fp, int nPos);
 double                  GetDuczmalCorrection(double dEllipseShape);
-char		      * GetWord(char *s, int num, BasePrint *pPrintDirection);
+const char	      * GetWord(const char *s, int num, BasePrint *pPrintDirection);
 
+/** The data read routines of CSaTScanData used to use function:
+    const char * GetWord(const char *s, int num, BasePrint *pPrintDirection)
+    but it relied on a static character array to store word and use as return
+    value. In a threaded enviroment, this could be trouble some.
+    This class wraps that functionality into an object.
+
+    Note: The routines could be designed to read from a ZdFile interface,
+          permitting direct reading from dBase or any other file type driver
+          we want to create. The reason this has not been implemented is:
+          1 - the overhead might cause an overall increase in time for data read rountines
+          2 - no one has asked for direct read for other file types
+          3 - there may be other concerns/problems as this hasn't been completely thought through */
+class StringParser {
+  private:
+    const ZdString    & gsSource;
+    char              * gpWord;
+    int                 giSizeOfWordBuffer;
+    short               gwCurrentWordIndex;
+
+  public:
+    StringParser(const ZdString & sSource);
+    ~StringParser();
+
+    void                ClearWordIndex() {gwCurrentWordIndex=-1;}
+    bool                HasWords();
+    int                 GetNumberWords();
+    const char        * GetWord(short wWordIndex);
+    const char        * GetString() const {return gsSource.GetCString();}
+    void                StringReloaded();
+};
 //*****************************************************************************
 #endif 
