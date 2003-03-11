56c8c4ecd00b2256d093d0ac07c27d52289e769f
Scott Hostovich <hostovic@imsweb.com>
2003-03-11 18:42:33 +0000

    * catch statement changed to base class exception type: ZdException

--- calculation/probability_model/BernoulliModel.cpp
+++ calculation/probability_model/BernoulliModel.cpp
@@ -29,133 +29,102 @@ bool CBernoulliModel::ReadData() {
   return true;
 }
 
-bool CBernoulliModel::CalculateMeasure()
-{
-   int i, j;
+bool CBernoulliModel::CalculateMeasure() {
+  int i, j;
 
-   try
-      {
-      gpPrintDirection->SatScanPrintf("Calculating expected number of cases\n");
-    
-      // Why allocated +1?  KR-980417
-      m_pData->m_pMeasure = (double**)Smalloc((m_pData->m_nTimeIntervals+1) * sizeof(measure_t *), gpPrintDirection);
-      for (i=0; i<m_pData->m_nTimeIntervals+1; i++)
-         m_pData->m_pMeasure[i] = 0;
-      for (i=0; i<m_pData->m_nTimeIntervals+1; i++)
-         {
-    	 m_pData->m_pMeasure[i] = (double*)Smalloc(m_pData->m_nTracts * sizeof(measure_t), gpPrintDirection);
-         if (! m_pData->m_pMeasure[i])
-            SSGenerateException("Could not allocate memory for m_pMeasure[].","CalculateMeasure()");
-         }
-    
-      m_pData->m_nTotalCases    = 0;
-      m_pData->m_nTotalControls = 0;
-      m_pData->m_nTotalMeasure  = 0;
-    
-      for (j=0; j<m_pData->m_nTracts; j++)
-      {
-        m_pData->m_nTotalCases    += m_pData->m_pCases[0][j];
-        m_pData->m_nTotalControls += m_pData->m_pControls[0][j];
-    
-        for (i=0; i<m_pData->m_nTimeIntervals/*+1*/; i++)
-        {
+  try {
+    gpPrintDirection->SatScanPrintf("Calculating expected number of cases\n");
+
+    // Why allocated +1?  KR-980417
+    m_pData->m_pMeasure = (double**)Smalloc((m_pData->m_nTimeIntervals+1) * sizeof(measure_t *), gpPrintDirection);
+    for (i=0; i<m_pData->m_nTimeIntervals+1; i++)
+       m_pData->m_pMeasure[i] = 0;
+    for (i=0; i<m_pData->m_nTimeIntervals+1; i++) {
+       m_pData->m_pMeasure[i] = (double*)Smalloc(m_pData->m_nTracts * sizeof(measure_t), gpPrintDirection);
+       if (! m_pData->m_pMeasure[i])
+         SSGenerateException("Could not allocate memory for m_pMeasure[].","CalculateMeasure()");
+    }
+
+    m_pData->m_nTotalCases    = 0;
+    m_pData->m_nTotalControls = 0;
+    m_pData->m_nTotalMeasure  = 0;
+
+    for (j=0; j<m_pData->m_nTracts; j++) {
+       m_pData->m_nTotalCases    += m_pData->m_pCases[0][j];
+       m_pData->m_nTotalControls += m_pData->m_pControls[0][j];
+       for (i=0; i<m_pData->m_nTimeIntervals/*+1*/; i++) {
           m_pData->m_pMeasure[i][j]  = m_pData->m_pCases[i][j] + m_pData->m_pControls[i][j];
-        }
-    
-        m_pData->m_nTotalMeasure += m_pData->m_pMeasure[0][j];
-        m_pData->m_pMeasure[i][j] = 0;
-    
-        // Check to see if total case or control values have wrapped
+       }
+       m_pData->m_nTotalMeasure += m_pData->m_pMeasure[0][j];
+       m_pData->m_pMeasure[i][j] = 0;
+
+       // Check to see if total case or control values have wrapped
         if (m_pData->m_nTotalCases < 0)
-        {
-          SSGenerateException("  Error: Total cases greater than maximum allowed.\n", "CBernoulliModel");
-          //fprintf(stderr, "  Error: Total cases greater than maximum allowed.\n");
-          //gpPrintDirection->SatScanPrintWarning("  Error: Total cases greater than maximum allowed.\n");
-          //return false;
-        }
-    
+          SSGenerateException("Error: Total cases greater than maximum allowed.\n", "CBernoulliModel");
         if (m_pData->m_nTotalControls < 0)
-        {
-          SSGenerateException("  Error: Total controls greater than maximum allowed.\n", "CBernoulliModel");
-          //fprintf(stderr, "  Error: Total controls greater than maximum allowed.\n");
-          //gpPrintDirection->SatScanPrintWarning("  Error: Total controls greater than maximum allowed.\n");
-          //return false;
-        }
-    
-      }
-    
-      if (m_pData->m_nTotalControls == 0)
-      {
-        SSGenerateException("  Error: No controls found in input data.\n", "CBernoulliModel");
-        //gpPrintDirection->SatScanPrintWarning("  Error: No controls found in input data.\n");
-        //return false;
-      }
-    
-      m_pData->m_nTotalPop = m_pData->m_nTotalMeasure;
-      }
-   catch (SSException & x)
-      {
-      x.AddCallpath("CalculateMeasure()", "CBernoulliModel");
-      throw;
-      }
-   return true;
+          SSGenerateException("Error: Total controls greater than maximum allowed.\n", "CBernoulliModel");
+    }
+
+    if (m_pData->m_nTotalControls == 0)
+      SSGenerateException("Error: No controls found in input data.\n", "CBernoulliModel");
+
+    m_pData->m_nTotalPop = m_pData->m_nTotalMeasure;
+  }
+  catch (ZdException &x) {
+    x.AddCallpath("CalculateMeasure()","CBernoulliModel");
+    throw;
+  }
+  return true;
 }
 
-double CBernoulliModel::GetLogLikelihoodForTotal() const
-{
-   count_t   N = m_pData->m_nTotalCases;
-   measure_t U = m_pData->m_nTotalMeasure;
-   return N*log(N/U) + (U-N)*log((U-N)/U);
+double CBernoulliModel::GetLogLikelihoodForTotal() const {
+  count_t   N = m_pData->m_nTotalCases;
+  measure_t U = m_pData->m_nTotalMeasure;
+  
+  return N*log(N/U) + (U-N)*log((U-N)/U);
 }
 
-double CBernoulliModel::CalcLogLikelihood(count_t n, measure_t u)
-{
-   double    nLogLikelihood;
-   count_t   N = m_pData->m_nTotalCases;
-   measure_t U = m_pData->m_nTotalMeasure;
+double CBernoulliModel::CalcLogLikelihood(count_t n, measure_t u) {
+  double    nLogLikelihood;
+  count_t   N = m_pData->m_nTotalCases;
+  measure_t U = m_pData->m_nTotalMeasure;
 
-   double    nLL_A = 0.0;
-   double    nLL_B = 0.0;
-   double    nLL_C = 0.0;
-   double    nLL_D = 0.0;
+  double    nLL_A = 0.0;
+  double    nLL_B = 0.0;
+  double    nLL_C = 0.0;
+  double    nLL_D = 0.0;
 
-   if (n != 0)
-     nLL_A = n*log(n/u);
-
-   if (n != u)
-     nLL_B = (u-n)*log(1-(n/u));
-
-   if (N-n != 0)
-     nLL_C = (N-n)*log((N-n)/(U-u));
-
+  if (n != 0)
+    nLL_A = n*log(n/u);
+  if (n != u)
+    nLL_B = (u-n)*log(1-(n/u));
+  if (N-n != 0)
+    nLL_C = (N-n)*log((N-n)/(U-u));
    if (N-n != U-u)
-     nLL_D = ((U-u)-(N-n))*log(1-((N-n)/(U-u)));
+    nLL_D = ((U-u)-(N-n))*log(1-((N-n)/(U-u)));
 
-   nLogLikelihood = nLL_A + nLL_B + nLL_C + nLL_D;
+  nLogLikelihood = nLL_A + nLL_B + nLL_C + nLL_D;
 
-   return (nLogLikelihood);
+  return (nLogLikelihood);
 }
 
-double CBernoulliModel::CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster)
-{
-   double    nLogLikelihood = 0;
-   count_t   n;
-   measure_t u;
+double CBernoulliModel::CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster) {
+  double    nLogLikelihood = 0;
+  count_t   n;
+  measure_t u;
 
-   for (int i=0; i<PSMCluster.m_nSteps; i++)
-     {
+  for (int i=0; i<PSMCluster.m_nSteps; i++) {
      n = PSMCluster.m_pCasesList[i];
      u = PSMCluster.m_pMeasureList[i];
-
      if (n != 0  && n != u)
        nLogLikelihood += n*log(n/u) + (u-n)*log(1-(n/u));
      else if (n == 0)
        nLogLikelihood += (u-n)*log(1-(n/u));
      else if (n == u)
        nLogLikelihood += n*log(n/u);
-     }
+  }
 
-   return nLogLikelihood;
+  return nLogLikelihood;
 }
 
 void CBernoulliModel::MakeData(int iSimulationNumber)
@@ -224,7 +193,7 @@ void CBernoulliModel::MakeData(int iSimulationNumber)
       }
       free(RandCounts);
       }
-   catch (SSException & x)
+   catch (ZdException & x)
       {
       free(RandCounts);
       x.AddCallpath("MakeData()", "CBernoulliModel");
@@ -262,7 +231,7 @@ count_t * CBernoulliModel::MakeDataB(count_t nTotalCounts, count_t* RandCounts)
     		}
       }	//end of for(i=0...)
       }
-   catch (SSException & x)
+   catch (ZdException & x)
       {
       x.AddCallpath("MakeDataB(count_t, count_t *)", "CBernoulliModel");
       throw;
@@ -270,18 +239,15 @@ count_t * CBernoulliModel::MakeDataB(count_t nTotalCounts, count_t* RandCounts)
    return(RandCounts);
 }
 
-double CBernoulliModel::GetPopulation(int m_iEllipseOffset, tract_t nCenter, tract_t nTracts,
-                                      int nStartInterval, int nStopInterval)
-{
-   double  nPop = 0.0;
-   count_t nNeighbor;
+double CBernoulliModel::GetPopulation(int m_iEllipseOffset, tract_t nCenter, tract_t nTracts, int nStartInterval, int nStopInterval) {
+  double  nPop = 0.0;
+  count_t nNeighbor;
 
-   for (int i=1; i<=nTracts; i++)
-      {
-      nNeighbor = m_pData->GetNeighbor(m_iEllipseOffset, nCenter, i);
-      nPop += m_pData->m_pMeasure[nStartInterval][nNeighbor] - m_pData->m_pMeasure[nStopInterval][nNeighbor];
-      }
+  for (int i=1; i<=nTracts; i++) {
+     nNeighbor = m_pData->GetNeighbor(m_iEllipseOffset, nCenter, i);
+     nPop += m_pData->m_pMeasure[nStartInterval][nNeighbor] - m_pData->m_pMeasure[nStopInterval][nNeighbor];
+  }
 
-   return nPop;
+  return nPop;
 }
 
