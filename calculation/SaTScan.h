9619a4fbe3d49f58a16c4925ecc1ec34a5567297
Scott Hostovich <hostovic@imsweb.com>
2003-03-25 14:01:02 +0000

    no message

--- calculation/SaTScan.h
+++ calculation/SaTScan.h
@@ -22,7 +22,7 @@
  **********************************************************************/
 
 /** Current version information. */ 
-#define VERSION_NUMBER "3.1.0 beta 2"
+#define VERSION_NUMBER "3.1.0 RC 2"
 #define VERSION_DATE "Not Released"
 /** New defines for ellipsoid calculations. */
 #define MAXELLIPSESHAPES 10
