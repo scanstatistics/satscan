0355978d72d6ff544b6c26d87294c5c36c48fb42
Scott Hostovich <hostovic@imsweb.com>
2003-07-17 18:54:52 +0000

    * include for download progress

--- windows_application/stsSaTScan.h
+++ windows_application/stsSaTScan.h
@@ -1,8 +1,10 @@
 //---------------------------------------------------------------------------
-#ifndef __SatScan_H
-#define __SatScan_H
+#ifndef __stsSatScan_H
+#define __stsSatScan_H
 //---------------------------------------------------------------------------
 #include <vcl.h>
+#include "stsFrmDownloadProgress.h" // must be included here, prior to 
+				    // zd header defining SOCKET
 #include "SaTScan.h"
 #include <Basis540.h>
 #include "DBFFile.h"
