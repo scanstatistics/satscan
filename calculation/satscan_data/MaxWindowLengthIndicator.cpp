70d106ff0e37a0e5ddc0aedc1e2bb508472779d3
Scott Hostovich <hostovic@imsweb.com>
2003-08-01 19:11:15 +0000

    * classes that dictate temporal window length of scanned windows

---
+++ calculation/satscan_data/MaxWindowLengthIndicator.cpp
@@ -0,0 +1,48 @@
+//---------------------------------------------------------------------------
+#include "SaTScan.h"
+#pragma hdrstop
+#include "MaxWindowLengthIndicator.h"
+#include "SaTScanData.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+/** constructor */
+FixedMaxWindowLengthIndicator::FixedMaxWindowLengthIndicator(CSaTScanData & Data)
+                             :AbstractMaxWindowLengthIndicator(), giMaxWindowLength(Data.m_nIntervalCut) {}
+
+/** destructor */
+FixedMaxWindowLengthIndicator::~FixedMaxWindowLengthIndicator() {}
+
+
+/** constructor */
+ProspectiveMaxWindowLengthIndicator::ProspectiveMaxWindowLengthIndicator(CSaTScanData & Data)
+                                   :AbstractMaxWindowLengthIndicator() {
+  int iMaxEndWindow, iWindowEnd, iIntervalCut, lTimeBetween;
+
+
+  if (Data.GetParameters().GetMaximumTemporalClusterSizeType() == TIMETYPE)
+    ZdGenerateException("For a maximum special cluster size defined as a fixed distance\n"
+                        "you should use FixedMaxWindowLengthIndicator.","constructor()");
+
+  gvMaxWindowLengths.push_back(0); //dummy entry
+
+  //prospective analyses always use time range class, provided we are doing replications
+  if (Data.GetParameters().GetIsProspectiveAnalysis() && Data.GetParameters().GetNumReplicationsRequested() > 0) {
+    iMaxEndWindow = Data.m_nTimeIntervals;
+    for (iWindowEnd=Data.m_nProspectiveIntervalStart; iWindowEnd <= iMaxEndWindow; ++iWindowEnd) {
+       lTimeBetween = floor(TimeBetween(Data.GetStudyPeriodStartDate(),
+                                        Data.GetTimeIntervalStartTimes()[iWindowEnd] - 1,
+                                        Data.GetParameters().GetTimeIntervalUnitsType()));
+       lTimeBetween *= Data.GetParameters().GetMaximumTemporalClusterSize() / 100.0;
+       iIntervalCut = floor(lTimeBetween / Data.GetParameters().GetTimeIntervalLength());
+       gvMaxWindowLengths.push_back(iIntervalCut);
+    }
+  }
+  gitr = gvMaxWindowLengths.begin();
+}
+
+/** destructor */
+ProspectiveMaxWindowLengthIndicator::~ProspectiveMaxWindowLengthIndicator() {}
+
+
+ 
---
+++ calculation/satscan_data/MaxWindowLengthIndicator.h
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------
+#ifndef __MaxWindowLengthAssessor_H
+#define __MaxWindowLengthAssessor_H
+//---------------------------------------------------------------------------
+#include <vector>
+
+class CSaTScanData;
+
+class AbstractMaxWindowLengthIndicator {
+  public:
+    AbstractMaxWindowLengthIndicator() {}
+    virtual ~AbstractMaxWindowLengthIndicator() {}
+
+    virtual int                         GetNextWindowLength() = 0;
+    virtual void                        Reset() {}
+};
+
+class FixedMaxWindowLengthIndicator : public AbstractMaxWindowLengthIndicator {
+  private:
+    int                                 giMaxWindowLength;
+
+  public:
+    FixedMaxWindowLengthIndicator(CSaTScanData & Data);
+    virtual ~FixedMaxWindowLengthIndicator();
+
+    virtual int                         GetNextWindowLength() {return giMaxWindowLength;}
+};
+
+class ProspectiveMaxWindowLengthIndicator : public AbstractMaxWindowLengthIndicator {
+  private:
+    std::vector<int>                    gvMaxWindowLengths;
+    std::vector<int>::const_iterator    gitr;
+
+  public:
+    ProspectiveMaxWindowLengthIndicator(CSaTScanData & Data);
+    virtual ~ProspectiveMaxWindowLengthIndicator();
+
+    virtual int                         GetNextWindowLength() {++gitr; return *(gitr);}
+    virtual void                        Reset() {gitr = gvMaxWindowLengths.begin();}
+};
+#endif
