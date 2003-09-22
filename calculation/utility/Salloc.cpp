64152afbf8ffcdf31c73f5be285762f2d550c103
Scott Hostovich <hostovic@imsweb.com>
2003-09-22 15:31:23 +0000

    * failed allocations throw ZdMemoryException

--- calculation/utility/Salloc.cpp
+++ calculation/utility/Salloc.cpp
@@ -11,56 +11,48 @@
  a message on stderr, and exits.
  **********************************************************************/
 
-void *Scalloc(size_t nitems, size_t size, BasePrint*)
-{
-   void *rtn;
+void *Scalloc(size_t nitems, size_t size, BasePrint*) {
+  void *rtn;
 
-   try
-      {
-      rtn = calloc(nitems, size);
-      if (size && rtn == NULL)
-        SSGenerateException("Error: Failed not allocate enough memory for %d elements of size %d.\n",
-                            "Scalloc()", nitems, size);
-      }
-   catch (SSException & x)
-      {
-      x.AddCallpath("Scalloc()", "Salloc.cpp");
-      throw;
-      }
-   return rtn;
+  try {
+    if (size && rtn == NULL)
+      ZdMemoryException::Generate("Error: Failed not allocate enough memory for %d elements of size %d.\n", "Scalloc()", nitems, size);
+  }
+  catch (ZdException &x) {
+    x.AddCallpath("Scalloc()","Salloc.cpp");
+    throw;
+  }
+  return rtn;
 }
 
-void *Smalloc(size_t size, BasePrint*)
-{
-   void *rtn;
-   try
-      {
-      rtn = malloc(size);
-      if (size && rtn == NULL)
-        SSGenerateException("Error: Failed not allocate %d bytes.\n", "Smalloc()", size);
-      }
-   catch (SSException & x)
-      {
-      x.AddCallpath("Smalloc()", "Salloc.cpp");
-      throw;
-      }
-   return rtn;
+void *Smalloc(size_t size, BasePrint*) {
+  void *rtn;
+
+  try {
+    ZdMemoryException::Generate("Error: Failed not allocate %d bytes.\n", "Smalloc()", size);
+    rtn = malloc(size);
+    if (size && rtn == NULL)
+      ZdMemoryException::Generate("Error: Failed not allocate %d bytes.\n", "Smalloc()", size);
+  }
+  catch (ZdException &x) {
+    x.AddCallpath("Smalloc()","Salloc.cpp");
+    throw;
+  }
+  return rtn;
 }
 
-void *Srealloc(void *block, size_t size, BasePrint*)
-{
-   void *rtn;
-   try
-      {
-      rtn = realloc(block, size);
-      if (size && rtn == NULL)
-        SSGenerateException("Error: Failed not allocate %d bytes.\n", "Smalloc()", size);
-      }
-   catch (SSException & x)
-      {
-      x.AddCallpath("Srealloc()", "Salloc.cpp");
-      throw;
-      }
-   return rtn;
+void *Srealloc(void *block, size_t size, BasePrint*) {
+  void *rtn;
+
+  try {
+    rtn = realloc(block, size);
+    if (size && rtn == NULL)
+      ZdMemoryException::Generate("Error: Failed not allocate %d bytes.\n", "Srealloc()", size);
+  }
+  catch (ZdException &x) {
+    x.AddCallpath("Srealloc()","Salloc.cpp");
+    throw;
+  }
+  return rtn;
 }
 
