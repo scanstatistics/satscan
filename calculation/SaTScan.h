7cb7ba6cfbda614ddf4ca9b9ef95c5e2ba8f7005
Scott Hostovich <hostovic@imsweb.com>
2003-10-08 21:05:32 +0000

    *

--- calculation/SaTScan.h
+++ calculation/SaTScan.h
@@ -27,8 +27,8 @@
  This file contains header information for all SaTScan program files
  **********************************************************************/
 /** Current version information. */
-#define VERSION_ID "5"
-#define VERSION_NUMBER "4.0_Beta_5"
+#define VERSION_ID "6"
+#define VERSION_NUMBER "4.0_Beta_6"
 #define VERSION_DATE "Not Released"
 /** New defines for ellipsoid calculations. */
 #define MAXELLIPSESHAPES 10
