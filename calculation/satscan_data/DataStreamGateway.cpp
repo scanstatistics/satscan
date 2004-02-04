e7abcc6f1cace4508cffe35b551436e25b2e23b6
Scott Hostovich <hostovic@imsweb.com>
2004-02-04 17:25:31 +0000

    * abstracted gateway class

--- calculation/satscan_data/DataStreamGateway.cpp
+++ calculation/satscan_data/DataStreamGateway.cpp
@@ -4,13 +4,26 @@
 #include "DataStreamGateway.h"
 //---------------------------------------------------------------------------
 /** constructor */
-DataStreamGateway::DataStreamGateway() {}
+DataStreamGateway::DataStreamGateway() : AbtractDataStreamGateway() {}
 
 /** destructor */                            
 DataStreamGateway::~DataStreamGateway() {}
 
 /** adds data stream interface to list of interfaces */
 void DataStreamGateway::AddDataStreamInterface(DataStreamInterface & Interface) {
+  gInterface = Interface;
+}
+
+
+
+/** constructor */
+MultipleDataStreamGateway::MultipleDataStreamGateway() : AbtractDataStreamGateway() {}
+
+/** destructor */                            
+MultipleDataStreamGateway::~MultipleDataStreamGateway() {}
+
+/** adds data stream interface to list of interfaces */
+void MultipleDataStreamGateway::AddDataStreamInterface(DataStreamInterface & Interface) {
   gvDataStreams.push_back(Interface);
 }
 
--- calculation/satscan_data/DataStreamGateway.h
+++ calculation/satscan_data/DataStreamGateway.h
@@ -4,19 +4,45 @@
 //---------------------------------------------------------------------------
 #include "DataStreamInterface.h"
 
+class AbtractDataStreamGateway {
+  public:
+    AbtractDataStreamGateway() {}
+    virtual ~AbtractDataStreamGateway() {}
+
+    virtual void                               AddDataStreamInterface(DataStreamInterface & Interface) = 0;
+    inline virtual const DataStreamInterface & GetDataStreamInterface(size_t tStream=0) const = 0;
+    inline virtual DataStreamInterface       & GetDataStreamInterface(size_t tStream) = 0;
+    inline virtual size_t                      GetNumInterfaces() const = 0;
+};
+
 /** Collection of data stream interfaces. */
-class DataStreamGateway {
+class DataStreamGateway : public AbtractDataStreamGateway {
+  private:
+    DataStreamInterface         gInterface;
+
+  public:
+    DataStreamGateway();
+    virtual ~DataStreamGateway();
+
+    virtual void                               AddDataStreamInterface(DataStreamInterface & Interface);
+    inline virtual const DataStreamInterface & GetDataStreamInterface(size_t tStream=0) const {return gInterface;}
+    inline virtual DataStreamInterface       & GetDataStreamInterface(size_t tStream) {return gInterface;}
+    inline virtual size_t                      GetNumInterfaces() const {return 1;}
+};
+
+/** Collection of data stream interfaces. */
+class MultipleDataStreamGateway : public AbtractDataStreamGateway {
   private:
     std::vector<DataStreamInterface>         gvDataStreams;
 
   public:
-            DataStreamGateway();
-    virtual ~DataStreamGateway();
+    MultipleDataStreamGateway();
+    virtual ~MultipleDataStreamGateway();
 
-    void                        AddDataStreamInterface(DataStreamInterface & Interface);
-    const DataStreamInterface & GetDataStreamInterface(size_t tStream) const {return gvDataStreams[tStream];}
-    DataStreamInterface       & GetDataStreamInterface(size_t tStream) {return gvDataStreams[tStream];}
-    size_t                      GetNumInterfaces() const {return gvDataStreams.size();}
+    virtual void                               AddDataStreamInterface(DataStreamInterface & Interface);
+    inline virtual const DataStreamInterface & GetDataStreamInterface(size_t tStream=0) const {return gvDataStreams[tStream];}
+    inline virtual DataStreamInterface       & GetDataStreamInterface(size_t tStream) {return gvDataStreams[tStream];}
+    inline virtual size_t                      GetNumInterfaces() const {return gvDataStreams.size();}
 };
 #endif
  
