715758642d37034668dd8c57a47ec0aca8942a81
Scott Hostovich <hostovic@imsweb.com>
2004-02-04 18:11:40 +0000

    * made functions inline - though there called through function pointer   - there might be a way to make use of this still - no sure though

--- calculation/cluster/IncidentRate.cpp
+++ calculation/cluster/IncidentRate.cpp
@@ -2,26 +2,4 @@
 #pragma hdrstop
 #include "IncidentRate.h"
 
-bool LowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
-{
-  if (nMeasure == 0)
-    return false;
 
-  return (nCases*nTotalMeasure  < nMeasure*nTotalCases);
-}
-
-bool HighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
-{
- if (nMeasure == 0 || nCases < 2/*one case should not be considered a high rate*/)
-    return false;
-
-  return (nCases*nTotalMeasure  > nMeasure*nTotalCases);
-}
-
-bool HighOrLowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
-{
-  if (nMeasure == 0)
-    return false;
-
-  return (nCases*nTotalMeasure != nMeasure*nTotalCases);
-}
--- calculation/cluster/IncidentRate.h
+++ calculation/cluster/IncidentRate.h
@@ -7,8 +7,32 @@
 
 typedef bool(*RATE_FUNCPTRTYPE)(count_t, measure_t, count_t, measure_t);
 
-bool LowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
-bool HighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
-bool HighOrLowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
+inline bool LowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
+inline bool HighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
+inline bool HighOrLowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
+
+inline bool LowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
+{
+  if (nMeasure == 0)
+    return false;
+
+  return (nCases*nTotalMeasure  < nMeasure*nTotalCases);
+}
+
+inline bool HighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
+{
+ if (nMeasure == 0 || nCases < 2/*one case should not be considered a high rate*/)
+    return false;
+
+  return (nCases*nTotalMeasure  > nMeasure*nTotalCases);
+}
+
+inline bool HighOrLowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
+{
+  if (nMeasure == 0)
+    return false;
+
+  return (nCases*nTotalMeasure != nMeasure*nTotalCases);
+}
 //*****************************************************************************
 #endif
