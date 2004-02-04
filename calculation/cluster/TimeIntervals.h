f082f6fd395cfbbd28edf27bb5aafbcb97630d5f
Scott Hostovich <hostovic@imsweb.com>
2004-02-04 18:18:06 +0000

    * changed API - design now around deriving class that implements    functions CompareMeasures(...) and CompareClusters(...)

--- calculation/cluster/TimeIntervals.h
+++ calculation/cluster/TimeIntervals.h
@@ -5,15 +5,13 @@
 #include "SaTScan.h"
 #include "Parameters.h"
 #include "Cluster.h"
+#include "AbstractClusterData.h"
 
 class CMeasureList;
 class CCluster;
 class CSaTScanData;
 
 class CTimeIntervals {
-  public:
-    typedef void (CTimeIntervals::* COMPARECLUSTERS)(CCluster & Running, CCluster & TopShapeCluster, StreamDataContainer_t & StreamData);
-
   protected:
     int                         giNumIntervals;         /* number of total time intervals */
     int                         giMaxWindowLength;      /* maximum window length          */
@@ -35,14 +33,13 @@ class CTimeIntervals {
 
     virtual CTimeIntervals    * Clone() const = 0;
 
-    virtual void                CompareClusters(CCluster & Running, CCluster & TopShapeCluster, StreamDataContainer_t & StreamData) = 0;
-    virtual void                CompareClustersEx(CCluster & Running, CCluster & TopShapeCluster, StreamDataContainer_t & StreamData) = 0;
-    virtual void                CompareDataStreamClusters(CCluster & Running, CCluster & TopShapeCluster, StreamDataContainer_t & StreamData) = 0;
-    virtual void                CompareDataStreamClustersEx(CCluster & Running, CCluster & TopShapeCluster, StreamDataContainer_t & StreamData) = 0;
-    virtual void                ComputeBestMeasures(AbstractTemporalClusterStreamData * pStreamData, CMeasureList & MeasureList) = 0;
-    virtual void                Initialize() {/*stub - no action */}
     virtual count_t             GetCaseCountForTract(const CCluster & Cluster, tract_t tTract, count_t** pCases) const = 0;
     virtual measure_t           GetMeasureForTract(const CCluster & Cluster, tract_t tTract,  measure_t** pMeasure) const = 0;
+
+    virtual void                Initialize() {/*stub - no action */}
     virtual IncludeClustersType GetType() const = 0;
+
+    virtual void                CompareClusters(CCluster & Running, CCluster & TopShapeCluster) = 0;
+    virtual void                CompareMeasures(AbstractTemporalClusterData * pStreamData, CMeasureList * pMeasureList) = 0;
 };
 #endif
