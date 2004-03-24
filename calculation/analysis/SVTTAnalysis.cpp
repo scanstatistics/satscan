f93cd55bd65d6fadb63fec6983b5f06cefb764fc
Scott Hostovich <hostovic@imsweb.com>
2004-03-24 20:32:18 +0000

    * call to CalcSVTTLogLikelihoodRatio replaced with CalcSVTTLogLikelihood() * removed calculation  of loglikelihood ratio with regards to multiple data   streams from CalculateTopCluster()   - SVTT still needs further refining, no need to complicate with multiple     streams just yet

--- calculation/analysis/SVTTAnalysis.cpp
+++ calculation/analysis/SVTTAnalysis.cpp
@@ -51,7 +51,6 @@ void CSpatialVarTempTrendAnalysis::AllocateTopClustersObjects(const AbtractDataS
 const CCluster & CSpatialVarTempTrendAnalysis::CalculateTopCluster(tract_t tCenter, const AbtractDataStreamGateway & DataGateway) {
   int                   k;
   tract_t               i, iNumNeighbors;
-  CModel              & ProbModel(m_pData->GetProbabilityModel());
 
   try {
     gpTopShapeClusters->Reset(tCenter);
@@ -66,9 +65,8 @@ const CCluster & CSpatialVarTempTrendAnalysis::CalculateTopCluster(tract_t tCent
        iNumNeighbors = m_pData->GetNeighborCountArray()[k][tCenter];
        for (i=1; i <= iNumNeighbors; ++i) {
           thisCluster.AddNeighbor(m_pData->GetNeighbor(k, tCenter, i), DataGateway);
-          thisCluster.m_nRatio = 0;
-          for (size_t t=0; t < DataGateway.GetNumInterfaces(); ++t)
-            thisCluster.m_nRatio += gpLikelihoodCalculator->CalcSVTTLogLikelihoodRatio(t, &thisCluster, *(DataGateway.GetDataStreamInterface(t).GetTimeTrend()));
+          //TODO: -- Calculate loglikelihood ratio for all data streams.
+          thisCluster.m_nRatio = gpLikelihoodCalculator->CalcSVTTLogLikelihood(0, &thisCluster, *(DataGateway.GetDataStreamInterface(0).GetTimeTrend()));
           if (thisCluster.m_nRatio > TopShapeCluster.m_nRatio)
            TopShapeCluster = thisCluster;
        }
@@ -76,7 +74,7 @@ const CCluster & CSpatialVarTempTrendAnalysis::CalculateTopCluster(tract_t tCent
 
     // the ratio needs to be calculated for each circle/cylinder, instead of here !!!!
     CSVTTCluster & Cluster = (CSVTTCluster&)(gpTopShapeClusters->GetTopCluster());
-    Cluster.m_nRatio = Cluster.m_nLogLikelihood - gpLikelihoodCalculator->GetLogLikelihoodForTotal();
+    Cluster.m_nRatio -= gpLikelihoodCalculator->GetLogLikelihoodForTotal();
     Cluster.SetTimeTrend(m_pParameters->GetTimeIntervalUnitsType(), m_pParameters->GetTimeIntervalLength());
   }
   catch (ZdException &x) {
@@ -92,9 +90,10 @@ double CSpatialVarTempTrendAnalysis::MonteCarlo(const DataStreamInterface & Inte
   int           k;
   tract_t       i, j, iNumNeighbors;
   double        dMaximumLogLikelihoodRatio;
-  CModel      & ProbModel(m_pData->GetProbabilityModel());  
 
   try {
+    gpTopShapeClusters->SetTopClusters(CSVTTCluster(Interface, m_pData->GetNumTimeIntervals()));
+  
     //Iterate over circle/ellipse(s) - remember that circle is allows zero'th item.
     for (k=0; k <= m_pParameters->GetNumTotalEllipses(); k++) {
        CSVTTCluster thisCluster(Interface, m_pData->GetNumTimeIntervals());
@@ -107,14 +106,14 @@ double CSpatialVarTempTrendAnalysis::MonteCarlo(const DataStreamInterface & Inte
           iNumNeighbors = m_pData->GetNeighborCountArray()[k][i];	
           for (j=1; j <= iNumNeighbors; j++) {
              thisCluster.AddNeighbor(m_pData->GetNeighbor(k, i, j), Interface, 0);
-             thisCluster.m_nRatio = gpLikelihoodCalculator->CalcSVTTLogLikelihoodRatio(0, &thisCluster, *(Interface.GetTimeTrend()));
+             thisCluster.m_nRatio = gpLikelihoodCalculator->CalcSVTTLogLikelihood(0, &thisCluster, *(Interface.GetTimeTrend()));
              if (thisCluster.m_nRatio > TopShapeCluster.m_nRatio)
                TopShapeCluster = thisCluster;
           }
        }
     }
     //get copy of best cluster over all iterations
-    dMaximumLogLikelihoodRatio = gpTopShapeClusters->GetTopCluster().m_nRatio;
+    dMaximumLogLikelihoodRatio = gpTopShapeClusters->GetTopCluster().m_nRatio - gpLikelihoodCalculator->GetLogLikelihoodForTotal();
   }
   catch (ZdException &x) {
     x.AddCallpath("MonteCarlo()","CSpatialVarTempTrendAnalysis");
