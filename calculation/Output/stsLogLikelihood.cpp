a66bc076d6daf4effb88a4801be0f05cde44f9a9
Scott Hostovich <hostovic@imsweb.com>
2004-04-12 19:38:41 +0000

    * LoglikelihoodRecord class replaced by OutputRecord class * global constants of class static members * ...

--- calculation/Output/stsLogLikelihood.cpp
+++ calculation/Output/stsLogLikelihood.cpp
@@ -1,107 +1,42 @@
-// Adam J Vaughn
-// November 2002
-
+//***************************************************************************
 #include "SaTScan.h"
 #pragma hdrstop
-
+//***************************************************************************
 #include "stsLogLikelihood.h"
 
-const char *	LOG_LIKELIHOOD_FILE_EXT		= ".llr";
-
-// ============================================================================
-// storage class for a log likelihood record
-// ============================================================================
-
-LogLikelihoodRecord::LogLikelihoodRecord() : BaseOutputRecord() {
-   gdLogLikelihood = 0.0;
+/** class constructor */
+LogLikelihoodData::LogLikelihoodData(const CParameters& Parameters)
+                  :BaseOutputStorageClass(), gParameters(Parameters) {
+  try {
+    SetupFields();
+  }
+  catch (ZdException &x) {
+    x.AddCallpath("constructor","LogLikelihoodData");
+    throw;
+  }
 }
 
-LogLikelihoodRecord::LogLikelihoodRecord(const double dLikelihood) : BaseOutputRecord() {
-   gdLogLikelihood = dLikelihood;
-}
+/** class destructor */
+LogLikelihoodData::~LogLikelihoodData() {}
 
-LogLikelihoodRecord::~LogLikelihoodRecord() {
-}
+const char * LogLikelihoodData::LOG_LIKELIHOOD_FILE_EXT		= ".llr";
 
-ZdFieldValue LogLikelihoodRecord::GetValue(int iFieldNumber) {
-   ZdFieldValue fv;
-   
-   try {
-      if (iFieldNumber != 0)
-         ZdGenerateException("Index out of range!", "Error!");
-      
-      BaseOutputRecord::SetFieldValueAsDouble(fv, gdLogLikelihood);
-   }
-   catch (ZdException &x) {
-      x.AddCallpath("GetValue()", "LogLikelihoodRecord");
-      throw;
-   }
-   return fv;   
-}
-
-// ============================================================================
-// Output data model for loglikelihood output file types.
-// ============================================================================
-
-// constructor
-LogLikelihoodData::LogLikelihoodData(BasePrint *pPrintDirection, const CParameters& Parameters)
-                          : BaseOutputStorageClass(pPrintDirection), gParameters(Parameters) {
-   try {
-      Init();
-      Setup();
-   }
-   catch (ZdException &x) {
-      if(pPrintDirection) {
-         pPrintDirection->SatScanPrintWarning(x.GetErrorMessage());
-         pPrintDirection->SatScanPrintWarning("\nWarning - Unable to create data for Log Likelihood file.\n");
-      }
-   }
-}
-
-// destructor
-LogLikelihoodData::~LogLikelihoodData() {
-   try {
-   }
-   catch (...) { }
-}
-
-// add a new log likelihood record to the data
+// add a new log likelihood ratio record to the data
 // pre: none
 // post : adds a new record to the global vector
-void LogLikelihoodData::AddLikelihood(const double dLikelihood) {
-   LogLikelihoodRecord*	pRecord = 0;
-   
-   try {
-      pRecord = new LogLikelihoodRecord(dLikelihood);
-      BaseOutputStorageClass::AddRecord(pRecord);	
-   }
-   catch(ZdException &x) {
-      delete pRecord;	
-      gpPrintDirection->SatScanPrintWarning(x.GetErrorMessage());
-      gpPrintDirection->SatScanPrintWarning("\nWarning - Unable to record data for Log Likelihood output file.\n");
-   }			
-}
+void LogLikelihoodData::AddLikelihoodRatio(double dLikelihoodRatio) {
+  OutputRecord               * pRecord = 0;
 
-void LogLikelihoodData::Init() {
-}
-
-// internal setup function
-void LogLikelihoodData::Setup() {
-   try {
-      ZdString sTempName(gParameters.GetOutputFileName().c_str());
-      ZdString sExt(ZdFileName(gParameters.GetOutputFileName().c_str()).GetExtension());
-      if(sExt.GetLength()) 
-         sTempName.Replace(sExt, LOG_LIKELIHOOD_FILE_EXT);
-      else
-         sTempName << LOG_LIKELIHOOD_FILE_EXT;
-      gsFileName = sTempName;
-
-      SetupFields();
-   }
-   catch (ZdException &x) {
-      x.AddCallpath("Setup()", "LogLikelihoodData");
-      throw;
-   }
+  try {
+    pRecord = new OutputRecord(gvFields);
+    pRecord->GetFieldValue(0).AsDouble() = dLikelihoodRatio;
+    BaseOutputStorageClass::AddRecord(pRecord);
+  }
+  catch(ZdException &x) {
+    delete pRecord;
+    x.AddCallpath("AddLikelihoodRatio()","LogLikelihoodData");
+    throw;
+  }
 }
 
 // sets up the appropraite fields for a log likelihood output data model
@@ -112,9 +47,9 @@ void LogLikelihoodData::SetupFields() {
    
    try {
      if (gParameters.GetLogLikelihoodRatioIsTestStatistic())
-       ::CreateField(gvFields, TST_STAT_FIELD, ZD_NUMBER_FLD, 7, 2, uwOffset);
-     else  
-       ::CreateField(gvFields, LOG_LIKL_FIELD, ZD_NUMBER_FLD, 7, 2, uwOffset);
+       CreateField(gvFields, TST_STAT_FIELD, ZD_NUMBER_FLD, 7, 2, uwOffset);
+     else
+       CreateField(gvFields, LOG_LIKL_FIELD, ZD_NUMBER_FLD, 7, 2, uwOffset);
    }
    catch (ZdException &x) {
       x.AddCallpath("SetupFields()", "LogLikelihoodData");
--- calculation/Output/stsLogLikelihood.h
+++ calculation/Output/stsLogLikelihood.h
@@ -1,39 +1,25 @@
+//***************************************************************************
 #ifndef LOGLIKELIHOODDBF_H
 #define LOGLIKELIHOODDBF_H
-
+//***************************************************************************
 #include "stsOutputFileData.h"
 #include "Parameters.h"
 
-extern const char *	LOG_LIKELIHOOD_FILE_EXT;
-
-class LogLikelihoodRecord : public BaseOutputRecord {
-   private:
-      double gdLogLikelihood;
-   public:
-      LogLikelihoodRecord();
-      LogLikelihoodRecord(const double dLikelihood);
-      virtual ~LogLikelihoodRecord();
-
-      virtual bool GetFieldIsBlank(int iFieldNumber) { return false; }
-      virtual int GetNumFields() { return 1; }
-      virtual ZdFieldValue GetValue(int iFieldNumber);
-      
-      void	SetLogLikelihood(const double dLogLikelihood) { gdLogLikelihood = dLogLikelihood; }
-};
-
+/** Output data model for loglikelihood output file types. */
 class LogLikelihoodData : public BaseOutputStorageClass {
    private:
-      const CParameters       & gParameters;
-      
-      void	                Init();
-      void	                Setup();
-   protected:
       virtual void              SetupFields();
+
+   protected:
+      static const char       * LOG_LIKELIHOOD_FILE_EXT;
+      const CParameters       & gParameters;
+
    public:
-      LogLikelihoodData(BasePrint *pPrintDirection, const CParameters& Parameters);
+      LogLikelihoodData(const CParameters& Parameters);
       virtual ~LogLikelihoodData();
 
-      void	                AddLikelihood(const double dLikelihood);	
+      void	                AddLikelihoodRatio(double dLikelihoodRatio);
+      virtual const char      * GetOutputExtension() const {return LOG_LIKELIHOOD_FILE_EXT;}
 };
-
+//***************************************************************************
 #endif
