ff028d5c525c99eb042cf4dcc5e943e741e3c4e2
Scott Hostovich <hostovic@imsweb.com>
2004-06-22 19:02:35 +0000

    * implemented abstraction for calculating loglikelihood ratios with multiple   data streams   - AbstractLoglikelihoodRatioUnifier <- MultivariateUnifier                                                          <- AdjustmentUnifier

--- calculation/loglikelihood/LikelihoodCalculation.cpp
+++ calculation/loglikelihood/LikelihoodCalculation.cpp
@@ -3,35 +3,155 @@
 #pragma hdrstop
 //---------------------------------------------------------------------------
 #include "LikelihoodCalculation.h"
+#include "SaTScanData.h"
 
-/** constructor */
-AbstractLikelihoodCalculator::AbstractLikelihoodCalculator(count_t tTotalCases, measure_t tTotalMeasure)
-                             :gtTotalCasesInDataSet(tTotalCases), gtTotalMeasureInDataSet(tTotalMeasure) {
+/** class constructor */
+MultivariateUnifier::MultivariateUnifier(AreaRateType eScanningArea)
+                    :gdHighRateRatios(0), gdLowRateRatios(0),
+                     gbScanLowRates(eScanningArea == LOW || eScanningArea == HIGHANDLOW),
+                     gbScanHighRates(eScanningArea == HIGH || eScanningArea == HIGHANDLOW) {}
+
+/** Calculates loglikelihood ratio given parameter data; accumulating like high and low
+    rate separately. */
+void MultivariateUnifier::AdjoinRatio(AbstractLikelihoodCalculator& Calculator,
+                                      unsigned int iStream,
+                                      count_t tCases,
+                                      measure_t tMeasure,
+                                      count_t tTotalCases,
+                                      measure_t tTotalMeasure) {
+
+  if (iStream == 0)
+    Reset();
+                                          
+  if (gbScanLowRates && LowRate(tCases, tMeasure, tTotalCases, tTotalMeasure))
+    gdLowRateRatios += Calculator.CalcLogLikelihoodRatio(tCases, tMeasure, tTotalCases, tTotalMeasure);
+  if (gbScanHighRates && HighRate(tCases, tMeasure, tTotalCases, tTotalMeasure))
+    gdHighRateRatios += Calculator.CalcLogLikelihoodRatio(tCases, tMeasure, tTotalCases, tTotalMeasure);
+}
+
+/** Returns the largest calculated loglikelihood ratio by comparing summed ratios
+    that were for high rates to those that were for low rates. */
+double MultivariateUnifier::GetLoglikelihoodRatio() const {
+  return std::max(gdHighRateRatios, gdLowRateRatios);
+}
+
+/** Resets internal class members for another iteration of computing unified
+    log likelihood ratios.*/
+void MultivariateUnifier::Reset() {
+  gdHighRateRatios = gdLowRateRatios = 0;
+}
+
+/** class constructor */
+AdjustmentUnifier::AdjustmentUnifier(AreaRateType eScanningArea) : geScanningArea(eScanningArea) {
+  switch (eScanningArea) {
+    case LOW        : gfRateOfInterest = LowRate;       break;
+    case HIGHANDLOW : gfRateOfInterest = HighOrLowRate; break;
+    case HIGH       :
+    default         : gfRateOfInterest = HighRate;
+  };
+}
+
+/** Calculates loglikelihood ratio given parameter data. The calculated ratio
+    might be adjusted through multiplying by positive or negative one; based
+    upon comparing observed to expected cases. */
+void AdjustmentUnifier::AdjoinRatio(AbstractLikelihoodCalculator& Calculator,
+                                    unsigned int iStream,
+                                    count_t tCases,
+                                    measure_t tMeasure,
+                                    count_t tTotalCases,
+                                    measure_t tTotalMeasure) {
+                                    
+  if (iStream == 0)
+    Reset();
+  //check if rate of interest
+  if (gfRateOfInterest(tCases, tMeasure, tTotalCases, tTotalMeasure)) {
+    int iRiskEvaluation = (tCases - (Calculator.GetDataHub().GetMeasureAdjustment(iStream) * tMeasure) >= 0 ? 1 : -1);
+    gdRatio += iRiskEvaluation * Calculator.CalcLogLikelihoodRatio(tCases, tMeasure, tTotalCases, tTotalMeasure);
+  }
+}                                 
+
+/** Returns calculated loglikelihood ratio that is the sum of adjoined values.
+    Based upon scanning rate, returned value is adjusted such that if rate is:
+    high         ; no adjustment occurs
+    low          ; ratio * -1 is returned
+    high and low ; absolute value of ratio is returned */
+double AdjustmentUnifier::GetLoglikelihoodRatio() const {
+  switch (geScanningArea) {
+    case HIGHANDLOW : return std::fabs(gdRatio);
+    case LOW        : return gdRatio * -1;
+    default         : return gdRatio;
+  };
+}
+
+/** Resets internal class members for another iteration of computing unified
+    log likelihood ratios.*/
+void AdjustmentUnifier::Reset() {
+  gdRatio = 0;
+}
+
+/** constructor */                           
+AbstractLikelihoodCalculator::AbstractLikelihoodCalculator(const CSaTScanData& Data)
+                             :gData(Data),
+                              gtTotalCasesInDataSet(Data.GetTotalCases()),
+                              gtTotalMeasureInDataSet(Data.GetTotalMeasure()) {
   Init();
+  Setup();
 }
 
 /** destructor */
-AbstractLikelihoodCalculator::~AbstractLikelihoodCalculator() {}
+AbstractLikelihoodCalculator::~AbstractLikelihoodCalculator() {
+  try {
+  }
+  catch (...){}
+}
 
 /** Throws exception. Not implemented in base class */
 double AbstractLikelihoodCalculator::CalcLogLikelihoodRatioEx(count_t tCases,
                                                               measure_t tMeasure,
                                                               measure_t tMeasure2,
                                                               count_t tTotalCases,
-                                                              measure_t tTotalMeasure) {
+                                                              measure_t tTotalMeasure) const {
   ZdGenerateException("CalcLogLikelihoodRatioEx() not implementated.","AbstractLikelihoodCalculator");
   return 0;
 }
 
 /** Throws exception. Not implemented in base class */
-double AbstractLikelihoodCalculator::CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster) {
+double AbstractLikelihoodCalculator::CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster) const {
   ZdGenerateException("CalcMonotoneLogLikelihood() not implementated.","AbstractLikelihoodCalculator");
   return 0;
 }
 
 /** Throws exception. Not implemented in base class */
-double AbstractLikelihoodCalculator::CalcSVTTLogLikelihood(size_t tStream, CSVTTCluster* Cluster, const CTimeTrend& GlobalTimeTrend) {
+double AbstractLikelihoodCalculator::CalcSVTTLogLikelihood(size_t tStream, CSVTTCluster* Cluster, const CTimeTrend& GlobalTimeTrend) const {
   ZdGenerateException("CalcSVTTLogLikelihood() not implementated.","AbstractLikelihoodCalculator");
   return 0;
 }
 
+/** Returns reference to AbstractLoglikelihoodRatioUnifier object. Throw exception
+    if object not allocated. */
+AbstractLoglikelihoodRatioUnifier & AbstractLikelihoodCalculator::GetUnifier() const {
+  if (!gpUnifier)
+    ZdGenerateException("Log likelihood unifier not allocated.","GetUnifier()");
+  return *gpUnifier;
+}
+
+/** Internal class setup */
+void AbstractLikelihoodCalculator::Setup() {
+  try {
+    if (gData.GetParameters().GetNumDataStreams() > 1) {
+      switch (gData.GetParameters().GetMultipleDataStreamPurposeType()) {
+        case MULTIVARIATE :
+          gpUnifier = new MultivariateUnifier(gData.GetParameters().GetAreaScanRateType()); break;
+        case ADJUSTMENT :
+          gpUnifier = new AdjustmentUnifier(gData.GetParameters().GetAreaScanRateType()); break;
+        default :
+          ZdGenerateException("Unknown purpose for multiple data streams '%d'.","GetUnifier",
+                              gData.GetParameters().GetMultipleDataStreamPurposeType());
+      }
+    }
+  }
+  catch (ZdException &x) {
+    x.AddCallpath("Setup()","AbstractLikelihoodCalculator");
+    throw;
+  }
+}
--- calculation/loglikelihood/LikelihoodCalculation.h
+++ calculation/loglikelihood/LikelihoodCalculation.h
@@ -3,30 +3,109 @@
 #define __LikelihoodCalculation_H
 //*****************************************************************************
 #include "SaTScan.h"
+#include "Parameters.h"
+#include "IncidentRate.h"
+
+class AbstractLikelihoodCalculator; /** forward class declaration */
+
+/** Abstract base class for unifying calculated data stream loglikelihood ratios. */
+class AbstractLoglikelihoodRatioUnifier {
+  public:
+    AbstractLoglikelihoodRatioUnifier() {}
+    ~AbstractLoglikelihoodRatioUnifier() {}
+
+    virtual void        AdjoinRatio(AbstractLikelihoodCalculator& Calculator,
+                                    unsigned int iStream,
+                                    count_t tCases,
+                                    measure_t tMeasure,
+                                    count_t tTotalCases,
+                                    measure_t tTotalMeasure) = 0;
+    virtual double      GetLoglikelihoodRatio() const = 0;
+    virtual void        Reset() = 0;
+};
+
+/** Unifies calculated data stream loglikelihood ratios, in such a way, that if
+     scanning rate is:
+     - high or low  ; No special behavior occurs, call to GetLoglikelihoodRatio()
+                      returns all calculated loglikelihood ratios added together.
+     - high and low ; Like loglikelihood ratios are added together, call to
+                      GetLoglikelihoodRatio() returns the greater of summed
+                      loglikelihoods for high rates vs low rates. */
+class MultivariateUnifier : public AbstractLoglikelihoodRatioUnifier {
+   double       gdHighRateRatios;          /** loglikelihood ratios for high rates */
+   double       gdLowRateRatios;           /** loglikelihood ratios for low rates */
+   bool         gbScanHighRates;           /** indicates whether to scan for high rates */
+   bool         gbScanLowRates;            /** indicates whether to scan for low rates */
+
+  public:
+    MultivariateUnifier(AreaRateType eScanningArea);
+    ~MultivariateUnifier() {}
+
+    virtual void        AdjoinRatio(AbstractLikelihoodCalculator& Calculator,
+                                    unsigned int iStream,    
+                                    count_t tCases,
+                                    measure_t tMeasure,
+                                    count_t tTotalCases,
+                                    measure_t tTotalMeasure);
+    virtual double      GetLoglikelihoodRatio() const;
+    virtual void        Reset();
+};
+
+/** Unifies calculated data stream loglikelihood ratios, in such a way, that if
+     scanning rate is:
+     - high         ; Call to GetLoglikelihoodRatio() returns summed loglikelihood ratios.
+     - low          ; Call to GetLoglikelihoodRatio() returns summed loglikelihood ratios
+                      times negative one.
+     - high and low ; Call to GetLoglikelihoodRatio() returns the absolute value of summed
+                      loglikelihood ratios. */
+class AdjustmentUnifier : public AbstractLoglikelihoodRatioUnifier {
+   double               gdRatio;              /** summed log likelihood ratios */
+   RATE_FUNCPTRTYPE     gfRateOfInterest;     /** function pointer to scanning area funciton */
+   AreaRateType         geScanningArea;       /** type of area scanned for */
+
+  public:
+    AdjustmentUnifier(AreaRateType eScanningArea);
+    ~AdjustmentUnifier() {}
+
+    virtual void        AdjoinRatio(AbstractLikelihoodCalculator& Calculator,
+                                    unsigned int iStream,
+                                    count_t tCases,
+                                    measure_t tMeasure,
+                                    count_t tTotalCases,
+                                    measure_t tTotalMeasure);
+    virtual double      GetLoglikelihoodRatio() const;
+    virtual void        Reset();
+};
+
 
 class CPSMonotoneCluster; /** forward class declaration */
 class CSaTScanData;       /** forward class declaration */
 class CSVTTCluster;       /** forward class declaration */
 class CTimeTrend;         /** forward class declaration */
 
+/** Abstract interface for which to calculate log likelihoods and log likelihood ratios. */
 class AbstractLikelihoodCalculator {
   protected:
-    //double              gdLikelihoodForTotal;
-    count_t             gtTotalCasesInDataSet;
-    measure_t           gtTotalMeasureInDataSet;
-    
-    void                Init() {/*gdLikelihoodForTotal=0;*/}
+    const CSaTScanData                & gData;                    /** const reference to data hub */
+    count_t                             gtTotalCasesInDataSet;    /** total number of cases, in all data streams */
+    measure_t                           gtTotalMeasureInDataSet;  /** total number of expected cases, in all data streams */
+    AbstractLoglikelihoodRatioUnifier * gpUnifier;                /** log likelihood ratio unifier for multiple data streams */
+
+    void                                Init() {gpUnifier=0;}
+    void                                Setup();
 
   public:
-    AbstractLikelihoodCalculator(count_t tTotalCases, measure_t tTotalMeasure);
+    AbstractLikelihoodCalculator(const CSaTScanData& Data);
     virtual ~AbstractLikelihoodCalculator();
 
-    virtual double      CalcLogLikelihood(count_t n, measure_t u) = 0;
-    virtual double      CalcLogLikelihoodRatio(count_t tCases, measure_t tMeasure, count_t tTotalCases, measure_t tTotalMeasure) = 0;
-    virtual double      CalcLogLikelihoodRatioEx(count_t tCases, measure_t tMeasure, measure_t tMeasure2, count_t tTotalCases, measure_t tTotalMeasure);
-    virtual double      CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster);
-    virtual double      CalcSVTTLogLikelihood(size_t tStream, CSVTTCluster* Cluster, const CTimeTrend& GlobalTimeTrend);
-    virtual double      GetLogLikelihoodForTotal() const = 0;
+    virtual double                      CalcLogLikelihood(count_t n, measure_t u) const = 0;
+    virtual double                      CalcLogLikelihoodRatio(count_t tCases, measure_t tMeasure, count_t tTotalCases, measure_t tTotalMeasure) const = 0;
+    virtual double                      CalcLogLikelihoodRatioEx(count_t tCases, measure_t tMeasure, measure_t tMeasure2, count_t tTotalCases, measure_t tTotalMeasure) const;
+    virtual double                      CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster) const;
+    virtual double                      CalcSVTTLogLikelihood(size_t tStream, CSVTTCluster* Cluster, const CTimeTrend& GlobalTimeTrend) const;
+    const CSaTScanData                & GetDataHub() const {return gData;}
+    virtual double                      GetLogLikelihoodForTotal() const = 0;
+    AbstractLoglikelihoodRatioUnifier & GetUnifier() const;
 };
 //*****************************************************************************
 #endif
