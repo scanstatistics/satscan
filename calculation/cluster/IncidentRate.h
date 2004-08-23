e96538dd007936276f42cf767d744d7b3960529d
Scott Hostovich <hostovic@imsweb.com>
2004-08-23 17:53:21 +0000

    * new function for high rates in multiple data streams   - does not require number of cases to be greater than 1

--- calculation/cluster/IncidentRate.h
+++ calculation/cluster/IncidentRate.h
@@ -11,6 +11,8 @@ inline bool LowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, mea
 inline bool HighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
 inline bool HighOrLowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
 
+/** Indicates whether an area has lower than expected cases for a clustering
+    within a single data stream. */
 inline bool LowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
 {
   if (nMeasure == 0)
@@ -19,6 +21,10 @@ inline bool LowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, mea
   return (nCases*nTotalMeasure  < nMeasure*nTotalCases);
 }
 
+/** Indicates whether an area has high than expected cases for a clustering
+    within a single data stream. Clusterings with less than two cases are not
+    considered for high rates. Note this function should not be used for scannning
+    for high rates with an analysis with multiple data streams; use MultipleSetsHighRate() */
 inline bool HighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
 {
  if (nMeasure == 0 || nCases < 2/*one case should not be considered a high rate*/)
@@ -27,12 +33,26 @@ inline bool HighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, me
   return (nCases*nTotalMeasure  > nMeasure*nTotalCases);
 }
 
+/** Indicates whether an area has lower than expected cases for a clustering
+    within a single data stream. */
 inline bool HighOrLowRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
 {
   if (nMeasure == 0)
     return false;
 
   return (nCases*nTotalMeasure != nMeasure*nTotalCases);
+}
+
+inline bool MultipleSetsHighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure);
+
+/** For multiple sets, the criteria that a high rate must have more than one case
+    is not currently implemented. */
+inline bool MultipleSetsHighRate(count_t nCases, measure_t nMeasure, count_t nTotalCases, measure_t nTotalMeasure)
+{
+ if (nMeasure == 0)
+    return false;
+
+  return (nCases*nTotalMeasure  > nMeasure*nTotalCases);
 }
 //*****************************************************************************
 #endif
