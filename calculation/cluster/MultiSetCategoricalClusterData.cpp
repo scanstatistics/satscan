68c68505b41a4be9cfbcdf6ff6bc7b788297ccc5
Scott Hostovich <hostovic@imsweb.com>
2005-03-01 19:32:38 +0000

    * categorical cluster data in multiple data sets

---
+++ calculation/cluster/MultiSetCategoricalClusterData.cpp
@@ -0,0 +1,239 @@
+//******************************************************************************
+#include "SaTScan.h"
+#pragma hdrstop
+//******************************************************************************
+#include "MultiSetCategoricalClusterData.h"
+#include "CategoricalClusterDataFactory.h"
+#include "LoglikelihoodRatioUnifier.h"
+
+/** class constructor */
+MultiSetCategoricalSpatialData::MultiSetCategoricalSpatialData(const CategoricalClusterDataFactory& DataFactory, const AbtractDataStreamGateway& DataGateway, int iRate)
+                                  :AbstractSpatialClusterData(0) {
+  for (size_t t=0; t < DataGateway.GetNumInterfaces(); ++t)
+     gvSetClusterData.push_back(dynamic_cast<CategoricalSpatialData*>(DataFactory.GetNewSpatialClusterData(DataGateway.GetDataSetInterface(t), iRate)));
+}
+
+/** class destructor */
+MultiSetCategoricalSpatialData::~MultiSetCategoricalSpatialData() {}
+
+/** Returns newly cloned MultiSetCategoricalSpatialData object. Caller is
+    responsible for deletion of object. */
+MultiSetCategoricalSpatialData * MultiSetCategoricalSpatialData::Clone() const {
+  return new MultiSetCategoricalSpatialData(*this);
+}
+
+/** Assigns cluster data of passed object to 'this' object. Caller of function
+    is responsible for ensuring that passed AbstractSpatialClusterData object
+    can be casted to 'MultiSetCategoricalSpatialData' object. */
+void MultiSetCategoricalSpatialData::Assign(const AbstractSpatialClusterData& rhs) {
+  const MultiSetCategoricalSpatialData& _rhs = (const MultiSetCategoricalSpatialData&)rhs;
+  gvSetClusterData = _rhs.gvSetClusterData;
+}
+
+/** Adds neighbor data to accumulation  - caller is responsible for ensuring that
+    'tNeighborIndex' is a valid index. */
+void MultiSetCategoricalSpatialData::AddNeighborData(tract_t tNeighborIndex, const AbtractDataStreamGateway& DataGateway, size_t) {
+  unsigned int i;
+  for (i=0, gitr=gvSetClusterData.begin(); gitr != gvSetClusterData.end(); ++i, ++gitr)
+     (*gitr)->AddNeighborData(tNeighborIndex, DataGateway, i);
+}
+
+/** Calculates loglikelihood ratio given current accumulated cluster data in
+    each data set and adds together. */
+double MultiSetCategoricalSpatialData::CalculateLoglikelihoodRatio(AbstractLikelihoodCalculator& Calculator) {
+  unsigned int                          i=0;
+  AbstractLoglikelihoodRatioUnifier   & Unifier = Calculator.GetUnifier();
+
+ Unifier.Reset();
+ for (gitr=gvSetClusterData.begin(); gitr != gvSetClusterData.end(); ++gitr, ++i)
+     Unifier.AdjoinRatio(Calculator, (*gitr)->gvCasesPerCategory, (*gitr)->gvTotalCasesPerCategory);
+  return Unifier.GetLoglikelihoodRatio();
+}
+
+/** Not implemented - throws ZdException. */
+count_t MultiSetCategoricalSpatialData::GetCaseCount(unsigned int) const {
+  ZdGenerateException("GetCaseCount(unsigned int) not implemented.","MultiSetCategoricalSpatialData");
+  return 0;
+}
+
+/** Returns number of cases in accumulated cluster data for data set and category.
+    Caller responsible for ensuring that 'iCategoryIndex' and 'tSetIndex' are valid indexes. */
+count_t MultiSetCategoricalSpatialData::GetCategoryCaseCount(unsigned int iCategoryIndex, unsigned int tSetIndex) const {
+  return gvSetClusterData[tSetIndex]->GetCategoryCaseCount(iCategoryIndex);
+}
+
+/** Not implemented - throws ZdException. */
+measure_t MultiSetCategoricalSpatialData::GetMeasure(unsigned int) const {
+  ZdGenerateException("GetMeasure(unsigned int) not implemented.","MultiSetCategoricalSpatialData");
+  return 0;
+}
+
+/** Initializes cluster data in each data set. */
+void MultiSetCategoricalSpatialData::InitializeData() {
+  for (gitr=gvSetClusterData.begin(); gitr != gvSetClusterData.end(); ++gitr)
+     (*gitr)->InitializeData();
+}
+
+//******************************************************************************
+
+/** class constructor */
+AbstractMultiSetCategoricalTemporalData::AbstractMultiSetCategoricalTemporalData() : AbstractTemporalClusterData() {}
+
+/** class destructor */
+AbstractMultiSetCategoricalTemporalData::~AbstractMultiSetCategoricalTemporalData() {}
+
+/** Not implemented - throws ZdException. */
+count_t AbstractMultiSetCategoricalTemporalData::GetCaseCount(unsigned int) const {
+  ZdGenerateException("GetCaseCount(unsigned int) not implemented.","AbstractMultiSetCategoricalTemporalData");
+  return 0;
+}
+
+/** Returns number of cases in accumulated cluster data for data set and category.
+    Caller responsible for ensuring that 'iCategoryIndex' and 'tSetIndex' are valid indexes. */
+count_t AbstractMultiSetCategoricalTemporalData::GetCategoryCaseCount(unsigned int iCategoryIndex, unsigned int tSetIndex) const {
+  return gvSetClusterData[tSetIndex]->GetCategoryCaseCount(iCategoryIndex);
+}
+
+/** Not implemented - throws ZdException. */
+measure_t AbstractMultiSetCategoricalTemporalData::GetMeasure(unsigned int) const {
+  ZdGenerateException("GetCaseCount(unsigned int) not implemented.","AbstractMultiSetCategoricalTemporalData");
+  return 0;
+}
+
+/** Initializes cluster data in each data set. */
+void AbstractMultiSetCategoricalTemporalData::InitializeData() {
+  for (gitr=gvSetClusterData.begin(); gitr != gvSetClusterData.end(); ++gitr)
+     (*gitr)->InitializeData();
+}
+
+//******************************************************************************
+
+/** class constructor */
+MultiSetCategoricalTemporalData::MultiSetCategoricalTemporalData(const CategoricalClusterDataFactory& DataFactory, const AbtractDataStreamGateway& DataGateway)
+                                :AbstractMultiSetCategoricalTemporalData() {
+  for (size_t t=0; t < DataGateway.GetNumInterfaces(); ++t)
+     gvSetClusterData.push_back(dynamic_cast<CategoricalTemporalData*>(DataFactory.GetNewTemporalClusterData(DataGateway.GetDataSetInterface(t))));
+}
+
+/** class destructor */
+MultiSetCategoricalTemporalData::~MultiSetCategoricalTemporalData() {}
+
+/** Return newly cloned MultiSetCategoricalTemporalData object.
+    Caller is responsible for deletion of object. */
+MultiSetCategoricalTemporalData * MultiSetCategoricalTemporalData::Clone() const {
+  return new MultiSetCategoricalTemporalData(*this);
+}
+
+/** Assigns cluster data of passed object to 'this' object. Caller of function
+    is responsible for ensuring that passed AbstractTemporalClusterData object
+    can be casted to 'MultiSetCategoricalTemporalData' object. */
+void MultiSetCategoricalTemporalData::Assign(const AbstractTemporalClusterData& rhs) {
+  const MultiSetCategoricalTemporalData& _rhs = (const MultiSetCategoricalTemporalData&)rhs;
+  gvSetClusterData = _rhs.gvSetClusterData;
+}
+
+/** Not implemented - throws ZdException. */
+void MultiSetCategoricalTemporalData::AddNeighborData(tract_t, const AbtractDataStreamGateway&, size_t) {
+  ZdGenerateException("AddNeighbor(tract_t, const AbtractDataStreamGateway&, size_t) not implemeneted.","MultiSetCategoricalTemporalData");
+}
+
+//******************************************************************************
+
+/** class constructor */
+MultiSetCategoricalProspectiveSpatialData::MultiSetCategoricalProspectiveSpatialData(const CategoricalClusterDataFactory& DataFactory, const CSaTScanData& Data, const AbtractDataStreamGateway& DataGateway)
+                                          :AbstractMultiSetCategoricalTemporalData() {
+  for (size_t t=0; t < DataGateway.GetNumInterfaces(); ++t)
+     gvSetClusterData.push_back(dynamic_cast<CategoricalTemporalData*>(DataFactory.GetNewProspectiveSpatialClusterData(Data, DataGateway.GetDataSetInterface(t))));
+}
+
+/** class destructor */
+MultiSetCategoricalProspectiveSpatialData::~MultiSetCategoricalProspectiveSpatialData() {}
+
+/** Return newly cloned MultiSetCategoricalProspectiveSpatialData object.
+    Caller is responsible for deletion of object. */
+MultiSetCategoricalProspectiveSpatialData * MultiSetCategoricalProspectiveSpatialData::Clone() const {
+  return new MultiSetCategoricalProspectiveSpatialData(*this);
+}
+
+/** Assigns cluster data of passed object to 'this' object. Caller of function
+    is responsible for ensuring that passed AbstractTemporalClusterData object
+    can be casted to 'MultiSetCategoricalProspectiveSpatialData' object. */
+void MultiSetCategoricalProspectiveSpatialData::Assign(const AbstractTemporalClusterData& rhs) {
+  const MultiSetCategoricalProspectiveSpatialData& _rhs = (const MultiSetCategoricalProspectiveSpatialData&)rhs;
+  gvSetClusterData = _rhs.gvSetClusterData;
+}
+
+/** Adds neighbor data to accumulated cluster data  - caller is responsible for
+    ensuring that 'tNeighborIndex' is a valid index. */
+void MultiSetCategoricalProspectiveSpatialData::AddNeighborData(tract_t tNeighborIndex, const AbtractDataStreamGateway& DataGateway, size_t) {
+  unsigned int i;
+
+  for (i=0, gitr=gvSetClusterData.begin(); gitr != gvSetClusterData.end(); ++i, ++gitr)
+     (*gitr)->AddNeighborData(tNeighborIndex, DataGateway, i);
+}
+
+/** Calculates loglikelihood ratio given current accumulated cluster data in
+    each data set and adds together.*/
+double MultiSetCategoricalProspectiveSpatialData::CalculateLoglikelihoodRatio(AbstractLikelihoodCalculator& Calculator) {
+  unsigned int                          iWindowEnd, iAllocationSize;
+  double                                dMaxLoglikelihoodRatio=0;
+  AbstractLoglikelihoodRatioUnifier   & Unifier = Calculator.GetUnifier();
+
+  Unifier.Reset();
+  iAllocationSize = (*gvSetClusterData.begin())->GetAllocationSize();
+  for (gitr=gvSetClusterData.begin(); gitr != gvSetClusterData.end(); ++gitr) {
+     for (size_t t=0; t < (*gitr)->gvCasesPerCategory.size(); ++t)
+        (*gitr)->gvCasesPerCategory[t] = (*gitr)->gppCategoryCases[t][0] - (*gitr)->gppCategoryCases[t][iWindowEnd];
+    Unifier.AdjoinRatio(Calculator, (*gitr)->gvCasesPerCategory, (*gitr)->gvTotalCasesPerCategory);
+  }  
+  dMaxLoglikelihoodRatio = Unifier.GetLoglikelihoodRatio();
+
+ for (iWindowEnd=1; iWindowEnd < iAllocationSize; ++iWindowEnd) {
+     Unifier.Reset();
+     for (gitr=gvSetClusterData.begin(); gitr != gvSetClusterData.end(); ++gitr) {
+        for (size_t t=0; t < (*gitr)->gvCasesPerCategory.size(); ++t)
+           (*gitr)->gvCasesPerCategory[t] = (*gitr)->gppCategoryCases[t][0] - (*gitr)->gppCategoryCases[t][iWindowEnd];
+        Unifier.AdjoinRatio(Calculator, (*gitr)->gvCasesPerCategory, (*gitr)->gvTotalCasesPerCategory);
+     }
+     dMaxLoglikelihoodRatio = std::max(dMaxLoglikelihoodRatio, Unifier.GetLoglikelihoodRatio());
+  }
+
+  return dMaxLoglikelihoodRatio;
+}
+
+//******************************************************************************
+
+/** class constructor */
+MultiSetCategoricalSpaceTimeData::MultiSetCategoricalSpaceTimeData(const CategoricalClusterDataFactory& DataFactory, const AbtractDataStreamGateway& DataGateway)
+                                 :AbstractMultiSetCategoricalTemporalData() {
+  gvSetClusterData.resize(DataGateway.GetNumInterfaces(), 0);
+  for (size_t t=0; t < DataGateway.GetNumInterfaces(); ++t)
+     gvSetClusterData[t] = dynamic_cast<CategoricalTemporalData*>(DataFactory.GetNewSpaceTimeClusterData(DataGateway.GetDataSetInterface(t)));
+}
+
+/** class destructor */
+MultiSetCategoricalSpaceTimeData::~MultiSetCategoricalSpaceTimeData() {}
+
+/** Return newly cloned MultiSetCategoricalSpaceTimeData object.
+    Caller is responsible for deletion of object. */
+MultiSetCategoricalSpaceTimeData * MultiSetCategoricalSpaceTimeData::Clone() const {
+  return new MultiSetCategoricalSpaceTimeData(*this);
+}
+
+/** Assigns cluster data of passed object to 'this' object. Caller of function
+    is responsible for ensuring that passed AbstractTemporalClusterData object
+    can be casted to 'MultiSetCategoricalSpaceTimeData' object. */
+void MultiSetCategoricalSpaceTimeData::Assign(const AbstractTemporalClusterData& rhs) {
+  const MultiSetCategoricalSpaceTimeData& _rhs = (const MultiSetCategoricalSpaceTimeData&)rhs;
+  gvSetClusterData = _rhs.gvSetClusterData;
+}
+
+/** Add neighbor data to accumulation  - caller is responsible for ensuring that
+   'tNeighborIndex' is a valid index. */
+void MultiSetCategoricalSpaceTimeData::AddNeighborData(tract_t tNeighborIndex, const AbtractDataStreamGateway& DataGateway, size_t) {
+  unsigned int i;
+  for (i=0, gitr=gvSetClusterData.begin(); gitr != gvSetClusterData.end(); ++i, ++gitr)
+     (*gitr)->AddNeighborData(tNeighborIndex, DataGateway, i);
+}
+
+
---
+++ calculation/cluster/MultiSetCategoricalClusterData.h
@@ -0,0 +1,90 @@
+//******************************************************************************
+#ifndef __MultiSetCategoricalClusterData_H
+#define __MultiSetCategoricalClusterData_H
+//******************************************************************************
+#include "CategoricalClusterData.h"
+
+class CategoricalClusterDataFactory; /* forward class declaration. */
+
+/** Class representing accumulated data of a spatial clustering in multiple data
+    sets for case data which is partitioned by category. */
+class MultiSetCategoricalSpatialData : public AbstractSpatialClusterData {
+  protected:
+    ZdPointerVector<CategoricalSpatialData>             gvSetClusterData;
+    ZdPointerVector<CategoricalSpatialData>::iterator   gitr;
+
+  public:
+    MultiSetCategoricalSpatialData(const CategoricalClusterDataFactory& DataFactory, const AbtractDataStreamGateway& DataGateway, int iRate);
+    virtual ~MultiSetCategoricalSpatialData();
+
+    virtual void                                        Assign(const AbstractSpatialClusterData& rhs);
+    virtual MultiSetCategoricalSpatialData            * Clone() const;
+
+    virtual void        AddNeighborData(tract_t tNeighborIndex, const AbtractDataStreamGateway& DataGateway, size_t tSetIndex=0);
+    virtual double      CalculateLoglikelihoodRatio(AbstractLikelihoodCalculator& Calculator);
+    virtual count_t     GetCaseCount(unsigned int tSetIndex=0) const;
+    virtual count_t     GetCategoryCaseCount(unsigned int iCategoryIndex, unsigned int tSetIndex=0) const;
+    virtual measure_t   GetMeasure(unsigned int tSetIndex=0) const;
+    virtual void        InitializeData();
+};
+
+/** Abstract class representing accumulated data of a temporal clustering in multiple
+    data sets for case data which is partitioned by category. */
+class AbstractMultiSetCategoricalTemporalData : public AbstractTemporalClusterData {
+  protected:
+    ZdPointerVector<CategoricalTemporalData>::iterator   gitr;
+
+  public:
+    AbstractMultiSetCategoricalTemporalData();
+    virtual ~AbstractMultiSetCategoricalTemporalData();
+
+   ZdPointerVector<CategoricalTemporalData>   gvSetClusterData; /* Note 'Public' member for speed reasons.*/
+
+    virtual count_t     GetCaseCount(unsigned int tSetIndex=0) const;
+    virtual count_t     GetCategoryCaseCount(unsigned int iCategoryIndex, unsigned int tSetIndex=0) const;
+    virtual measure_t   GetMeasure(unsigned int tSetIndex=0) const;
+    virtual void        InitializeData();
+};
+
+/** Class representing accumulated data of a temporal clustering in multiple data
+    sets for case data which is partitioned by category. */
+class MultiSetCategoricalTemporalData : public AbstractMultiSetCategoricalTemporalData {
+  public:
+    MultiSetCategoricalTemporalData(const CategoricalClusterDataFactory& DataFactory, const AbtractDataStreamGateway & DataGateway);
+    virtual ~MultiSetCategoricalTemporalData();
+
+    virtual void                              Assign(const AbstractTemporalClusterData& rhs);
+    virtual MultiSetCategoricalTemporalData * Clone() const;
+
+    virtual void        AddNeighborData(tract_t tNeighborIndex, const AbtractDataStreamGateway& DataGateway, size_t tSetIndex=0);
+};
+
+/** Class representing accumulated data of a prospective spatial clustering in
+    multiple data sets for case data which is partitioned by category. */
+class MultiSetCategoricalProspectiveSpatialData : public AbstractMultiSetCategoricalTemporalData {
+  public:
+    MultiSetCategoricalProspectiveSpatialData(const CategoricalClusterDataFactory& DataFactory, const CSaTScanData& Data, const AbtractDataStreamGateway& DataGateway);
+    virtual ~MultiSetCategoricalProspectiveSpatialData();
+
+    virtual void                                        Assign(const AbstractTemporalClusterData& rhs);
+    virtual MultiSetCategoricalProspectiveSpatialData * Clone() const;
+
+    virtual void        AddNeighborData(tract_t tNeighborIndex, const AbtractDataStreamGateway& DataGateway, size_t tSetIndex=0);
+    virtual double      CalculateLoglikelihoodRatio(AbstractLikelihoodCalculator& Calculator);
+};
+
+/** Class representing accumulated data of a space-time clustering in multiple
+    data sets for case data which is partitioned by category. */
+class MultiSetCategoricalSpaceTimeData : public AbstractMultiSetCategoricalTemporalData {
+  public:
+    MultiSetCategoricalSpaceTimeData(const CategoricalClusterDataFactory& DataFactory, const AbtractDataStreamGateway& DataGateway);
+    virtual ~MultiSetCategoricalSpaceTimeData();
+
+    virtual void                               Assign(const AbstractTemporalClusterData& rhs);
+    virtual MultiSetCategoricalSpaceTimeData * Clone() const;
+
+    virtual void        AddNeighborData(tract_t tNeighborIndex, const AbtractDataStreamGateway& DataGateway, size_t tSetIndex=0);
+};
+//******************************************************************************
+#endif
+
