0724b561c3aec87d94f96a8bfd297486765da1c2
Scott Hostovich <hostovic@imsweb.com>
2005-03-18 18:08:48 +0000

    * v5.1.1

--- calculation/SaTScan.h
+++ calculation/SaTScan.h
@@ -42,14 +42,14 @@
  This file contains header information for all SaTScan program files
  **********************************************************************/
 /** Current version information. */
-#define VERSION_ID      "6"  /** incremental version identifier - this value must
+#define VERSION_ID      "7"  /** incremental version identifier - this value must
                                  be incremented for each released version in order
                                  for update feature to function correctly */
 #define VERSION_MAJOR   "5"
 #define VERSION_MINOR   "1"
-#define VERSION_RELEASE "0"
+#define VERSION_RELEASE "1"
 #define VERSION_PHASE   "" /** testing phase name - leave blank for release */
-#define VERSION_DATE    "December 30, 2004"
+#define VERSION_DATE    "March 18, 2005"
 /** New defines for ellipsoid calculations. */
 #define MAXELLIPSESHAPES 10
 /** PI declaration - this is now defined in math.h but changing may cause
