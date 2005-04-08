6808e0248d56ee1dc98f1b376b59676f8a189c42
Scott Hostovich <hostovic@imsweb.com>
2005-04-08 19:45:58 +0000

    * CSaTScanData& no longer held by class * GetPopulation() now takes const CSaTScanData& * removed debug code   - if such a need is ever needed again, we will but this in toolkit so that     it is accessible anywhere

--- calculation/probability_model/ProbabilityModel.cpp
+++ calculation/probability_model/ProbabilityModel.cpp
@@ -6,33 +6,13 @@
 #include "SaTScanData.h"
 
 /** constructor */
-CModel::CModel(const CParameters& Parameters, CSaTScanData& DataHub, BasePrint& PrintDirection)
-       : gParameters(Parameters), gDataHub(DataHub), gPrintDirection(PrintDirection) {
-#ifdef DEBUGMODEL
-  try {
-    ZdFileName DebugFile(GetToolkit().GetApplicationFullPath());
-    DebugFile.SetFileName("Debug_Info_ProbabiltyModel");
-    DebugFile.SetExtension(".txt");
-
-    if ((m_pDebugModelFile = fopen(DebugFile.GetFullPath(), "w")) == NULL)
-      ZdGenerateException("Unable to create probability model debug file.\n","constructor()");
-  }
-  catch (ZdException &x) {
-    x.AddCallpath("CModel", "CModel");
-    throw;
-  }
-#endif
-}
+CModel::CModel() {}
 
 /** destructor */
-CModel::~CModel() {
-#ifdef DEBUGMODEL
-  fclose(m_pDebugModelFile);
-#endif
-}
+CModel::~CModel() {}
 
 /** Returns population as defined in CCluster object. Not implemented - throws exception. */
-double CModel::GetPopulation(size_t, const CCluster&) const {
+double CModel::GetPopulation(size_t, const CCluster&, const CSaTScanData&) const {
   ZdGenerateException("GetPopulation() not implementated.","CModel");
   return 0;
 }
--- calculation/probability_model/ProbabilityModel.h
+++ calculation/probability_model/ProbabilityModel.h
@@ -7,32 +7,19 @@
 #include "DataSet.h"
 #include "DataSetInterface.h"
 
-//#define DEBUGMODEL 0
-
-class CPSMonotoneCluster;
+//class CPSMonotoneCluster;
 class CSaTScanData;
 class CCluster;
-class CSVTTCluster;
-class CTimeTrend;
+//class CSVTTCluster;
+//class CTimeTrend;
 
 class CModel {
-  protected:
-    BasePrint                 & gPrintDirection;
-    const CParameters         & gParameters;
-    CSaTScanData              & gDataHub;
-#ifdef DEBUGMODEL
-    FILE                      * m_pDebugModelFile;
-#endif
-
   public:
-    CModel(const CParameters& Parameters, CSaTScanData& DataHub, BasePrint& PrintDirection);
+    CModel();
     virtual ~CModel();
 
     virtual void                CalculateMeasure(RealDataSet& DataSet) = 0;
-    virtual double              GetPopulation(size_t tSetIndex, const CCluster& Cluster) const;
-//    virtual double              GetPopulation(size_t iSetIndex, int m_iEllipseOffset,
-//                                              tract_t nCenter, tract_t nTracts,
-//                                              int nStartInterval, int nStopInterval) const;
+    virtual double              GetPopulation(size_t tSetIndex, const CCluster& Cluster, const CSaTScanData& DataHub) const;
 };
 //******************************************************************************
 #endif
