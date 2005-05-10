85b57baf03ca70c42235722501d5b12118ed1525
Scott Hostovich <hostovic@imsweb.com>
2005-05-10 17:39:24 +0000

    * moved clusters container to own unit from Analysis.cpp,.h

---
+++ calculation/analysis/IntermediateClustersContainer.h
@@ -0,0 +1,35 @@
+//******************************************************************************
+#ifndef __IntermediateClustersContainer_H
+#define __IntermediateClustersContainer_H
+//******************************************************************************
+/** Container class to store top clusters for a spatial analysis during
+    method - GetTopCluster(). This class stores the top cluster for a
+    shape, circle and ellipses, inorder to postpone determination of top
+    cluster by adjusted loglikelihood ratio until all possibilities
+    have been calculated and ranked by loglikelihood. For most analyses, the
+    shape will not be be a factor, but when penalizing for non-compactness,
+    the top cluster for circles and each ellipse shape will have be retained
+    until all other calculations have been completed for each iteration of
+    function. */
+
+class CSaTScanData; /** forward class declaration */
+
+template <class T>
+class IntermediateClustersContainer {
+  private:
+    const CSaTScanData                & gData;
+ //   ZdPointerVector<CCluster>           gvTopShapeClusters;
+    std::vector<T>                      gvClusters;
+
+  public:
+    IntermediateClustersContainer(const CSaTScanData& Data);
+    ~IntermediateClustersContainer() {}
+
+    T                                 & GetTopCluster(int iShapeOffset);
+    T                                 & GetTopCluster();
+    void                                Reset(int iCenter);
+    void                                SetTopClusters(const T& Cluster);
+};
+#include "IntermediateClustersContainer.hpp"
+//******************************************************************************
+#endif
