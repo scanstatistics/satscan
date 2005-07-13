084c6e2dd47c7c64eb1ff3812b7889b075a208f4
Scott Hostovich <hostovic@imsweb.com>
2005-07-13 18:49:27 +0000

    * initial version of run time components classes

---
+++ calculation/RunTimeComponents.cpp
@@ -0,0 +1,151 @@
+//******************************************************************************
+#include "SaTScan.h"
+#pragma hdrstop
+//******************************************************************************
+#include "RunTimeComponents.h"
+#include "AsciiPrintFormat.h"
+
+/** constructor */
+RunTimeComponentManager::RunTimeComponentManager() {}
+
+/** destructor */
+RunTimeComponentManager::~RunTimeComponentManager() {}
+
+/** Returns corresponding SerialRunTimeComponent type which FocusRunTimeComponent
+    type belongs to. */
+SerialRunTimeComponent::Type RunTimeComponentManager::GetCorrespondingRunTimeComponent(FocusRunTimeComponent::Type eType) const {
+  switch (eType) {
+    case FocusRunTimeComponent::AddingMeasureList  : return SerialRunTimeComponent::ScanningSimulatedData;
+    case FocusRunTimeComponent::MeasureListCalcLL  : return SerialRunTimeComponent::ScanningSimulatedData;
+  }
+  return SerialRunTimeComponent::CatchAll;
+}
+
+/** Returns label associated with SerialRunTimeComponent type. */
+const char * RunTimeComponentManager::GetLabel(SerialRunTimeComponent::Type eComponent) const {
+  switch (eComponent) {
+    case SerialRunTimeComponent::DataRead              : return "Data Read";
+    case SerialRunTimeComponent::NeighborCalcuation    : return "Neighbor Calculation";
+    case SerialRunTimeComponent::RealDataAnalysis      : return "Real Data Analysis";
+    case SerialRunTimeComponent::RandomDataGeneration  : return "Randomized Data Creation";
+    case SerialRunTimeComponent::ScanningSimulatedData : return "Scanning Simulated Data";
+    case SerialRunTimeComponent::PrintingResults       : return "Printing Results";
+    case SerialRunTimeComponent::CatchAll              : return "Catch All"; 
+  }
+  return "?";
+}
+
+/** Returns label associated with FocusRunTimeComponent type. */
+const char * RunTimeComponentManager::GetLabel(FocusRunTimeComponent::Type eComponent) const {
+  switch (eComponent) {
+    case FocusRunTimeComponent::AddingMeasureList  : return "Add Measure List";
+    case FocusRunTimeComponent::MeasureListCalcLL  : return "Measure List CalcLogLikelihood";
+  }
+  return "?";
+}
+
+/** Returns formated string which indicates total run time. */
+ZdString & RunTimeComponentManager::GetTimeString(double dTimeInSeconds, ZdString& sTimeString) const {
+  short                 uwDays, uwHours, uwMinutes;
+  ZdString              sBuffer;
+
+  uwDays = static_cast<short>(dTimeInSeconds / 86400);
+  dTimeInSeconds -= uwDays * 86400;
+  uwHours = static_cast<short>(dTimeInSeconds / 3600);
+  dTimeInSeconds -= uwHours * 3600;
+  uwMinutes = static_cast<short>(dTimeInSeconds / 60);
+  dTimeInSeconds -= uwMinutes * 60;
+
+  sTimeString.Clear();
+  if (uwDays)
+    sTimeString << uwDays << " d ";
+  if (uwHours)
+    sTimeString << uwHours << " h ";
+  if (uwMinutes)
+    sTimeString << uwMinutes << " m ";
+  sBuffer.printf("%g", dTimeInSeconds);
+  sTimeString << sBuffer << " s ";
+
+  return sTimeString;
+}
+
+/** Initializes run time manager for another analysis. */
+void RunTimeComponentManager::Initialize() {
+  gtRunTimeComponents.clear();
+  gtCurrent=gtRunTimeComponents.end();
+  gtFocusedRunTimeComponents.clear();
+  gTimer.restart();
+}
+
+/** Prints details of component runtimes to file stream. */
+void RunTimeComponentManager::Print(FILE* fp) {
+  double                dTotalExecutionTime;
+  ZdString              sBuffer;
+  AsciiPrintFormat      Printer;
+
+  //stop total execution timer
+  dTotalExecutionTime = gTimer.elapsed();
+  Printer.SetMarginsAsRunTimeReportSection();
+  Printer.PrintSectionSeparatorString(fp, 1, 2);
+  //print SerialRunTimeComponent objects
+  fprintf(fp, "Run Time Components\n");
+  fprintf(fp, "-------------------\n");
+  Printer.PrintSectionLabel(fp, "Total Time", false, false);
+  Printer.PrintAlignedMarginsDataString(fp, GetTimeString(dTotalExecutionTime, sBuffer));
+  for (const_srt_itr_t itr=gtRunTimeComponents.begin(); itr != gtRunTimeComponents.end(); ++itr) {
+     sBuffer.printf("%d) %s", itr->second.GetType(), GetLabel(itr->second.GetType()));
+     Printer.PrintSectionLabel(fp, sBuffer.GetCString(), false, false);
+     dTotalExecutionTime -= itr->second.GetTotalTime();
+     Printer.PrintAlignedMarginsDataString(fp, GetTimeString(itr->second.GetTotalTime(), sBuffer));
+  }
+  sBuffer.printf("%d) %s", SerialRunTimeComponent::CatchAll, GetLabel(SerialRunTimeComponent::CatchAll));
+  Printer.PrintSectionLabel(fp, sBuffer.GetCString(), false, false);
+  Printer.PrintAlignedMarginsDataString(fp, GetTimeString(dTotalExecutionTime, sBuffer));
+  //print FocusRunTimeComponent objects
+  if (gtFocusedRunTimeComponents.size()) {
+    fprintf(fp, "\nFocused Components\n");
+    fprintf(fp, "------------------\n");
+    for (const_frt_itr_t itr=gtFocusedRunTimeComponents.begin(); itr != gtFocusedRunTimeComponents.end(); ++itr) {
+       sBuffer.printf("%s (%d)", GetLabel(itr->second.GetType()), itr->second.GetBelongingSerialType());
+       Printer.PrintSectionLabel(fp, sBuffer.GetCString(), false, false);
+       Printer.PrintAlignedMarginsDataString(fp, GetTimeString(itr->second.GetTotalTime(), sBuffer));
+    }
+  }  
+  Printer.PrintSectionSeparatorString(fp);
+}
+
+/** Starts timing of SerialRunTimeComponent type, stopping current component, creating
+    new SerialRunTimeComponent object if component of type does not already exist. */
+void RunTimeComponentManager::StartSerialComponent(SerialRunTimeComponent::Type eComponent) {
+  if (gtCurrent != gtRunTimeComponents.end()) gtCurrent->second.Stop();
+  gtCurrent = gtRunTimeComponents.find(eComponent);
+  if (gtCurrent == gtRunTimeComponents.end())
+    gtCurrent = gtRunTimeComponents.insert(gtRunTimeComponents.begin(),
+                                           std::make_pair(eComponent, SerialRunTimeComponent(eComponent)));
+  gtCurrent->second.Start();
+}
+
+/** Starts timing of FocusRunTimeComponent type, creating new FocusRunTimeComponent object if
+    component of type does not already exist. */
+void RunTimeComponentManager::StartFocused(FocusRunTimeComponent::Type eComponent) {
+  frt_itr_t itr = gtFocusedRunTimeComponents.find(eComponent);
+  if (itr == gtFocusedRunTimeComponents.end())
+    itr = gtFocusedRunTimeComponents.insert(gtFocusedRunTimeComponents.begin(),
+                                            std::make_pair(eComponent,
+                                                           FocusRunTimeComponent(GetCorrespondingRunTimeComponent(eComponent), eComponent)));
+  itr->second.Start();
+}
+
+/** Stops timing of current SerialRunTimeComponent object. */
+void RunTimeComponentManager::StopSerialComponent() {
+  if (gtCurrent != gtRunTimeComponents.end()) gtCurrent->second.Stop();
+  gtCurrent = gtRunTimeComponents.end();
+}
+
+/** Stops timing of FocusRunTimeComponent object for type. */
+void RunTimeComponentManager::StopFocused(FocusRunTimeComponent::Type eComponent) {
+  frt_itr_t itr = gtFocusedRunTimeComponents.find(eComponent);
+  if (itr != gtFocusedRunTimeComponents.end())
+    itr->second.Stop();
+}
+
---
+++ calculation/RunTimeComponents.h
@@ -0,0 +1,92 @@
+//******************************************************************************
+#ifndef __RunTimeComponents_H
+#define __RunTimeComponents_H
+//******************************************************************************
+#include <boost/timer.hpp>
+#include <string>
+
+/** Abtract interface for runtime component timer. */
+class AbstractRunTimeComponent {
+  protected:
+    boost::timer                gTimer;
+    double                      gdTotalTime;
+
+  public:
+    AbstractRunTimeComponent() : gdTotalTime(0) {}
+    virtual ~AbstractRunTimeComponent() {}
+
+    double                      GetTotalTime() const {return gdTotalTime;}
+    void                        Reset() {gdTotalTime=0;}
+    void                        Start() {gTimer.restart();}
+    void                        Stop() {gdTotalTime += gTimer.elapsed();}
+};
+
+/** Runtime component timer. */
+class SerialRunTimeComponent : public AbstractRunTimeComponent {
+  public:
+    /** Runtime types where time will not overlap. */
+    enum Type  {DataRead=1, NeighborCalcuation, RealDataAnalysis, RandomDataGeneration,
+                ScanningSimulatedData, PrintingResults, CatchAll};
+
+  protected:
+    Type        geType;
+
+  public:
+    SerialRunTimeComponent(Type eType) : AbstractRunTimeComponent(), geType(eType) {}
+    virtual ~SerialRunTimeComponent() {}
+
+    Type        GetType() const {return geType;}
+};
+
+/** Focused runtime component timer. */
+class FocusRunTimeComponent : public AbstractRunTimeComponent {
+  public:
+    /** Runtime types where time can overlap. */
+    enum Type  {AddingMeasureList=1, MeasureListCalcLL};
+
+  protected:
+    SerialRunTimeComponent::Type  geSerialType;
+    Type                          geFocusType;
+
+  public:
+    FocusRunTimeComponent(SerialRunTimeComponent::Type eSerialType, Type eFocusType)
+            :AbstractRunTimeComponent(), geSerialType(eSerialType), geFocusType(eFocusType) {}
+    virtual ~FocusRunTimeComponent() {}
+
+    SerialRunTimeComponent::Type  GetBelongingSerialType() const {return geSerialType;}
+    Type                          GetType() const {return geFocusType;}
+};
+
+/** Manages runtime component timers. */
+class RunTimeComponentManager {
+  typedef std::map<SerialRunTimeComponent::Type, SerialRunTimeComponent>                  srt_container_t;
+  typedef std::map<SerialRunTimeComponent::Type, SerialRunTimeComponent>::iterator        srt_itr_t;
+  typedef std::map<SerialRunTimeComponent::Type, SerialRunTimeComponent>::const_iterator  const_srt_itr_t;
+  typedef std::map<FocusRunTimeComponent::Type, FocusRunTimeComponent>                    frt_container_t;
+  typedef std::map<FocusRunTimeComponent::Type, FocusRunTimeComponent>::iterator          frt_itr_t;
+  typedef std::map<FocusRunTimeComponent::Type, FocusRunTimeComponent>::const_iterator    const_frt_itr_t;
+
+  private:
+    boost::timer                  gTimer;
+    srt_container_t               gtRunTimeComponents;
+    srt_itr_t                     gtCurrent;
+    frt_container_t               gtFocusedRunTimeComponents;
+
+    SerialRunTimeComponent::Type  GetCorrespondingRunTimeComponent(FocusRunTimeComponent::Type eType) const;
+    const char                  * GetLabel(SerialRunTimeComponent::Type eComponent) const;
+    const char                  * GetLabel(FocusRunTimeComponent::Type eComponent) const;
+    ZdString                    & GetTimeString(double dTimeInSeconds, ZdString& sTimeString) const;
+
+  public:
+    RunTimeComponentManager();
+    ~RunTimeComponentManager();
+
+    void                          Print(FILE* fp);
+    void                          Initialize();
+    void                          StartSerialComponent(SerialRunTimeComponent::Type eComponent);
+    void                          StartFocused(FocusRunTimeComponent::Type eComponent);
+    void                          StopSerialComponent();
+    void                          StopFocused(FocusRunTimeComponent::Type eComponent);
+};
+//******************************************************************************
+#endif
