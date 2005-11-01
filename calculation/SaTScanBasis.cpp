b79b11be0fb2fd88926d774b21f4470bd64adc4b
Scott Hostovich <hostovic@imsweb.com>
2005-11-01 15:19:09 +0000

    * last update reverted

--- calculation/SaTScanBasis.cpp
+++ calculation/SaTScanBasis.cpp
@@ -2,7 +2,6 @@
 #pragma hdrstop
 #include "SaTScanBasis.h"
 
-#pragma warn -8071
 #ifndef __BATCH_COMPILE
  #include<Basis540.h>
  #include<Basis540.cpp>
@@ -10,5 +9,4 @@
  #include<Basis540Unix.h>
  #include<Basis540Unix.cpp>
 #endif
-#pragma warn +8071
 
