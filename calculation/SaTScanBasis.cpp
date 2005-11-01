db80bf827d5d79b00342e47de9d080068488844b
Scott Hostovich <hostovic@imsweb.com>
2005-11-01 14:36:03 +0000

    * suppress warning messages from basis library

--- calculation/SaTScanBasis.cpp
+++ calculation/SaTScanBasis.cpp
@@ -2,6 +2,7 @@
 #pragma hdrstop
 #include "SaTScanBasis.h"
 
+#pragma warn -8071
 #ifndef __BATCH_COMPILE
  #include<Basis540.h>
  #include<Basis540.cpp>
@@ -9,3 +10,5 @@
  #include<Basis540Unix.h>
  #include<Basis540Unix.cpp>
 #endif
+#pragma warn +8071
+
