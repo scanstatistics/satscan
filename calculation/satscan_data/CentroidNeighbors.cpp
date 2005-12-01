4571aa7d3c237671d8da985c52653b16cd0176cf
Scott Hostovich <hostovic@imsweb.com>
2005-12-01 19:04:16 +0000

    * centroid neighbor code moved from MakeNeighbors.cpp,.h to own unit

---
+++ calculation/satscan_data/CentroidNeighbors.cpp
@@ -0,0 +1,96 @@
+//******************************************************************************
+#include "SaTScan.h"
+#pragma hdrstop
+//******************************************************************************
+#include "CentroidNeighbors.h"
+#include "SaTScanData.h"
+
+/** Comparison function for LocationDistance objects. */
+bool CompareLocationDistance::operator() (const LocationDistance& lhs, const LocationDistance& rhs) {
+  //first check whether distances are equal - we may need to break a tie
+  if (lhs.GetDistance() == rhs.GetDistance()) {
+    // if lhs and rhs reference same location - just return false 
+    if (lhs.GetTractNumber() == rhs.GetTractNumber())
+      return false;
+    // break ties in a controlled scheme:
+    //   - compare coordinates starting at first dimension and
+    //       continue until last dimension(if needed)
+    //   - lesser coordinate breaks tie, not for any particular reason
+    //     that was the decision made by Martin
+    //   - if all coordinates are equal, then something is wrong as
+    //     duplicate coordinates should have been handled by this point
+    //     in program execution. Throw exception - else we've lost tie
+    //     breaking control.
+    for (gbContinue=true,gi=0; gi < gTractInformation.tiGetDimensions() && gbContinue; gi++) {
+       gdCoordinateLHS = gTractInformation.tiGetTractCoordinate(lhs.GetTractNumber(), gi);
+       gdCoordinateRHS = gTractInformation.tiGetTractCoordinate(rhs.GetTractNumber(), gi);
+       gbContinue = (gdCoordinateLHS == gdCoordinateRHS);
+    }
+    if (gbContinue) // Done comparing coordinates, are they duplicates?
+      ZdGenerateException("Identical coordinates found during sort comparison for tracts \"%s\" and \"%s\".",
+                          "CompareTractDistance()",
+                          gTractInformation.tiGetTid(lhs.GetTractNumber(), gsLHS),
+                          gTractInformation.tiGetTid(rhs.GetTractNumber(), gsRHS));
+    return (gdCoordinateLHS < gdCoordinateRHS);
+  }
+  //distances not equal, compare as normal
+  else
+    return (lhs.GetDistance() < rhs.GetDistance());
+}
+
+//******************************************************************************
+
+/** constructor */
+CentroidNeighbors::CentroidNeighbors()
+                  :gtCentroid(0), gtEllipseOffset(0), giNeighbors(0), giMaxNeighbors(0),
+                   giMaxReportedNeighbors(0), gpSortedNeighborsIntegerType(0),
+                   gpSortedNeighborsUnsignedShortType(0), gppSortedNeighborsIntegerType(0),
+                   gppSortedNeighborsUnsignedShortType(0), gpNeighborArray(0) {}
+
+/** constructor */
+CentroidNeighbors::CentroidNeighbors(tract_t tEllipseOffset, const CSaTScanData& DataHub)
+                  : gtCentroid(0), gtEllipseOffset(tEllipseOffset), giNeighbors(0), giMaxNeighbors(0),
+                    giMaxReportedNeighbors(0), gpSortedNeighborsIntegerType(0),
+                    gpSortedNeighborsUnsignedShortType(0), gppSortedNeighborsIntegerType(0),
+                    gppSortedNeighborsUnsignedShortType(0), gpNeighborArray(0) {
+                    
+  gpNeighborArray = DataHub.GetNeighborCountArray()[gtEllipseOffset];
+  if (DataHub.GetSortedArrayAsTract_T(tEllipseOffset))
+    gppSortedNeighborsIntegerType = DataHub.GetSortedArrayAsTract_T(gtEllipseOffset);
+  else
+    gppSortedNeighborsUnsignedShortType = DataHub.GetSortedArrayAsUShort_T(gtEllipseOffset);
+}
+
+/** destructor */
+CentroidNeighbors::~CentroidNeighbors() {}
+
+/** Sets class members to define locations about centroid index / ellipse index.
+    The neighbor information referenced is that which is calculated by CentroidNeighborCalculator
+    object; caller is responsible for ensuring that:
+        1) tEllipseOffset, tCentroid, iNumNeighbors and iNumReportedNeighbors are valid indexes
+        
+    Allocates vector of either integers or unsigned shorts, based upon specified number of
+    neighbors for centroid (iNumNeighbors). Sets maxium number of neighbors variable returned
+    through GetNumNeighbors() method to that of 'iNumReportedNeighbors' variable. */
+void CentroidNeighbors::Set(tract_t tEllipseOffset, tract_t tCentroid, int iNumNeighbors, int iNumReportedNeighbors, const std::vector<LocationDistance>& vOrderedLocations) {
+
+  //conditionally allocate unsigned short vs tract_t
+  if (vOrderedLocations.size() < (size_t)std::numeric_limits<unsigned short>::max()) {
+    gvSortedNeighborsUnsignedShortType.resize(iNumNeighbors);
+    gpSortedNeighborsUnsignedShortType = (iNumNeighbors ? &gvSortedNeighborsUnsignedShortType[0] : 0);
+    for (tract_t j=iNumNeighbors-1; j >= 0; j--) /* copy tract numbers */
+       gpSortedNeighborsUnsignedShortType[j] = static_cast<unsigned short>(vOrderedLocations[j].GetTractNumber());
+  }
+  else {
+    gvSortedNeighborsIntegerType.resize(iNumNeighbors);
+    gpSortedNeighborsIntegerType = (iNumNeighbors ? &gvSortedNeighborsIntegerType[0] : 0);
+    for (tract_t j=iNumNeighbors-1; j >= 0; j--) /* copy tract numbers */
+       gpSortedNeighborsIntegerType[j] = vOrderedLocations[j].GetTractNumber();
+  }
+
+  gtCentroid = tCentroid;
+  gtEllipseOffset = tEllipseOffset;
+  giMaxNeighbors = iNumNeighbors;
+  giNeighbors = giMaxReportedNeighbors = iNumReportedNeighbors;
+}
+
---
+++ calculation/satscan_data/CentroidNeighbors.h
@@ -0,0 +1,102 @@
+//******************************************************************************
+#ifndef CentroidNeighborsH
+#define CentroidNeighborsH
+//******************************************************************************
+#include "Tracts.h"
+
+/** Distance from a centroid to a neighboring location. */
+class LocationDistance {
+   private:
+     tract_t    m_tTractNumber;     /* tract number */
+     double     m_dDistance;        /* distance from centroid to location at m_tTractNumber */
+
+   public:
+     LocationDistance(tract_t t=0, double d=0) {SetTractNumber(t); SetDistance(d);}
+     virtual ~LocationDistance() {}
+
+     double     GetDistance() const {return m_dDistance;}
+     tract_t    GetTractNumber() const {return m_tTractNumber;}
+     void       SetDistance(double d) {m_dDistance=d;}
+     void       SetTractNumber(tract_t t) {m_tTractNumber=t;}
+};
+
+/** Function object used to compare LocationDistance objects by m_dDistance. */
+class CompareLocationDistance {
+  private:
+    int                 	gi;                     /* loop index */
+    double              	gdCoordinateLHS,        /* coordinate variables */
+                        	gdCoordinateRHS;
+    std::string                 gsLHS, gsRHS;
+    bool                	gbContinue;             /* stops coordinate comparisons */
+    const TractHandler 	      & gTractInformation;      /* tract information */
+
+  public:
+    CompareLocationDistance(const TractHandler & TractInformation) : gTractInformation(TractInformation) {}
+
+    bool                operator() (const LocationDistance& lhs, const LocationDistance& rhs);
+};
+
+class CSaTScanData; /** forward class declaration */
+
+/** Interface which details calculated neighbor indexes about a specific centroid. */
+class CentroidNeighbors {
+  friend class CentroidNeighborCalculator;
+  private:
+    tract_t                       gtCentroid;                             /* centroid index */
+    tract_t                       gtEllipseOffset;                        /* ellipse index */
+    int                           giNeighbors;                            /* neighbor count - variable returned in get method */
+    int                           giMaxNeighbors;                         /* neighbor count - variable which defines max for replication process */
+    int                           giMaxReportedNeighbors;                 /* neighbor count - varible whch defines maximum for evaluation of real data */
+    std::vector<tract_t>          gvSortedNeighborsIntegerType;           /* conditionally allocated integer vector of neighbor indexes */
+    std::vector<unsigned short>   gvSortedNeighborsUnsignedShortType;     /* conditionally allocated unsigned short vector of neighbors indexes */
+    tract_t                    ** gppSortedNeighborsIntegerType;          /* pointer to integer array of neighbors indexes */
+    unsigned short             ** gppSortedNeighborsUnsignedShortType;    /* pointer to unsigned short array of neighbors indexes */
+    tract_t                     * gpNeighborArray;
+    tract_t                     * gpSortedNeighborsIntegerType;           /* pointer to integer array of neighbors indexes */
+    unsigned short              * gpSortedNeighborsUnsignedShortType;     /* pointer to unsigned short array of neighbors indexes */
+
+    void                          Set(tract_t tEllipseOffset, tract_t tCentroid, int iNumNeighbors, int iNumReportedNeighbors, const std::vector<LocationDistance>& vOrderedLocations);
+
+  public:
+    CentroidNeighbors();
+    CentroidNeighbors(tract_t tEllipseOffset, const CSaTScanData& DataHub);
+    ~CentroidNeighbors();
+
+    tract_t                     GetEllipseIndex() const {return gtEllipseOffset;}
+    tract_t                     GetCentroidIndex() const {return gtCentroid;}
+    inline tract_t              GetNeighborTractIndex(tract_t tNeighborIndex) const;
+    inline int                  GetNumNeighbors() const {return giNeighbors;}
+    tract_t                   * GetRawIntegerArray() const {return gpSortedNeighborsIntegerType;}
+    unsigned short            * GetRawUnsignedShortArray() const {return gpSortedNeighborsUnsignedShortType;}
+    inline void                 Set(tract_t tCentroid);
+    void                        SetMaximumClusterSize_RealData() {giNeighbors = giMaxReportedNeighbors;}
+    void                        SetMaximumClusterSize_SimulatedData() {giNeighbors = giMaxNeighbors;}
+};
+
+/** Returns zero based tNeighborIndex'th nearest neighbor's tract index.
+    Caller is responsible for ensuring that internal structures have been previously
+    set and 'tNeighborIndex' is within zero based range for defined neighbor information. */
+inline tract_t CentroidNeighbors::GetNeighborTractIndex(tract_t tNeighborIndex) const {
+ //assert(tNeighborIndex + 1 <= (size_t)giNeighbors && (gpSortedNeighborsUnsignedShortType || gpSortedNeighborsIntegerType));
+ return (gpSortedNeighborsUnsignedShortType ? (tract_t)gpSortedNeighborsUnsignedShortType[tNeighborIndex] : gpSortedNeighborsIntegerType[tNeighborIndex]);
+}
+
+/** Sets class members to define locations about centroid index / ellipse index.
+    The neigbor information referenced is that of the 'sorted' array, so caller is
+    responsible for ensuring that:
+        1) sorted array is allocated and contains calculated neighbors about centroids
+        2) tEllipseOffset and tCentroid are valid indexes into sorted array
+        3) sorted array will persist until this object is destructed
+    Returns reference to self.*/
+inline void CentroidNeighbors::Set(tract_t tCentroid) {
+  gtCentroid = tCentroid;
+  giNeighbors = giMaxNeighbors = giMaxReportedNeighbors = gpNeighborArray[tCentroid];
+
+  if (gppSortedNeighborsIntegerType)
+    gpSortedNeighborsIntegerType = gppSortedNeighborsIntegerType[tCentroid];
+  else
+    gpSortedNeighborsUnsignedShortType = gppSortedNeighborsUnsignedShortType[tCentroid];
+}
+//******************************************************************************
+#endif
+
