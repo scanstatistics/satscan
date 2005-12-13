c6e82a917333d0cb8dfee2f80cb647497a86a930
Scott Hostovich <hostovic@imsweb.com>
2005-12-13 20:33:08 +0000

    * defined purely temporal randomizer

--- calculation/randomization/BernoulliRandomizer.cpp
+++ calculation/randomization/BernoulliRandomizer.cpp
@@ -5,27 +5,25 @@
 #include "BernoulliRandomizer.h"
 #include "SaTScanData.h"
 
-/** constructor */
-BernoulliNullHypothesisRandomizer::BernoulliNullHypothesisRandomizer(long lInitialSeed)
-                                  :AbstractOrdinalDenominatorDataRandomizer(lInitialSeed) {}
-
-/** destructor */
-BernoulliNullHypothesisRandomizer::~BernoulliNullHypothesisRandomizer() {}
-
-/** returns newly cloned BernoulliNullHypothesisRandomizer */
-BernoulliNullHypothesisRandomizer * BernoulliNullHypothesisRandomizer::Clone() const {
-  return new BernoulliNullHypothesisRandomizer(*this);
-}
-
-/** Randomizes dataset's data under null hypothesis for Bernoulli model. */
-void BernoulliNullHypothesisRandomizer::RandomizeData(const RealDataSet& thisRealSet, SimDataSet& thisSimSet, unsigned int iSimulation) {
-
-  // reset seed of random number generator
-  SetSeed(iSimulation, thisSimSet.GetSetIndex());
-  // reset simulation cases to zero
-  thisSimSet.ResetCumulativeCaseArray();
+/** Creates randomized under the null hypothesis for Bernoulli model, assigning data to SimDataSet objects structures.
+    Random number generator seed initialized based upon 'iSimulation' index. */
+void BernoulliNullHypothesisRandomizer::RandomizeData(const RealDataSet& RealSet, SimDataSet& SimSet, unsigned int iSimulation) {
+  //reset seed of random number generator
+  SetSeed(iSimulation, SimSet.GetSetIndex());
+  SimSet.GetCaseArrayHandler().Set(0);
   //randomize data
-  RandomizeOrdinalData(thisRealSet.GetTotalCases(), thisRealSet.GetTotalControls(),
-                       thisSimSet.GetCaseArray(), thisRealSet.GetMeasureArray(),
-                       thisRealSet.GetNumTracts(), thisRealSet.GetNumTimeIntervals());
+  RandomizeOrdinalData(RealSet.GetTotalCases(), RealSet.GetTotalControls(), SimSet.GetCaseArray(),
+                       RealSet.GetMeasureArray(), RealSet.GetNumTracts(), RealSet.GetNumTimeIntervals());
 }
+//******************************************************************************
+
+/** Creates randomized under the null hypothesis for Bernoulli model, assigning data to SimDataSet objects structures.
+    Random number generator seed initialized based upon 'iSimulation' index. */
+void BernoulliPurelyTemporalNullHypothesisRandomizer::RandomizeData(const RealDataSet& RealSet, SimDataSet& SimSet, unsigned int iSimulation) {
+  //reset seed of random number generator
+  SetSeed(iSimulation, SimSet.GetSetIndex());
+  memset(SimSet.GetPTCasesArray(), 0, (RealSet.GetNumTimeIntervals()+1) * sizeof(count_t));
+  //randomize data
+  RandomizePurelyTemporalOrdinalData(RealSet.GetTotalCases(), RealSet.GetTotalControls(), SimSet.GetPTCasesArray(), RealSet.GetPTMeasureArray(), RealSet.GetNumTimeIntervals());
+}
+
--- calculation/randomization/BernoulliRandomizer.h
+++ calculation/randomization/BernoulliRandomizer.h
@@ -7,11 +7,23 @@
 /** Bernoulli randomizer for null hypothesis. */
 class BernoulliNullHypothesisRandomizer : public AbstractOrdinalDenominatorDataRandomizer {
   public:
-    BernoulliNullHypothesisRandomizer(long lInitialSeed=RandomNumberGenerator::glDefaultSeed);
-    virtual ~BernoulliNullHypothesisRandomizer();
-    virtual BernoulliNullHypothesisRandomizer * Clone() const;
+    BernoulliNullHypothesisRandomizer(long lInitialSeed=RandomNumberGenerator::glDefaultSeed) : AbstractOrdinalDenominatorDataRandomizer(lInitialSeed) {}
+    virtual ~BernoulliNullHypothesisRandomizer() {}
 
-    virtual void  RandomizeData(const RealDataSet& thisRealSet, SimDataSet& thisSimSet, unsigned int iSimulation);
+    virtual BernoulliNullHypothesisRandomizer * Clone() const {return new BernoulliNullHypothesisRandomizer(*this);}
+
+    virtual void  RandomizeData(const RealDataSet& RealSet, SimDataSet& SimSet, unsigned int iSimulation);
+};
+
+/** Bernoulli randomizer for null hypothesis, optimized for purely temporal analyses. */
+class BernoulliPurelyTemporalNullHypothesisRandomizer : public AbstractOrdinalDenominatorDataRandomizer {
+  public:
+    BernoulliPurelyTemporalNullHypothesisRandomizer(long lInitialSeed=RandomNumberGenerator::glDefaultSeed) : AbstractOrdinalDenominatorDataRandomizer(lInitialSeed) {}
+    virtual ~BernoulliPurelyTemporalNullHypothesisRandomizer() {}
+
+    virtual BernoulliPurelyTemporalNullHypothesisRandomizer * Clone() const {return new BernoulliPurelyTemporalNullHypothesisRandomizer(*this);}
+
+    virtual void  RandomizeData(const RealDataSet& RealSet, SimDataSet& SimSet, unsigned int iSimulation);
 };
 //******************************************************************************
 #endif
