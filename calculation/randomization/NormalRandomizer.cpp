63996a0b2c53be19087af1193adadafe79190de4
Scott Hostovich <hostovic@imsweb.com>
2005-12-13 20:28:55 +0000

    * moved NormalRandomizers from ContinuousVariableRandomizer unit * defined purely temporal randomizer

---
+++ calculation/randomization/NormalRandomizer.cpp
@@ -0,0 +1,98 @@
+//******************************************************************************
+#include "SaTScan.h"
+#pragma hdrstop
+//******************************************************************************
+#include "NormalRandomizer.h"
+
+/** Adds new randomization entry with passed attrbiute values. */
+void AbstractNormalRandomizer::AddCase(count_t tCount, int iTimeInterval, tract_t tTractIndex, measure_t tContinuousVariable) {
+  for (count_t t=0; t < tCount; ++t) {
+    //add stationary values
+    gvStationaryAttribute.push_back(NormalStationary_t(std::make_pair(iTimeInterval, tTractIndex)));
+    //add permutated value
+    gvPermutedAttribute.push_back(NormalPermuted_t(tContinuousVariable));
+    //add to vector which maintains original order
+    gvOriginalPermutedAttribute.push_back(NormalPermuted_t(tContinuousVariable));
+  }
+}
+
+/** Allocates and sets appropriate data structures of RealDataSet object from internal
+    collection of attributes. */
+void AbstractNormalRandomizer::AssignFromAttributes(RealDataSet& RealSet) {
+  StationaryContainer_t::const_iterator itr_stationary=gvStationaryAttribute.begin(), itr_end=gvStationaryAttribute.end();
+  PermutedContainer_t::const_iterator   itr_permuted=gvPermutedAttribute.begin();
+  int                                   i, tTract, iNumTracts=RealSet.GetNumTracts(), iNumTimeIntervals=RealSet.GetNumTimeIntervals();
+  measure_t                          ** ppMeasure, ** ppSqMeasure;
+  count_t                            ** ppCases;
+
+  RealSet.AllocateCasesArray();
+  ppCases = RealSet.GetCaseArray();
+  RealSet.AllocateMeasureArray();
+  ppMeasure = RealSet.GetMeasureArray();
+  RealSet.AllocateSqMeasureArray();
+  ppSqMeasure = RealSet.GetSqMeasureArray();
+  for (; itr_stationary != itr_end; ++itr_stationary, ++itr_permuted) {
+     ++ppCases[itr_stationary->GetStationaryVariable().first][itr_stationary->GetStationaryVariable().second];
+     ppMeasure[itr_stationary->GetStationaryVariable().first][itr_stationary->GetStationaryVariable().second] += itr_permuted->GetPermutedVariable();
+     ppSqMeasure[itr_stationary->GetStationaryVariable().first][itr_stationary->GetStationaryVariable().second] += pow(itr_permuted->GetPermutedVariable(), 2);
+  }
+  //now set as cumulative
+  for (tTract=0; tTract < iNumTracts; ++tTract)
+     for (i=iNumTimeIntervals-2; i >= 0; --i) {
+        ppCases[i][tTract] = ppCases[i+1][tTract] + ppCases[i][tTract];
+        ppMeasure[i][tTract] = ppMeasure[i+1][tTract] + ppMeasure[i][tTract];
+        ppSqMeasure[i][tTract] = ppSqMeasure[i+1][tTract] + ppSqMeasure[i][tTract];
+     }
+}
+//******************************************************************************
+
+/** Assigns randomized data to dataset's simulation measure structures. */
+void NormalRandomizer::AssignRandomizedData(const RealDataSet& RealSet, SimDataSet& SimSet) {
+  StationaryContainer_t::const_iterator itr_stationary=gvStationaryAttribute.begin(), itr_end=gvStationaryAttribute.end();
+  PermutedContainer_t::const_iterator   itr_permuted=gvPermutedAttribute.begin();
+  measure_t                          ** ppMeasure, ** ppSqMeasure;
+  int                                   i, tTract, iNumTracts=RealSet.GetNumTracts(), iNumTimeIntervals=RealSet.GetNumTimeIntervals();
+
+  //reset simulation measure arrays to zero
+  SimSet.GetMeasureArrayHandler().Set(0);
+  SimSet.GetSqMeasureArrayHandler().Set(0);
+  ppMeasure = RealSet.GetMeasureArray();
+  ppSqMeasure = RealSet.GetSqMeasureArray();
+  //assign randomized continuous data to measure and measure squared arrays
+  for (; itr_stationary != itr_end; ++itr_stationary, ++itr_permuted) {
+     ppMeasure[itr_stationary->GetStationaryVariable().first][itr_stationary->GetStationaryVariable().second] += itr_permuted->GetPermutedVariable();
+     ppSqMeasure[itr_stationary->GetStationaryVariable().first][itr_stationary->GetStationaryVariable().second] += pow(itr_permuted->GetPermutedVariable(), 2);
+  }
+  //now set as cumulative
+  for (tTract=0; tTract < iNumTracts; ++tTract)
+     for (i=iNumTimeIntervals-2; i >= 0; --i) {
+        ppMeasure[i][tTract] = ppMeasure[i+1][tTract] + ppMeasure[i][tTract];
+        ppSqMeasure[i][tTract] = ppSqMeasure[i+1][tTract] + ppSqMeasure[i][tTract];
+     }
+}
+//******************************************************************************
+
+/** Assigns randomized data to dataset's simulation measure structures. */
+void NormalPurelyTemporalRandomizer::AssignRandomizedData(const RealDataSet& RealSet, SimDataSet& SimSet) {
+  StationaryContainer_t::const_iterator itr_stationary=gvStationaryAttribute.begin(), itr_end=gvStationaryAttribute.end();
+  PermutedContainer_t::const_iterator   itr_permuted=gvPermutedAttribute.begin();
+  measure_t                           * pMeasure, * pSqMeasure;
+  int                                   i, iNumTimeIntervals=RealSet.GetNumTimeIntervals();
+
+  //reset simulation measure arrays to zero
+  pMeasure = RealSet.GetPTMeasureArray();
+  memset(pMeasure, 0, (iNumTimeIntervals+1) * sizeof(measure_t));
+  pSqMeasure = RealSet.GetPTSqMeasureArray();
+  memset(pSqMeasure, 0, (iNumTimeIntervals+1) * sizeof(measure_t));
+  //assign randomized continuous data to measure and measure squared arrays
+  for (; itr_stationary != itr_end; ++itr_stationary, ++itr_permuted) {
+     pMeasure[itr_stationary->GetStationaryVariable().first] += itr_permuted->GetPermutedVariable();
+     pSqMeasure[itr_stationary->GetStationaryVariable().first] += pow(itr_permuted->GetPermutedVariable(), 2);
+  }
+  //now set as cumulative
+  for (i=iNumTimeIntervals-2; i >= 0; --i) {
+     pMeasure[i] = pMeasure[i+1] + pMeasure[i];
+     pSqMeasure[i] = pSqMeasure[i+1] + pSqMeasure[i];
+  }
+}
+
---
+++ calculation/randomization/NormalRandomizer.h
@@ -0,0 +1,49 @@
+//******************************************************************************
+#ifndef NormalRandomizerH
+#define NormalRandomizerH
+//******************************************************************************
+#include "PermutationDataRandomizer.h"
+
+typedef StationaryAttribute<std::pair<int, tract_t> >   NormalStationary_t;
+typedef PermutedAttribute<double>                       NormalPermuted_t;
+
+/** */
+class AbstractNormalRandomizer :  public AbstractPermutedDataRandomizer<NormalStationary_t, NormalPermuted_t>{
+   public:
+     AbstractNormalRandomizer(long lInitialSeed) : AbstractPermutedDataRandomizer<NormalStationary_t, NormalPermuted_t>(lInitialSeed) {}
+     virtual ~AbstractNormalRandomizer() {}
+
+    virtual void               AddCase(count_t tCount, int iTimeInterval, tract_t tTractIndex, measure_t tContinuousVariable);
+    virtual void               AssignFromAttributes(RealDataSet& RealSet);
+};
+
+/** Randomizes data of dataset for a 'normal' probablility model.
+    Instead of assigning data to simulation case structures, assigns
+    randomized data to simulation measure structures. */
+class NormalRandomizer : public AbstractNormalRandomizer {
+  protected:
+    virtual void               AssignRandomizedData(const RealDataSet& RealSet, SimDataSet& SimSet);
+
+  public:
+    NormalRandomizer::NormalRandomizer(long lInitialSeed=RandomNumberGenerator::glDefaultSeed) : AbstractNormalRandomizer(lInitialSeed) {}
+    virtual ~NormalRandomizer() {}
+
+    virtual NormalRandomizer * Clone() const {return new NormalRandomizer(*this);}
+};
+
+/** Randomizes data of dataset for a 'normal' probablility model.
+    Instead of assigning data to simulation case structures, assigns
+    randomized data to simulation meaure structures. */
+class NormalPurelyTemporalRandomizer : public AbstractNormalRandomizer {
+  protected:
+    virtual void               AssignRandomizedData(const RealDataSet& RealSet, SimDataSet& SimSet);
+
+  public:
+    NormalPurelyTemporalRandomizer(long lInitialSeed=RandomNumberGenerator::glDefaultSeed) : AbstractNormalRandomizer(lInitialSeed) {}
+    virtual ~NormalPurelyTemporalRandomizer() {}
+
+    virtual NormalPurelyTemporalRandomizer * Clone() const {return new NormalPurelyTemporalRandomizer(*this);}
+};
+//******************************************************************************
+#endif
+
