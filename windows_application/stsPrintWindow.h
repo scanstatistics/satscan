67042f64f37f69b8c4656c8113a544672f8daad5
Scott Hostovich <hostovic@imsweb.com>
2006-04-21 17:56:34 +0000

    * NullPrint class moved to BasePrint.h

--- windows_application/stsPrintWindow.h
+++ windows_application/stsPrintWindow.h
@@ -4,23 +4,6 @@
 //******************************************************************************
 #include "BasePrint.h"
 
-/** Print direction class that quietly suppresses printing messages. */
-class PrintNull : public BasePrint {
-   protected:
-    virtual void        PrintError(const char * sMessage) {}
-    virtual void        PrintNotice(const char * sMessage) {}
-    virtual void        PrintStandard(const char * sMessage) {}
-    virtual void        PrintWarning(const char * sMessage) {}
-
-   public:
-     PrintNull(bool bSuppressWarnings=true) : BasePrint(bSuppressWarnings) {}
-     virtual ~PrintNull() {}
-
-     bool               GetIsCanceled() const {return false;}
-     virtual void       Print(const char * sMessage, PrintType ePrintType) {}
-     virtual void       Printf(const char * sMessage, PrintType ePrintType, ...) {}
-};
-
 /** Print direction class that directs messages to calculation thread so
     that communication with analysis run window is synchronized with main
     VCL thread. */
