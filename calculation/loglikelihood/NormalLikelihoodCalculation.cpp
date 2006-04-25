1c561e5a56515943957bfc824c12d003db1b83ad
Scott Hostovich <hostovic@imsweb.com>
2006-04-25 15:44:55 +0000

    * defined normal loglikelihood calculation

--- calculation/loglikelihood/NormalLikelihoodCalculation.cpp
+++ calculation/loglikelihood/NormalLikelihoodCalculation.cpp
@@ -24,9 +24,18 @@ double NormalLikelihoodCalculator::CalcLogLikelihoodRatio(count_t n, measure_t u
 }
 
 /** calculates the Poisson log likelihood ratio given the number of observed and expected cases, and second measure  - not implemented - throws exception. */
-double NormalLikelihoodCalculator::CalcLogLikelihoodRatioNormal(count_t tCases, measure_t tMeasure, measure_t tMeasure2, count_t tTotalCases, measure_t tTotalMeasure) const {
-  ZdGenerateException("CalcLogLikelihoodRatioNormal() not implementated.","NormalLikelihoodCalculator");
-  return 0;
+double NormalLikelihoodCalculator::CalcLogLikelihoodRatioNormal(count_t tCases, measure_t tMeasure, measure_t tMeasure2, count_t tTotalCases, measure_t tTotalMeasure, measure_t tTotalMeasureSq) const {
+  if (!(tTotalCases - tCases)) return 0; //when the cluster contains all the cases in set, ratio is zero
+  assert(tCases > 0);
+  double dEstimatedMeanInside = tMeasure/tCases;
+  double dEstimatedMeanOutside = (tTotalMeasure - tMeasure)/(tTotalCases - tCases);
+  double dEstimatedVariance = (1.0/tTotalCases)
+                              * (tMeasure2 - 2.0 * tMeasure * dEstimatedMeanInside + tCases * std::pow(dEstimatedMeanInside, 2) +
+                                 (tTotalMeasureSq - tMeasure2) - 2.0 * (tTotalMeasure - tMeasure) * dEstimatedMeanOutside +
+                                 (tTotalCases - tCases) * std::pow(dEstimatedMeanOutside, 2) );
+
+
+  return -1 * tTotalCases * std::log(std::sqrt(dEstimatedVariance)) + tTotalCases * std::log(std::sqrt(tTotalMeasureSq/tTotalCases - std::pow(tTotalMeasure/tTotalCases, 2)));
 }
 
 /** returns log likelihood for total - not implemented - throws exception. */
