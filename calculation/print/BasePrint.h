896fbb768cf57417e1809606d80e600fdfdbecc7
Scott Hostovich <hostovic@imsweb.com>
2006-04-25 15:46:08 +0000

    * NullPrint class moved here from stsPrintWindow.h

--- calculation/print/BasePrint.h
+++ calculation/print/BasePrint.h
@@ -41,6 +41,22 @@ class BasePrint {
     void                                SetSuppressWarnings(bool b) {gbSuppressWarnings=b;}
 };
 
+/** Print direction class that quietly suppresses printing messages. */
+class PrintNull : public BasePrint {
+   protected:
+    virtual void        PrintError(const char * sMessage) {}
+    virtual void        PrintNotice(const char * sMessage) {}
+    virtual void        PrintStandard(const char * sMessage) {}
+    virtual void        PrintWarning(const char * sMessage) {}
+
+   public:
+     PrintNull(bool bSuppressWarnings=true) : BasePrint(bSuppressWarnings) {}
+     virtual ~PrintNull() {}
+
+     bool               GetIsCanceled() const {return false;}
+     virtual void       Print(const char * sMessage, PrintType ePrintType) {}
+     virtual void       Printf(const char * sMessage, PrintType ePrintType, ...) {}
+};
 //******************************************************************************
 #endif
 
