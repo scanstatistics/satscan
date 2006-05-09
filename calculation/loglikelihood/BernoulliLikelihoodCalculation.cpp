51025cd1c775261b7b1f82bb90e0459fd034a756
Scott Hostovich <hostovic@imsweb.com>
2006-05-09 15:15:08 +0000

    * calculation methods updated to refer to data set index instead of specifiying totals * defined new 'maximizing' methods that replace loglikelihood calculations

--- calculation/loglikelihood/BernoulliLikelihoodCalculation.cpp
+++ calculation/loglikelihood/BernoulliLikelihoodCalculation.cpp
@@ -1,13 +1,20 @@
-//---------------------------------------------------------------------------
+//******************************************************************************
 #include "SaTScan.h"
 #pragma hdrstop
-//---------------------------------------------------------------------------
+//******************************************************************************
 #include "BernoulliLikelihoodCalculation.h"
 #include "PurelySpatialMonotoneCluster.h"
 
 /** constructor */
 BernoulliLikelihoodCalculator::BernoulliLikelihoodCalculator(const CSaTScanData& DataHub)
-                              :AbstractLikelihoodCalculator(DataHub) {}
+                              :AbstractLikelihoodCalculator(DataHub) {
+  //store data set loglikelihoods under null
+  for (size_t t=0; t < DataHub.GetDataSetHandler().GetNumDataSets(); ++t) {
+    count_t   N = DataHub.GetDataSetHandler().GetDataSet(t).GetTotalCases();
+    measure_t U = DataHub.GetDataSetHandler().GetDataSet(t).GetTotalMeasure();
+    gvDataSetLogLikelihoodUnderNull.push_back((N*log(N/U) + (U-N)*log((U-N)/U)));
+  }
+}
 
 /** destructor */
 BernoulliLikelihoodCalculator::~BernoulliLikelihoodCalculator() {}
@@ -15,8 +22,8 @@ BernoulliLikelihoodCalculator::~BernoulliLikelihoodCalculator() {}
 /** calculates the Bernoulli log likelihood given the number of observed and expected cases
     - the total cases and expected cases used are that of first data set */
 double BernoulliLikelihoodCalculator::CalcLogLikelihood(count_t n, measure_t u) const {
-  count_t   N = gtTotalCasesInFirstDataSet;
-  measure_t U = gtTotalMeasureInFirstDataSet;
+  count_t   N = gvDataSetTotals[0].first;
+  measure_t U = gvDataSetTotals[0].second;
 
   double    nLL_A = 0.0;
   double    nLL_B = 0.0;
@@ -35,8 +42,10 @@ double BernoulliLikelihoodCalculator::CalcLogLikelihood(count_t n, measure_t u) 
   return nLL_A + nLL_B + nLL_C + nLL_D;
 }
 
-/** calculates the Bernoulli log likelihood ratio given the number of observed and expected cases */
-double BernoulliLikelihoodCalculator::CalcLogLikelihoodRatio(count_t n, measure_t u, count_t N, measure_t U) const {
+/** calculates the Bernoulli log likelihood ratio given the number of observed, expected cases and data set index*/
+double BernoulliLikelihoodCalculator::CalcLogLikelihoodRatio(count_t n, measure_t u, size_t tSetIndex) const {
+  count_t   N = gvDataSetTotals[tSetIndex].first;
+  measure_t U = gvDataSetTotals[tSetIndex].second;
   double    dLogLikelihood;
   double    nLL_A = 0.0;
   double    nLL_B = 0.0;
@@ -56,24 +65,7 @@ double BernoulliLikelihoodCalculator::CalcLogLikelihoodRatio(count_t n, measure_
   dLogLikelihood = nLL_A + nLL_B + nLL_C + nLL_D;
 
   // return the logliklihood ratio (loglikelihood - loglikelihood for total)
-  return dLogLikelihood - (N*log(N/U) + (U-N)*log((U-N)/U));
-}
-
-/** returns log likelihood for total
-    - the total cases and expected cases used are that of first data set */
-double BernoulliLikelihoodCalculator::GetLogLikelihoodForTotal() const {
-  count_t   N = gtTotalCasesInFirstDataSet;
-  measure_t U = gtTotalMeasureInFirstDataSet;
-
-  return (N*log(N/U) + (U-N)*log((U-N)/U));
-}
-
-/** Returns log likelihood ratio given passed log likelihood.  */
-double BernoulliLikelihoodCalculator::GetLogLikelihoodRatio(double dLogLikelihood) const {
-  count_t   N = gtTotalCasesInFirstDataSet;
-  measure_t U = gtTotalMeasureInFirstDataSet;
-
-  return dLogLikelihood - (N*log(N/U) + (U-N)*log((U-N)/U));
+  return dLogLikelihood - (gvDataSetLogLikelihoodUnderNull[tSetIndex]);
 }
 
 /** calculates loglikelihood ratio for purely spatial monotone analysis given passed cluster */
@@ -94,5 +86,41 @@ double BernoulliLikelihoodCalculator::CalcMonotoneLogLikelihood(const CPSMonoton
   }
 
   return nLogLikelihood;
+}
+
+/** Calculates the full loglikelihood ratio/test statistic given passed maximizing value and
+    data set index. For the Bernoulli calculator, the maximizing value would be the loglikelihood in
+    a particular clustering. If maximizing value equals negative double max value, zero is returned
+    as this indicates that no significant maximizing value was calculated. */
+double BernoulliLikelihoodCalculator::CalculateFullStatistic(double dMaximizingValue, size_t tSetIndex) const {
+  if (dMaximizingValue == -std::numeric_limits<double>::max()) return 0.0;
+  return dMaximizingValue - (gvDataSetLogLikelihoodUnderNull[tSetIndex]);
+}
+
+/** Calculates the maximizing value given observed cases, expected cases and data set index.
+    For the Bernoulli calculator, the maximizing value is the loglikelihood. */
+double BernoulliLikelihoodCalculator::CalculateMaximizingValue(count_t n, measure_t u, size_t tDataSetIndex) const {
+  count_t   N = gvDataSetTotals[tDataSetIndex].first;
+  measure_t U = gvDataSetTotals[tDataSetIndex].second;
+  double    nLL_A = 0.0;
+  double    nLL_B = 0.0;
+  double    nLL_C = 0.0;
+  double    nLL_D = 0.0;
+
+  if (n != 0)
+    nLL_A = n*log(n/u);
+  if (n != u)
+    nLL_B = (u-n)*log(1-(n/u));
+  if (N-n != 0)
+    nLL_C = (N-n)*log((N-n)/(U-u));
+   if (N-n != U-u)
+    nLL_D = ((U-u)-(N-n))*log(1-((N-n)/(U-u)));
+
+  return nLL_A + nLL_B + nLL_C + nLL_D;
+}
+
+/** returns log likelihood for total data set at index */
+double BernoulliLikelihoodCalculator::GetLogLikelihoodForTotal(size_t tSetIndex) const {
+  return gvDataSetLogLikelihoodUnderNull[tSetIndex];
 }
 
--- calculation/loglikelihood/BernoulliLikelihoodCalculation.h
+++ calculation/loglikelihood/BernoulliLikelihoodCalculation.h
@@ -1,20 +1,24 @@
-//*****************************************************************************
+//******************************************************************************
 #ifndef __BernoulliLikelihoodCalculation_H
 #define __BernoulliLikelihoodCalculation_H
-//*****************************************************************************
+//******************************************************************************
 #include "LikelihoodCalculation.h"
 
 /** Bernoulli log likelihood calculator. */
 class BernoulliLikelihoodCalculator : public AbstractLikelihoodCalculator {
+  private:
+    std::vector<double> gvDataSetLogLikelihoodUnderNull;
+
   public:
     BernoulliLikelihoodCalculator(const CSaTScanData& DataHub);
     virtual ~BernoulliLikelihoodCalculator();
 
     virtual double      CalcLogLikelihood(count_t n, measure_t u) const;
-    virtual double      CalcLogLikelihoodRatio(count_t tCases, measure_t tMeasure, count_t tTotalCases, measure_t tTotalMeasure) const;
+    virtual double      CalcLogLikelihoodRatio(count_t tCases, measure_t tMeasure, size_t tSetIndex=0) const;
     virtual double      CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster) const;
-    virtual double      GetLogLikelihoodForTotal() const;
-    virtual double      GetLogLikelihoodRatio(double dLogLikelihood) const;
+    virtual double      CalculateFullStatistic(double dMaximizingValue, size_t tDataSetIndex=0) const;
+    virtual double      CalculateMaximizingValue(count_t n, measure_t u, size_t tDataSetIndex=0) const;
+    virtual double      GetLogLikelihoodForTotal(size_t tSetIndex=0) const;
 };
-//*****************************************************************************
+//******************************************************************************
 #endif
