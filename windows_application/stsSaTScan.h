5c48a27bef3096f8d5629cc4ffb89f52ba0e180a
Scott Hostovich <hostovic@imsweb.com>
2006-06-16 17:10:32 +0000

    * removed file include 'stsUtilityFunctions.cpp'

--- windows_application/stsSaTScan.h
+++ windows_application/stsSaTScan.h
@@ -13,7 +13,6 @@
 #include "stsFrmAnalysisRun.h"
 #include "stsPrintWindow.h"
 #include "stsOutputFileRegistry.h"
-#include "stsUtilityFunctions.h"
 #include "stsDataImporter.h"
 #include "stsDlgDataImporter.h"
 #include "stsFrmAnalysisParameters.h"
