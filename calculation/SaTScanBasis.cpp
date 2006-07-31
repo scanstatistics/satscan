b3237d58af85e370914788a54745fc5c5a5ee481
Scott Hostovich <hostovic@imsweb.com>
2006-07-31 19:50:35 +0000

    * VCL inclusion moved to separate unit

--- calculation/SaTScanBasis.cpp
+++ calculation/SaTScanBasis.cpp
@@ -1,12 +1,8 @@
+//******************************************************************************
 #include "SaTScan.h"
 #pragma hdrstop
+//******************************************************************************
 #include "SaTScanBasis.h"
-
-#ifndef __BATCH_COMPILE
- #include<Basis540.h>
- #include<Basis540.cpp>
-#else
- #include<Basis540Unix.h>
- #include<Basis540Unix.cpp>
-#endif
+#include<Basis540Unix.h>
+#include<Basis540Unix.cpp>
 
--- calculation/SaTScanBasis.h
+++ calculation/SaTScanBasis.h
@@ -1,19 +1,8 @@
-//---------------------------------------------------------------------------
+//******************************************************************************
 #ifndef __SaTScanBasis_H
 #define __SaTScanBasis_H
-//---------------------------------------------------------------------------
-
-#ifndef __BATCH_COMPILE
-// defines go here for windows interface -- vcl dependent stuff
-  #define __USE_xbZdFileImporter
-  #define __USE_xbCSVFile
-  #define __USE_xbZdFileViewController
-  #define __USE_xbGridZdSingleFileModel
-  #define __USE_xbDlgException
-#endif
-
+//******************************************************************************
 // defines for non-vcl dependent stuff here
 #define __USE_xbTree
-
-//---------------------------------------------------------------------------
+//******************************************************************************
 #endif
