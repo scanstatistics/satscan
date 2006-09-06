f8dcb2660b9b62d9f9b897c9785bd5f3961899d4
Scott Hostovich <hostovic@imsweb.com>
2006-09-06 19:59:07 +0000

    * refactored classess to be more robust; simplier public interface * reused inner classes of TractHandler when possible * renamed methods, the "gi****" naming convention was strange * updated grid point addition process, previous insertion by label design wasn't really ensuring that points are stored in specified order    -- re-ordering file records would have caused different internal indexes    -- note that this matches new behavior in TractHandler class  so the CentroidHandlerPassThrow class will have same desired behavior

--- calculation/satscan_data/GridTractCoordinates.cpp
+++ calculation/satscan_data/GridTractCoordinates.cpp
@@ -3,89 +3,47 @@
 #pragma hdrstop
 //*****************************************************************************
 #include "GridTractCoordinates.h"
-#include "Tracts.h"
 
-/** constructor */
-CentroidHandler::GridPoint::GridPoint(const char * sLabel, const std::vector<double>& vCoordinates) {
-   gsLabel = new char[std::strlen(sLabel) + 1];
-   strcpy(gsLabel, sLabel);
-   gpCoordinates = new double[vCoordinates.size()];
-   std::memcpy(gpCoordinates, &vCoordinates[0], vCoordinates.size() * sizeof(double));
-}
-
-/** constructor */
-CentroidHandler::GridPoint::GridPoint(const double* pCoordinates, int size) {
-   gsLabel = new char[1];
-   gsLabel[0] = '\0';
-   gpCoordinates = new double[size];
-   std::memcpy(gpCoordinates, pCoordinates, size * sizeof(double));
-}
-
-/** destructor  */
-CentroidHandler::GridPoint::~GridPoint() {
-  try {
-    delete[] gsLabel;
-    delete[] gpCoordinates;
+/** Prints coordinates to file stream. */
+void GInfo::displayGridPoints(FILE* pDisplay) {
+  std::vector<double> vRepository;
+  
+  for (tract_t tPoint=0; tPoint < getNumGridPoints(); ++tPoint) {
+    retrieveCoordinates(tPoint, vRepository);
+    for (size_t c=0; c < vRepository.size(); ++c) fprintf(pDisplay, "%lf\t", vRepository[c]);
+    fprintf(pDisplay, "\n");
   }
-  catch (...){}
 }
 
-void CentroidHandler::giDisplayGridTractInfo(FILE* pDisplay) {
-  fprintf(pDisplay, "GID        x           y\n");
-  for (size_t i=0; i< gvGridPoints.size(); ++i)
-    fprintf(pDisplay, "%s   %f   %f\n", gvGridPoints[i]->gsLabel, gvGridPoints[i]->gpCoordinates[0], gvGridPoints[i]->gpCoordinates[1]);
-  fprintf(pDisplay, "\n");
-}
-
-/** Inserts grid point into internal container, ordered by first coordinate. */
-void CentroidHandler::giInsertGnode(const char *gid, std::vector<double>& vCoordinates) {
+/** Inserts grid point into internal container, ordered by coordinates. */
+void CentroidHandler::addGridPoint(const std::vector<double>& vCoordinates) {
   try {
-    //validate that passed coordinates have same dimensions as class has defined
-    if (vCoordinates.size() != (unsigned int)nDimensions)
-      ZdGenerateException("Passed coordinates have %u dimensions, wanted %i.", "giInsertGnode()", vCoordinates.size(), nDimensions);
+    if (gAdditionStatus == Closed)
+      ZdGenerateException("This TractHandler object is closed to insertions.", "addGridPoint()");
 
-    ZdPointerVector<GridPoint>::iterator  itr, itrPosition;
-    std::auto_ptr<GridPoint> Point(new GridPoint(gid, vCoordinates));
-    //find insertion position based upon first coordinate
-    itr = itrPosition = lower_bound(gvGridPoints.begin(), gvGridPoints.end(), Point.get(), CompareFirstCoordinate());
+    //validate that passed coordinates have same dimensions as class has defined
+    if (vCoordinates.size() != (unsigned int)giPointDimensions)
+      ZdGenerateException("Passed coordinates have %u dimensions, wanted %i.", "addGridPoint()", vCoordinates.size(), giPointDimensions);
+
+    std::auto_ptr<Point_t> pPoint(new Point_t(vCoordinates, gvPoints.size()));
+    //keeping coordinates in sorted order accomplishes two things:
+    // 1) makes determination of duplicates much faster
+    // 2) consistancy with index into gvPoints -- we sometimes use this attribute to break ties in ranking of clusters
+    PointsContainer_t::iterator itrPoint = std::lower_bound(gvPoints.begin(), gvPoints.end(), pPoint.get(), TractHandler::CompareCoordinates());
     //if there exists a grid point with same coordinates, ignore this record
-    while (itr != gvGridPoints.end() && (*itr)->gpCoordinates[0] == vCoordinates[0]) {
-       if (!std::memcmp((*itr)->gpCoordinates, &vCoordinates[0], nDimensions * sizeof(double))) return; //ignore duplicates
-       ++itr;
-    }
-    gvGridPoints.insert(itrPosition, Point.release());
+    if (itrPoint == gvPoints.end() ||  *(pPoint.get()) != *(*itrPoint))
+      gvPoints.insert(itrPoint, pPoint.release());
   }
   catch (ZdException &x) {
-    x.AddCallpath("giInsertGnode()", "CentroidHandler");
+    x.AddCallpath("addGridPoint()", "CentroidHandler");
     throw;
   }
 }
 
-/** Retrieves coordinates of grid point at index 't'. */
-void CentroidHandler::giRetrieveCoords(tract_t t, std::vector<double> & vRepository) const {
-  if (t < 0 || t >= (tract_t)gvGridPoints.size())
-    ZdException::Generate("Index %d is out of bounds: [size=%d].", "CentroidHandler", t, gvGridPoints.size());
-
-  vRepository.resize(nDimensions);
-  std::copy(gvGridPoints[t]->gpCoordinates, gvGridPoints[t]->gpCoordinates + nDimensions, vRepository.begin());
-}
-
-/** Sorts accumulated grid points by specified label. The internal collection of
-    grid points needs to be sorted by their labels to maintain consistancy of output. */
-void CentroidHandler::SortGridPointsByLabel() {
-  std::sort(gvGridPoints.begin(), gvGridPoints.end(), Comparelabel());
-}
-
-
-int CentroidHandlerPassThrow::giGetDimensions() const {
-  return gTractHandler.tiGetDimensions();
-}
-
-tract_t CentroidHandlerPassThrow::giGetNumTracts() const {
-  return gTractHandler.tiGetNumTracts();
-}
-
-void CentroidHandlerPassThrow::giRetrieveCoords(tract_t t, std::vector<double> & vRepository) const {
-  gTractHandler.tiRetrieveCoords(t, vRepository);
+/** sets dimensions expected for points added to this object. */
+void CentroidHandler::setDimensions(unsigned int iPointDimensions) {
+  if (gvPoints.size())
+    ZdGenerateException("Changing the coordinate dimensions is not permited once points have been defined.","setDimensions()");
+  giPointDimensions = iPointDimensions;
 }
 
--- calculation/satscan_data/GridTractCoordinates.h
+++ calculation/satscan_data/GridTractCoordinates.h
@@ -3,54 +3,38 @@
 #define __GRIDTRACTCOORDINATES_H
 //*****************************************************************************
 #include "SaTScan.h"
-
-class TractHandler;
+#include "Tracts.h"
 
 /** Abstract base class which manages centroid grid points. */
 class GInfo {
   public:
-    virtual int         giGetDimensions() const = 0;
-    virtual void        giDisplayGridTractInfo(FILE* pDisplay) = 0;
-    virtual tract_t     giGetNumTracts() const = 0;
-    virtual void        giRetrieveCoords(tract_t t, std::vector<double> & vRepository) const = 0;
+    virtual void        displayGridPoints(FILE* pDisplay);
+    virtual int         getGridPointDimensions() const = 0;
+    virtual tract_t     getNumGridPoints() const = 0;
+    virtual void        retrieveCoordinates(tract_t tPoint, std::vector<double> & vRepository) const = 0;
 };
 
 /** Manages centroid grid points in class collection structure. */
 class CentroidHandler : public GInfo {
+   public:
+     typedef TractHandler::Coordinates Point_t;
+     typedef ZdPointerVector<Point_t>  PointsContainer_t;
+     enum addition_status_t            {Accepting=0, Closed};
+
    private:
-     class GridPoint {
-       public:
-         GridPoint(const char * sLabel, const std::vector<double>& vCoordinates);
-         GridPoint(const double* pCoordinates, int size);
-         ~GridPoint();
-
-         char   * gsLabel;
-         double * gpCoordinates;
-     };
-     //functor that compares grid point labels
-     class Comparelabel {
-       public:
-         bool operator() (GridPoint * lhs, GridPoint * rhs) {return (strcmp(lhs->gsLabel, rhs->gsLabel) < 0);}
-     };
-     //functor that compares first coordinate of grid point
-     class CompareFirstCoordinate {
-       public:
-         bool operator() (GridPoint * lhs, GridPoint * rhs) {return (lhs->gpCoordinates[0] < rhs->gpCoordinates[0]);}
-     };
-
-     ZdPointerVector<GridPoint>  gvGridPoints;
-     int 	                 nDimensions;
+     addition_status_t   gAdditionStatus;
+     PointsContainer_t   gvPoints;
+     unsigned int        giPointDimensions;
 
    public:
-     CentroidHandler() : GInfo() {}
+     CentroidHandler() : GInfo(), gAdditionStatus(Accepting), giPointDimensions(2) {}
 
-     virtual void        giDisplayGridTractInfo(FILE* pDisplay);
-     virtual int         giGetDimensions() const {return nDimensions;}
-     virtual tract_t     giGetNumTracts() const {return (tract_t)gvGridPoints.size();}
-     void                giInsertGnode(const char *gid, std::vector<double>& vCoordinates);
-     virtual void        giRetrieveCoords(tract_t t, std::vector<double> & vRepository) const;
-     void                giSetDimensions(int nDim) {nDimensions = nDim;}
-     void                SortGridPointsByLabel();
+     void                additionsCompleted() {gAdditionStatus = Closed;}
+     void                addGridPoint(const std::vector<double>& vCoordinates);
+     virtual int         getGridPointDimensions() const {return giPointDimensions;}
+     virtual tract_t     getNumGridPoints() const {return (tract_t)gvPoints.size();}
+     virtual void        retrieveCoordinates(tract_t tPoint, std::vector<double> & vRepository) const {gvPoints.at(tPoint)->retrieve(vRepository);}
+     void                setDimensions(unsigned int iPointDimensions);
 };
 
 /** Provides an interface to centroid grid points that are actually maintained by the TractHandler class.
@@ -63,10 +47,10 @@ class CentroidHandlerPassThrow : public GInfo {
    public:
      CentroidHandlerPassThrow(const TractHandler& Handler) : GInfo(), gTractHandler(Handler) {}
 
-    virtual void        giDisplayGridTractInfo(FILE* pDisplay) {}
-    virtual int         giGetDimensions() const;
-    virtual tract_t     giGetNumTracts() const;
-    virtual void        giRetrieveCoords(tract_t t, std::vector<double> & vRepository) const;
+    virtual int         getGridPointDimensions() const {return gTractHandler.getCoordinateDimensions();}
+    virtual tract_t     getNumGridPoints() const {return gTractHandler.getCoordinates().size();}
+    virtual void        retrieveCoordinates(tract_t tPoint, std::vector<double> & vRepository) const
+                         { gTractHandler.getCoordinates().at(tPoint)->retrieve(vRepository); }
 };
 //*****************************************************************************
 #endif
