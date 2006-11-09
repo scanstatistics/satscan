21afb6f0feb1f4f17d70848d9ab26b46808eb1cd
Scott Hostovich <hostovic@imsweb.com>
2006-11-09 19:01:00 +0000

    * SVTT log likelihood calculation moved to separate unit

--- calculation/loglikelihood/PoissonLikelihoodCalculation.h
+++ calculation/loglikelihood/PoissonLikelihoodCalculation.h
@@ -10,9 +10,7 @@ class PoissonLikelihoodCalculator : public AbstractLikelihoodCalculator {
   private:
     const CParameters & gParameters;   /** const reference to CParameters object */
     std::vector<double> gvDataSetLogLikelihoodUnderNull;
-    double              CalcSVTTLogLikelihood(count_t* pCases, measure_t* pMeasure,
-                                              count_t pTotalCases, double nAlpha,
-                                              double nBeta, int nStatus) const;
+
   public:
     PoissonLikelihoodCalculator(const CSaTScanData& DataHub);
     virtual ~PoissonLikelihoodCalculator();
@@ -20,7 +18,6 @@ class PoissonLikelihoodCalculator : public AbstractLikelihoodCalculator {
     virtual double      CalcLogLikelihood(count_t n, measure_t u) const;
     virtual double      CalcLogLikelihoodRatio(count_t tCases, measure_t tMeasure, size_t tSetIndex=0) const;
     virtual double      CalcMonotoneLogLikelihood(const CPSMonotoneCluster& PSMCluster) const;
-    virtual double      CalcSVTTLogLikelihood(size_t tSetIndex, CSVTTCluster* Cluster, const CTimeTrend& GlobalTimeTrend) const;
     virtual double      CalculateFullStatistic(double dMaximizingValue, size_t tDataSetIndex=0) const;
     virtual double      CalculateMaximizingValue(count_t n, measure_t u, size_t tDataSetIndex=0) const;
     virtual double      GetLogLikelihoodForTotal(size_t tSetIndex=0) const;
