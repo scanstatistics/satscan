023de18e842dbf9fa84fe36dd927d68c65ca22d0
Scott Hostovich <hostovic@imsweb.com>
2006-11-17 19:32:29 +0000

    * re-implemented exception classes to be derived from std::exeception    - continued isolation of Zd library usage

--- calculation/utility/SSException.cpp
+++ calculation/utility/SSException.cpp
@@ -3,64 +3,108 @@
 #pragma hdrstop
 //---------------------------------------------------------------------------
 #include "SSException.h"
-#include "UtilityFunctions.h" 
+#include "UtilityFunctions.h"
+#include "FileName.h" 
 
-/**  Construct. This is an alternate constructor for when the varArgs list for sMessage
-     has already been prepared. Primarily, this will be used by derived classes.        */
-ResolvableException::ResolvableException ( va_list varArgs, const char *sMessage, const char *sSourceModule, Level iLevel )
-                    :ZdException ( varArgs, sMessage, sSourceModule, iLevel ) {}
+////////////////////////////////////////////////////////////////////////////////
 
-/**  This function will throw the exception with the parameters.  It is equivalent to
-     throw ZdException(...), but includes the ability to format the message string.
-     This function should be used to generate all Zd Exceptions within ZD.             */
-void GenerateResolvableException(const char * sMessage, const char * sSourceModule, ... ) {
-  va_list      varArgs;
-  va_start ( varArgs, sSourceModule );
+prg_exception::prg_exception() : std::exception() {}
 
-  ResolvableException  theException ( varArgs, sMessage, sSourceModule, ResolvableException::Normal );
-  va_end ( varArgs );
-
-  throw theException;
-}
-
-
-/**  Construct. This is an alternate constructor for when the varArgs list for sMessage
-     has already been prepared. Primarily, this will be used by derived classes.        */
-UsageException::UsageException(const char * sExecutableFullpathName) : ZdException () {
-   std::string s;
-
-   printString(s, "usage: %s [parameters file] [options]\n\n"
-                  "options: -o     -- ignore parameter result filename setting, use next parameter\n"
-                  "                   ex. c:\\>satscan.exe c:\\parameters.prm -o c:\\alternate.out.txt\n"
-                  "         -c     -- confirm parameter file correctness only (does not perform analysis)\n"
-                  "         -p     -- print parameter settings to screen (does not perform analysis)\n",
-            ZdFileName(sExecutableFullpathName).GetCompleteFileName());
-
-   SetErrorMessage(s.c_str());
-}
-
-/**  This function will throw the exception with the parameters.  It is equivalent to
-     throw ZdException(...), but includes the ability to format the message string.
-     This function should be used to generate all Zd Exceptions within ZD.             */
-void GenerateUsageException(const char * sExecutableFullpathName) {
-  throw UsageException(sExecutableFullpathName);
-}
-
-program_error::program_error(const std::string& what_arg) : std::exception() {
+prg_exception::prg_exception(const std::string& what_arg) : std::exception() {
   __what=what_arg;
 }
 
-program_error::program_error(const char * format, ...) : std::exception() {
+prg_exception::prg_exception(const char * format, const char * method, ...) : std::exception() {
+  va_list varArgs;
+  va_start (varArgs, method);
+  printStringArgs(__what, varArgs, format);
+  va_end(varArgs);
+  addTrace(method);
+}
+
+prg_exception::prg_exception(va_list varArgs, const char * format, const char * method) {
+  printStringArgs(__what, varArgs, format);
+  va_end(varArgs);
+  addTrace(method);
+}
+
+void prg_exception::addWhat(const char * message) {
+ __what += message;
+}
+
+void prg_exception::addTrace(const char * method) {
+  std::string temp;
+  printString(temp, "%s\n", method);
+  __trace.append(temp);
+}
+
+void prg_exception::addTrace(const char * method, const char * clazz) {
+  std::string temp;
+  printString(temp, "%s  of  %s\n", method, clazz);
+  __trace.append(temp);
+}
+
+/** ::addTrace(__FILE__, __LINE__); */
+void prg_exception::addTrace(const char * file, int line) {
+  std::string temp;
+  printString(temp, "line %d  of %s\n", line, file);
+  __trace.append(temp);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+prg_error::prg_error(const char * format, const char * method, ...) : prg_exception() {
+  va_list varArgs;
+  va_start (varArgs, method);
+  printStringArgs(__what, varArgs, format);
+  va_end(varArgs);
+  addTrace(method);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+memory_exception::memory_exception(const char * message) : std::bad_alloc() {
+  __what = message;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+resolvable_error::resolvable_error() : prg_exception() {}
+
+resolvable_error::resolvable_error(const char * format, ...) : prg_exception() {
   va_list varArgs;
   va_start (varArgs, format);
   printStringArgs(__what, varArgs, format);
   va_end(varArgs);
 }
 
-/** ::addTrace(__FILE__, __LINE__); */
-void program_error::addTrace(const char * file, int line) {
-  std::string temp;
-  printString(temp, "file: %s\nline: %d\n", file, line);
-  __trace.append(temp);
+////////////////////////////////////////////////////////////////////////////////
+
+/**  Construct. This is an alternate constructor for when the varArgs list for sMessage
+     has already been prepared. Primarily, this will be used by derived classes.        */
+usage_error::usage_error(const char * sExecutableFullpathName) : prg_exception() {
+   FileName     exe(sExecutableFullpathName);
+
+   printString(__what, "usage: %s%s [parameters file] [options]\n\n"
+                  "options: -o     -- ignore parameter result filename setting, use next parameter\n"
+                  "                   ex. c:\\>satscan.exe c:\\parameters.prm -o c:\\alternate.out.txt\n"
+                  "         -c     -- confirm parameter file correctness only (does not perform analysis)\n"
+                  "         -p     -- print parameter settings to screen (does not perform analysis)\n",
+               exe.getFileName().c_str(), exe.getExtension().c_str());
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+char * out_of_memory_cache = 0;
+
+void reserve_memory_cache() {
+  if (!out_of_memory_cache) out_of_memory_cache = new char[32000];
+}
+
+void prg_new_handler() {
+   delete [] out_of_memory_cache; out_of_memory_cache=0;
+   throw std::bad_alloc();
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
