c4d26c2b645291eb2b2807b29ebe5b431cca59f0
Scott Hostovich <hostovic@imsweb.com>
2007-05-10 13:35:40 +0000

    * 7.0.3

--- calculation/SaTScan.h
+++ calculation/SaTScan.h
@@ -56,14 +56,14 @@
  This file contains header information for all SaTScan program files
  **********************************************************************/
 /** Current version information. */
-#define VERSION_ID      "17"  /** incremental version identifier - this value must
+#define VERSION_ID      "18"  /** incremental version identifier - this value must
                                   be incremented for each released version in order
                                   for update feature to function correctly */
 #define VERSION_MAJOR   "7"
 #define VERSION_MINOR   "0"
-#define VERSION_RELEASE "2"
+#define VERSION_RELEASE "3"
 #define VERSION_PHASE   "" /** testing phase name - leave blank for release */
-#define VERSION_DATE    "March 8, 2007"
+#define VERSION_DATE    "May 10, 2007"
 
 /** PI declaration - this is now defined in math.h but changing may cause
     precision to differ and the programs results. */
