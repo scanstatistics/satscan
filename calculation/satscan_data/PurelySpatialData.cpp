195e8cb5ba13188912b0fc0880b76b193b8c34e4
Scott Hostovich <hostovic@imsweb.com>
2008-04-18 14:48:51 +0000

    * categorical model added * homogeneous poisson added

--- calculation/satscan_data/PurelySpatialData.cpp
+++ calculation/satscan_data/PurelySpatialData.cpp
@@ -11,6 +11,7 @@
 #include "RankModel.h"
 #include "OrdinalModel.h"
 #include "SSException.h"
+#include "HomogeneousPoissonModel.h"
 
 /** class constructor */
 CPurelySpatialData::CPurelySpatialData(const CParameters& Parameters, BasePrint& PrintDirection)
@@ -42,11 +43,13 @@ void CPurelySpatialData::SetProbabilityModel() {
   switch (gParameters.GetProbabilityModelType()) {
      case POISSON              : m_pModel = new CPoissonModel(*this);   break;
      case BERNOULLI            : m_pModel = new CBernoulliModel(); break;
+     case CATEGORICAL          :
      case ORDINAL              : m_pModel = new OrdinalModel(); break;
      case EXPONENTIAL          : m_pModel = new ExponentialModel(); break;
      case WEIGHTEDNORMAL       :
      case NORMAL               : m_pModel = new CNormalModel(); break;
      case RANK                 : m_pModel = new CRankModel(); break;
+     case HOMOGENEOUSPOISSON   : m_pModel = new HomogenousPoissonModel(); break;
      case SPACETIMEPERMUTATION : throw prg_error("Purely Spatial analysis not implemented for Space-Time Permutation model.\n",
                                                  "SetProbabilityModel()");
      default : throw prg_error("Unknown probability model type: '%d'.\n", "SetProbabilityModel()",
