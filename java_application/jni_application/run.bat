8263706821c839e8118b6a0644733ed79c7ed27a
Scott Hostovich <hostovic@imsweb.com>
2008-04-18 14:28:30 +0000

    * path updates

--- java_application/jni_application/run.bat
+++ java_application/jni_application/run.bat
@@ -1,2 +1,2 @@
 
-"C:\Program Files\Java\jdk1.6.0_03/bin/java" -jar -Xcheck:jni -Djava.library.path=C:\prj\satscan.development\SatScan\shared_library "C:\prj\satscan.development\SatScan\jni_application\dist\jni_application.jar"
+"C:\Program Files\Java\jdk1.6.0_03/bin/java" -jar -Xcheck:jni -Djava.library.path=C:\prj\satscan.development\SatScan\java_application\shared_library "C:\prj\satscan.development\SatScan\java_application\jni_application\dist\jni_application.jar"
