487b7ce0dd7b09a77cf95dc8ac9a5d3e22f66b80
Scott Hostovich <hostovic@imsweb.com>
2008-06-20 20:08:13 +0000

    * new native function in parameter settings window

--- java_application/jni_application/build.jni.headers.bat
+++ java_application/jni_application/build.jni.headers.bat
@@ -1,8 +1,8 @@
 
-"C:\Program Files\Java\jdk1.6.0_03\bin\javah.exe" -classpath c:\prj\satscan.development\satscan\jni_application\build\classes org.satscan.app.Parameters
-"C:\Program Files\Java\jdk1.6.0_03\bin\javah.exe" -classpath c:\prj\satscan.development\satscan\jni_application\build\classes org.satscan.app.CalculationThread
-"C:\Program Files\Java\jdk1.6.0_03\bin\javah.exe" -classpath c:\prj\satscan.development\satscan\jni_application\build\classes org.satscan.app.NativeTableModel
-"C:\Program Files\Java\jdk1.6.0_03\bin\javah.exe" -classpath c:\prj\satscan.development\satscan\jni_application\build\classes org.satscan.app.AppConstants
 
-"C:\Program Files\Java\jdk1.6.0_03\bin\javah.exe" -classpath c:\prj\satscan.development\satscan\jni_application\build\classes org.satscan.gui.ParameterSettingsFrame
+"C:\Program Files\Java\jdk1.6.0_04\bin\javah.exe" -classpath C:\prj\satscan.development\SatScan\java_application\jni_application\build\classes org.satscan.app.Parameters
+"C:\Program Files\Java\jdk1.6.0_04\bin\javah.exe" -classpath C:\prj\satscan.development\SatScan\java_application\jni_application\build\classes org.satscan.app.CalculationThread
+"C:\Program Files\Java\jdk1.6.0_04\bin\javah.exe" -classpath C:\prj\satscan.development\SatScan\java_application\jni_application\build\classes org.satscan.app.AppConstants
+"C:\Program Files\Java\jdk1.6.0_04\bin\javah.exe" -classpath C:\prj\satscan.development\SatScan\java_application\jni_application\build\classes org.satscan.gui.ParameterSettingsFrame
+"C:\Program Files\Java\jdk1.6.0_04\bin\javah.exe" -classpath C:\prj\satscan.development\SatScan\java_application\jni_application\build\classes org.satscan.gui.OberservableRegionsFrame
 
