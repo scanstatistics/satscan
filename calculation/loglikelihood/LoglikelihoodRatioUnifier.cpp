e27d71abdafef7409e5233fe4d17d45d32fa448d
Scott Hostovich <hostovic@imsweb.com>
2008-11-06 15:07:16 +0000

    * wasn't checking if data was interesting for low rates before adding calculated value to total

--- calculation/loglikelihood/LoglikelihoodRatioUnifier.cpp
+++ calculation/loglikelihood/LoglikelihoodRatioUnifier.cpp
@@ -123,7 +123,7 @@ void AdjustmentUnifier::AdjoinRatio(AbstractLikelihoodCalculator& Calculator,
                                     size_t tSetIndex) {
   if (Calculator.MultipleSetsHighRate(tCases, tMeasure, tSetIndex))
     gdRatio += Calculator.CalcLogLikelihoodRatioNormal(tCases, tMeasure, tMeasureAux, tSetIndex);
-  else
+  else if (Calculator.LowRate(tCases, tMeasure, tSetIndex))
     gdRatio += -1 * Calculator.CalcLogLikelihoodRatioNormal(tCases, tMeasure, tMeasureAux, tSetIndex);
 }
 
