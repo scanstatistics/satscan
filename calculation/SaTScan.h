7630c202a0c4d0f1d394af5093996264f7df9682
Scott Hostovich <hostovic@imsweb.com>
2010-07-22 19:24:33 +0000

    * https://www.squishlist.com/ims/satscan/66287/  - saving new parameter window settings failed silently -- missing class member defaults

--- calculation/SaTScan.h
+++ calculation/SaTScan.h
@@ -50,14 +50,14 @@
  This file contains header information for all SaTScan program files
  **********************************************************************/
 /** Current version information. */
-#define VERSION_ID      "26"  /** incremental version identifier - this value must
+#define VERSION_ID      "27"  /** incremental version identifier - this value must
                                   be incremented for each released version in order
                                   for update feature to function correctly */
 #define VERSION_MAJOR   "9"
 #define VERSION_MINOR   "0"
-#define VERSION_RELEASE "0"
+#define VERSION_RELEASE "1"
 #define VERSION_PHASE   "" /** testing phase name - leave blank for release */
-#define VERSION_DATE    "July 19, 2010"
+#define VERSION_DATE    "July 23, 2010"
 
 /** PI declaration - this is now defined in math.h but changing may cause
     precision to differ and the programs results. */
