b4aa8ad7ecf1ce0d05efefddefe6eac0deab13dc
Scott Hostovich <hostovic@imsweb.com>
2012-01-09 16:16:14 +0000

    * https://www.squishlist.com/ims/satscan/66314/  - no longer building to Solaris

--- scripts/solaris/solaris.satscan.run.gcc3.3.1_64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#temporarily set enviroment variables to point to gcc3.3.1 64-bit libraries
-env -i LD_LIBRARY_PATH=/usr/openwin/lib:/opt/net/sybase/as11.9.2/lib:/opt/net/utils/gcc3.3.1/lib/gcc-lib/sparc-sun-solaris2.9/3.3.1/sparcv9:/opt/net/utils/lib:/opt/net/pgsql/production/lib $1 $2 $3 $4
--- scripts/solaris/solaris.satscan.run.gcc3.4.4_64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#temporarily set enviroment variables to point to gcc3.4.4 64-bit libraries
-env -i LD_LIBRARY_PATH=/usr/openwin/lib:/opt/net/sybase/as11.9.2/lib:/opt/net/utils/gcc3.4.4/lib/sparcv9:/opt/net/utils/lib:/opt/net/pgsql/production/lib $1 $2 $3 $4
