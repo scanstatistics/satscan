d962a2e3b11db0e897cd33b1daaf815ed5486046
Scott Hostovich <hostovic@imsweb.com>
2013-10-18 18:24:01 +0000

    * https://www.squishlist.com/ims/satscan/66379/  - updated version constants

--- calculation/SaTScan.h
+++ calculation/SaTScan.h
@@ -57,8 +57,8 @@
 #define VERSION_MAJOR   "9"
 #define VERSION_MINOR   "2"
 #define VERSION_RELEASE "0"
-#define VERSION_PHASE   "Beta 13" /** testing phase name - leave blank for release */
-#define VERSION_DATE    "Not Released"
+#define VERSION_PHASE   "" /** testing phase name - leave blank for release */
+#define VERSION_DATE    "October 22, 2013"
 
 /** PI declaration - this is now defined in math.h but changing may cause
     precision to differ and the programs results. */
