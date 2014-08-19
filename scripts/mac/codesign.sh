2a42f9c232e4fe3d5770da713c7542db0529a97d
Scott Hostovich <hostovic@imsweb.com>
2014-08-19 16:08:21 +0000

    * https://www.squishlist.com/ims/satscan/66329/  - single codesign script needed

--- scripts/mac/codesign.sh
+++ scripts/mac/codesign.sh
@@ -3,12 +3,14 @@
 REQUIRED_ARGS=1
 if [ $# -lt "$REQUIRED_ARGS" ]
 then
-echo "Usage: `basename $0` <app>"
+echo "Usage: `basename $0` <file>"
+echo "   example: `basename $0` ./libsatscan.jnilib"
+echo "   example: `basename $0` ./satscan"
 echo "   example: `basename $0` ./SaTScan.app"
+echo "   example: `basename $0` ./Install.app"
 exit 1
 fi
 
 security unlock-keychain $HOME/Library/Keychains/login.keychain && codesign --force -v --deep -s "Developer ID Application: Information Management Services, Inc. (VF82MCMA83)" $1
-
 
 spctl --assess --verbose=4 --raw $1
