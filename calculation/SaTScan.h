0eb57423b0e375ebaebe1663a329eaa13a963b69
Scott Hostovich <hostovic@imsweb.com>
2015-02-13 16:35:23 +0000

    * https://www.squishlist.com/ims/satscan/66411/  - reverted last update

--- calculation/SaTScan.h
+++ calculation/SaTScan.h
@@ -52,7 +52,7 @@
  This file contains header information for all SaTScan program files
  **********************************************************************/
 /** Current version information. */
-#define VERSION_ID      "34"  /** incremental version identifier - this value must
+#define VERSION_ID      "33"  /** incremental version identifier - this value must
                                   be incremented for each released version in order
                                   for update feature to function correctly */
 #define VERSION_MAJOR   "9"
