d3aa4b1e64a5766109f9b993f8727b9bd5d04a18
Scott Hostovich <hostovic@imsweb.com>
2016-06-22 22:37:29 +0000

    * dfs mounts changes on network

--- installers/izpack/signSaTScanGuiApp.bat
+++ installers/izpack/signSaTScanGuiApp.bat
@@ -1,6 +1,6 @@
 
 REM Signing a file: (cross-certificate)
-\\nfse.omni.imsweb.com\satscan\build.area\satscan\installers\izpack\sign4j\sign4j.exe --verbose \\nfsf.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe sign /v /ac \\nfsf.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\MSCV-VSClass3.cer /f \\nfsf.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\ims-cross-sign.pfx /p %%iaUzoA0cl!p /n "Information Management Services, Inc." /t http://timestamp.verisign.com/scripts/timstamp.dll \\nfse.omni.imsweb.com\satscan\build.area\satscan\java_application\jni_application\dist\SaTScan.exe
+\\nfsk.omni.imsweb.com\satscan\build.area\satscan\installers\izpack\sign4j\sign4j.exe --verbose \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe sign /v /ac \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\MSCV-VSClass3.cer /f \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\ims-cross-sign.pfx /p %%iaUzoA0cl!p /n "Information Management Services, Inc." /t http://timestamp.verisign.com/scripts/timstamp.dll \\nfsk.omni.imsweb.com\satscan\build.area\satscan\java_application\jni_application\dist\SaTScan.exe
 
 REM To verify the file is signed correctly:
-\\nfsf.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe verify /pa /v \\nfse.omni.imsweb.com\satscan\build.area\satscan\java_application\jni_application\dist\SaTScan.exe
+\\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe verify /pa /v \\nfsk.omni.imsweb.com\satscan\build.area\satscan\java_application\jni_application\dist\SaTScan.exe
