7eac0b5a3e0a32921106a7df37887b9e61291c46
Scott Hostovich <hostovic@imsweb.com>
2016-12-14 13:59:34 +0000

    * 9.4 -> 9.5

--- installers/izpack/signWindowsInstaller.bat
+++ installers/izpack/signWindowsInstaller.bat
@@ -1,6 +1,6 @@
 
 REM Signing a file: (cross-certificate)
-\\nfsk.omni.imsweb.com\satscan\build.area\satscan\installers\izpack\sign4j\sign4j.exe --verbose \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe sign /v /ac \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\MSCV-VSClass3.cer /f \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\ims-cross-sign.pfx /p %%iaUzoA0cl!p /n "Information Management Services, Inc." /t http://timestamp.verisign.com/scripts/timstamp.dll \\nfsk.omni.imsweb.com\satscan\installers\v.9.4.x\install-9_4_windows.exe
+\\nfsk.omni.imsweb.com\satscan\build.area\satscan\installers\izpack\sign4j\sign4j.exe --verbose \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe sign /v /ac \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\MSCV-VSClass3.cer /f \\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\ims-cross-sign.pfx /p %%iaUzoA0cl!p /n "Information Management Services, Inc." /t http://timestamp.verisign.com/scripts/timstamp.dll \\nfsk.omni.imsweb.com\satscan\installers\v.9.5.x\install-9_5_windows.exe
 
 REM To verify the file is signed correctly:
-\\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe verify /pa /v \\nfsk.omni.imsweb.com\satscan\installers\v.9.4.x\install-9_4_windows.exe
+\\nfsl.omni.imsweb.com\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe verify /pa /v \\nfsk.omni.imsweb.com\satscan\installers\v.9.5.x\install-9_5_windows.exe
