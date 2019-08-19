4b7053929574127e83c8a7e606a49dd5de2c6897
Scott Hostovich <hostovic@imsweb.com>
2019-08-19 20:34:43 +0000

    * https://squishlist.com/ims/treescan/139/  - updated certificate password  - updated launch4j configs

--- installers/izpack/signWindowsInstaller.bat
+++ installers/izpack/signWindowsInstaller.bat
@@ -1,6 +1,6 @@
 
 REM Signing a file: (cross-certificate)
-\\oriole-04-int\satscan\build.area\satscan\installers\izpack\sign4j\sign4j.exe --verbose \\oriole-04-int\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe sign /v /ac \\oriole-04-int\imsadmin\code.sign.cert.ms.auth.verisign\MSCV-VSClass3.cer /f \\oriole-04-int\imsadmin\code.sign.cert.ms.auth.verisign\ims-cross-sign-2017.pfx /p "YKtNv&otBfX7" /n "Information Management Services, Inc." /t http://timestamp.verisign.com/scripts/timstamp.dll \\oriole-04-int\satscan\installers\v.9.6.x\install-9_6_windows.exe
+\\oriole-04-int\satscan\build.area\satscan\installers\izpack\sign4j\sign4j.exe --verbose \\oriole-04-int\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe sign /f \\oriole-04-int\imsadmin\code.sign.cert.ms.auth.verisign\ims-cross-sign.pfx /p "&4L(JyhyOmwF)$Z" /t http://timestamp.verisign.com/scripts/timstamp.dll /v \\oriole-04-int\satscan\installers\v.9.6.x\install-9_6_windows.exe
 
 REM To verify the file is signed correctly:
 \\oriole-04-int\imsadmin\code.sign.cert.ms.auth.verisign\signtool.exe verify /pa /v \\oriole-04-int\satscan\installers\v.9.6.x\install-9_6_windows.exe
