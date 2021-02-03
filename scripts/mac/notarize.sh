3aedde9e2cb1ffd68f5a1abbe93ba69ec7a99b02
Scott Hostovich <hostovichs@imsweb.com>
2021-02-03 10:21:16 -0500

    - working on Mac build

---
+++ scripts/mac/notarize.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+XCRUN="/usr/bin/xcrun"
+ALTOOL="/Applications/Xcode.app/Contents/Developer/usr/bin/altool"
+
+if [[ $# -gt 0 ]]; then
+  PASSWORD=$1
+else
+  read -p "Apple developer password: " PASSWORD
+fi
+
+$XCRUN $ALTOOL --notarize-app \
+               --primary-bundle-id "org.satscan.gui.SaTScanApplication.dmg" \
+               --username "meagherk@imsweb.com" \
+               --password "${PASSWORD}" \
+               --file SaTScan.dmg
