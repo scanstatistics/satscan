f8415cf0edb42bbde3f851ecba8b432772a6bca6
Scott Hostovich <hostovichs@imsweb.com>
2021-06-14 15:36:37 -0400

    #66528 - install/update Windows with both 32-bit and 64-bit

--- installers/buildWindowsInstaller.bat
+++ installers/buildWindowsInstaller.bat
@@ -11,8 +11,10 @@ set satscanversionf=10_0
 set satscanexe=%fileshare%\satscan\build.area\satscan\java_application\jni_application\dist\SaTScan.exe
 set satscaninstaller=%fileshare%\satscan\installers\v.%satscanversion%.x\install-%satscanversionf%_windows.exe
 
-set javajdk=%fileshare%\satscan\installers\install.applications\java\jdk-16.0.1+9_adopt_windows_x64
-set runtimeoutput=%fileshare%\satscan\build.area\satscan\installers\java\jre
+set javajdkx64=%fileshare%\satscan\installers\install.applications\java\jdk-16.0.1+9_adopt_windows_x64
+set runtimeoutputx64=%fileshare%\satscan\build.area\satscan\installers\java\jre_x64
+set javajdkx86=%fileshare%\satscan\installers\install.applications\java\jdk-16.0.1+9_adopt_windows_x86
+set runtimeoutputx86=%fileshare%\satscan\build.area\satscan\installers\java\jre_x86
 
 set innosetup="C:\Program Files (x86)\Inno Setup 6\iscc.exe"
 set innoiss=%fileshare%\satscan\build.area\satscan\installers\inno-setup\satscan.iss
@@ -30,9 +32,13 @@ REM Codesigning a GUI exe file.
 REM Verify the GUI exe file is codesigned correctly.
 %signtool% verify /pa /v %satscanexe%
 
-REM Create Java runtime
-if exist %runtimeoutput% rmdir %runtimeoutput% /s /q
-%javajdk%\bin\jlink.exe --module-path %javajdk%\jmods --add-modules java.base,java.datatransfer,java.desktop,java.logging,java.prefs,java.xml,java.xml.crypto,jdk.crypto.cryptoki --output %runtimeoutput% --strip-debug --compress 2 --no-header-files --no-man-pages
+REM Create Java 64-bit runtime
+if exist %runtimeoutputx64% rmdir %runtimeoutputx64% /s /q
+%javajdkx64%\bin\jlink.exe --module-path %javajdkx64%\jmods --add-modules java.base,java.datatransfer,java.desktop,java.logging,java.prefs,java.xml,java.xml.crypto,jdk.crypto.cryptoki --output %runtimeoutputx64% --strip-debug --compress 2 --no-header-files --no-man-pages
+
+REM Create Java 32-bit runtime
+if exist %runtimeoutputx86% rmdir %runtimeoutputx86% /s /q
+%javajdkx86%\bin\jlink.exe --module-path %javajdkx86%\jmods --add-modules java.base,java.datatransfer,java.desktop,java.logging,java.prefs,java.xml,java.xml.crypto,jdk.crypto.cryptoki --output %runtimeoutputx86% --strip-debug --compress 2 --no-header-files --no-man-pages
 
 REM Build InnoSetup installer.
 %innosetup% %innoiss%
--- installers/inno-setup/satscan.iss
+++ installers/inno-setup/satscan.iss
@@ -49,7 +49,8 @@ Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{
 Source: "Q:\build.area\satscan\java_application\jni_application\dist\SaTScan.exe"; DestDir: "{app}"; Flags: ignoreversion
 Source: "Q:\build.area\satscan\java_application\jni_application\dist\lib\*"; DestDir: "{app}\lib"; Flags: ignoreversion recursesubdirs createallsubdirs
 Source: "Q:\build.area\satscan\installers\sample_data\*"; DestDir: "{app}\sample_data"; Flags: ignoreversion recursesubdirs createallsubdirs
-Source: "Q:\build.area\satscan\installers\java\jre\*"; DestDir: "{app}\jre"; Flags: ignoreversion recursesubdirs createallsubdirs
+Source: "Q:\build.area\satscan\installers\java\jre_x86\*"; DestDir: "{app}\jre"; Flags: ignoreversion recursesubdirs createallsubdirs; Check: not Is64BitInstallMode
+Source: "Q:\build.area\satscan\installers\java\jre_x64\*"; DestDir: "{app}\jre"; Flags: ignoreversion recursesubdirs createallsubdirs; Check: Is64BitInstallMode
 Source: "Q:\build.area\satscan\installers\documents\eula.html"; DestDir: "{app}"; Flags: ignoreversion
 Source: "Q:\build.area\satscan\installers\documents\SaTScan_Users_Guide.pdf"; DestDir: "{app}"; Flags: ignoreversion
 Source: "Q:\build.area\satscan\batch_application\Win32\Release\SaTScanBatch.exe"; DestDir: "{app}"; Flags: ignoreversion; Check: not Is64BitInstallMode
