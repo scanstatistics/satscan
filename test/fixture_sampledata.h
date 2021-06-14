ebdd382d1b07bbe26ac711a9389308e6f51c748f
Scott Hostovich <hostovichs@imsweb.com>
2021-06-14 08:11:21 -0400

    #66557 – update tests for sample data parameter file rename

--- test/fixture_sampledata.h
+++ test/fixture_sampledata.h
@@ -5,31 +5,31 @@
 #include "fixture_prm_source.h"
 
 struct new_mexico_fixture : prm_sampledata_fixture {
-    new_mexico_fixture() : prm_sampledata_fixture("nm.prm") { }
+    new_mexico_fixture() : prm_sampledata_fixture("NewMexicoSpaceTimePoisson.prm") { }
     virtual ~new_mexico_fixture() { }
 };
 struct nhumberside_fixture : prm_sampledata_fixture {
-    nhumberside_fixture() : prm_sampledata_fixture("NHumberside.prm") { }
+    nhumberside_fixture() : prm_sampledata_fixture("NHumbersideSpatialBernoulli.prm") { }
     virtual ~nhumberside_fixture() { }
 };
 struct nycfever_fixture : prm_sampledata_fixture {
-    nycfever_fixture() : prm_sampledata_fixture("NYCfever.prm") { }
+    nycfever_fixture() : prm_sampledata_fixture("NYCSpaceTimePermutation.prm") { }
     virtual ~nycfever_fixture() { }
 };
 struct normalfake_fixture : prm_sampledata_fixture {
-    normalfake_fixture() : prm_sampledata_fixture("NormalFake.prm") { }
+    normalfake_fixture() : prm_sampledata_fixture("FakeSpatialNormal.prm") { }
     virtual ~normalfake_fixture() { }
 };
 struct survivalfake_fixture : prm_sampledata_fixture {
-    survivalfake_fixture() : prm_sampledata_fixture("SurvivalFake.prm") { }
+    survivalfake_fixture() : prm_sampledata_fixture("FakeSpaceTimeExponential.prm") { }
     virtual ~survivalfake_fixture() { }
 };
 struct marylandeducation_fixture : prm_sampledata_fixture {
-    marylandeducation_fixture() : prm_sampledata_fixture("MarylandEducation.prm") { }
+    marylandeducation_fixture() : prm_sampledata_fixture("MarylandSpatialOrdinal.prm") { }
     virtual ~marylandeducation_fixture() { }
 };
 struct continuouspoissonfake_fixture : prm_sampledata_fixture {
-    continuouspoissonfake_fixture() : prm_sampledata_fixture("ContinuousPoissonFake.prm") { }
+    continuouspoissonfake_fixture() : prm_sampledata_fixture("PolygonSpatialContinuousPoisson.prm") { }
     virtual ~continuouspoissonfake_fixture() { }
 };
 #endif
