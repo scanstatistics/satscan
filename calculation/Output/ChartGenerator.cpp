5c007decef1e6525b73edbd2063a7b03a3be3eeb
Scott Hostovich <spatovich@gmail.com>
2025-10-02 10:19:10 -0400

    #66722 - disallow decimals in y-axis of temporal graph

--- calculation/Output/ChartGenerator.cpp
+++ calculation/Output/ChartGenerator.cpp
@@ -201,7 +201,7 @@ const char * TemporalChartGenerator::TEMPLATE_CHARTHEADER = "\n \
                     tooltip: { crosshairs: true, shared: true, formatter: function(){var is_cluster = false;var has_observed = false;$.each(this.points, function(i, point) {if (point.series.options.id == 'cluster') {is_cluster = true;}if (point.series.options.id == 'obs') {has_observed = true;}});var s = '<b>'+ this.x +'</b>'; if (is_cluster) {s+= '<br/><b>Cluster Point</b>';}$.each(this.points,function(i, point){if (point.series.options.id == 'cluster'){if (!has_observed) {s += '<br/>Observed: '+ point.y;}} else {s += '<br/>'+ point.series.name +': '+ point.y;}});return s;}, }, \n \
                     legend: { enabled: true, backgroundColor: '#F5F5F5', verticalAlign: 'top', y: 40 }, \n \
                     xAxis: [{ categories: [--categories--], tickmarkPlacement: 'on', labels: { step: --step--, rotation: -45, align: 'right' }, tickInterval: --tickinterval-- }], \n \
-                    yAxis: [{ reversedStacks: false, title: { enabled: true, text: 'Number of Cases', style: { fontWeight: 'normal' } }, min: 0, showEmpty: false }--additional-yaxis--], \n \
+                    yAxis: [{ allowDecimals: false,  reversedStacks: false, title: { enabled: true, text: 'Number of Cases', style: { fontWeight: 'normal' } }, min: 0, showEmpty: false }--additional-yaxis--], \n \
                     navigation: { buttonOptions: { align: 'right' } }, \n \
                     series: [--series--]\n \
                 }; \n \
